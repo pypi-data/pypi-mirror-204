09c262f8d88dfd8dd4f91c0ec37eae3bd0aeff13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:16 +0000

    Add package postpruner/1.1.2/postpruner-1.1.2.tar.gz

---
+++ packages/postpruner/postpruner-1.1.2.tar.gz/postpruner-1.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="postpruner",
+    version="1.1.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "postpruner = postpruner.__main__:main",
+        ],
+    },
+    install_requires=[
+        "torch",
+        "transformers",
+        "numpy",
+        "scipy",
+        "cupy-cuda12x",
+        "tqdm",
+        "datasets"
+    ]
+)
