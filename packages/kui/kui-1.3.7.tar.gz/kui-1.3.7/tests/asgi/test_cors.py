abf807dfa7b3610884b0d3d205325a22a6a8e7c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:56 +0000

    Add package kui/1.3.7/kui-1.3.7.tar.gz

---
+++ packages/kui/kui-1.3.7.tar.gz/kui-1.3.7/tests/asgi/test_cors.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import pytest
+from async_asgi_testclient import TestClient
+
+from kui.asgi import allow_cors
+
+
+@pytest.mark.asyncio
+async def test_cors():
+    cors_middleware = allow_cors()
+
+    from kui.asgi import HttpRoute, Kui
+
+    app = Kui()
+
+    async def homepage():
+        return "homepage"
+
+    app.router <<= HttpRoute("/", homepage) @ cors_middleware
+
+    async with TestClient(app, headers={"origin": "testserver"}) as client:
+        resp = await client.get("/")
+        assert resp.headers["access-control-allow-origin"] == "testserver"
+
+        resp = await client.options("/")
+        assert resp.headers["access-control-allow-origin"] == "testserver"
