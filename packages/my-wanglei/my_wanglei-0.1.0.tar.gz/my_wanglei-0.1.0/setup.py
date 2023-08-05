846dff5f18ad475b1622c5af9c3b880646ebfbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:44 +0000

    Add package my-wanglei/0.1.0/my_wanglei-0.1.0.tar.gz

---
+++ packages/my-wanglei/my_wanglei-0.1.0.tar.gz/my_wanglei-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='my_wanglei',
+    version='0.1.0',
+    author='wanglei',
+    author_email='819966818@qq.com',
+    description='.....',
+    packages=['my_wanglei'],
+    install_requires=[
+        'requests',
+        'numpy',
+    ]
+)
+
+packages=['my_wanglei'],
+
+
