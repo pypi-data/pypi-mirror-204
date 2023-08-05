1611c53c156e52eac549fbf91d7486bc37aedd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:04 +0000

    Add package caesar-rock/0.0.7/caesar-rock-0.0.7.tar.gz

---
+++ packages/caesar-rock/caesar-rock-0.0.7.tar.gz/caesar-rock-0.0.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+with open('README.md', 'r') as arq:
+    readme = arq.read()
+
+setup(name='caesar-rock',
+      version='0.0.7',
+      license='MIT',
+      long_description= long_description,
+      long_description_content_type="text/markdown",
+      author='LCCV/UFAL',
+      author_email='erasmo.bezerra@lccv.ufal.br',
+      description='Package for calculating the confined compressive strength of rock.',
+      packages= ['caesarrock'],
+      install_requires=['pandas', 'matplotlib', 'numpy', 'sympy'],)
