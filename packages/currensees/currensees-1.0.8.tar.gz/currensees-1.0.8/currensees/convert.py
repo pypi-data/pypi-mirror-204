57bbf90a0d1414b7c55e135727490776eb647798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:44 +0000

    Add package currensees/1.0.8/currensees-1.0.8.tar.gz

---
+++ packages/currensees/currensees-1.0.8.tar.gz/currensees-1.0.8/currensees/convert.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+class Convert:
+    def __init__(self, headers):
+        self.base_url = 'https://currensees.com/v1'
+        self.headers = headers
+
+    def convert_currency(self, date, base_currency, target_currency, amount):
+        url = f'{self.base_url}/convert'
+        data = {
+            'date': date,
+            'base_currency': base_currency,
+            'target_currency': target_currency,
+            'amount': amount
+        }
+        response = requests.post(url, headers=self.headers, data=json.dumps(data))
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to convert currency: {response.text}")
