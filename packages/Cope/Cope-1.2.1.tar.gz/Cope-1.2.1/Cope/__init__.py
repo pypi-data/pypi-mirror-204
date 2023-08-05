ee2f9a9d7a7961b5f5d9fe4541d933ced728a00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:19 +0000

    Add package Cope/1.2.1/Cope-1.2.1.tar.gz

---
+++ packages/Cope/Cope-1.2.1.tar.gz/Cope-1.2.1/Cope/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '1.2.1'
+from .constants import *
+from .debugging import debug
+from .decorators import *
+from .imports import *
+from .geometry import *
+from .logging import *
+from .misc import *
+from .key import *
+from .point import *
+from .timing import *
+from .prettyTable import *
+from .iterables import *
+from .func import *
+from .colors import *
+from .errors import *
+# from .linalg import *
+from . import linalg
