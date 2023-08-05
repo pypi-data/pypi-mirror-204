a03d7dac3c51556a757a740e92b49b586bc64679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:25 +0000

    Add package cavasspy/1.1.5/cavasspy-1.1.5.tar.gz

---
+++ packages/cavasspy/cavasspy-1.1.5.tar.gz/cavasspy-1.1.5/cavasspy/converter.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+from uuid import uuid4
+
+from cavasspy.op import execute_cmd
+
+
+def nii2dicom(input_file, output_dir, accession_number=1):
+    """
+    Require nifti2dicom.
+    sudo apt install nifti2dicom
+    https://github.com/biolab-unige/nifti2dicom
+    """
+    cmd = f'nifti2dicom -i {input_file} -o {output_dir} -a {accession_number}'
+    result = os.popen(cmd)
+    return result.read()
+
+
+def dicom2cavass(input_dir, output_file):
+    execute_cmd(f'from_dicom {input_dir}/* {output_file}')
+
+
+def nii2cavass(input_file, output_file, dicom_accession_number=1):
+    save_path = os.path.split(output_file)[0]
+    tmp_dicom_dir = os.path.join(save_path, f'{uuid4()}')
+    nii2dicom(input_file, tmp_dicom_dir, dicom_accession_number)
+    dicom2cavass(tmp_dicom_dir, output_file)
+    shutil.rmtree(tmp_dicom_dir)
+
+
+if __name__ == '__main__':
+    nii2cavass('/home/ubuntu/sda1.8T/Dataset/AMOS/AMOS22/imagesTr/amos_0086.nii.gz', '/home/ubuntu/sdb1.8T/dj/amos_0086.IM0')
