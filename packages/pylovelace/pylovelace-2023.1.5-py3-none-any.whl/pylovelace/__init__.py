d9dff804ea1b4a3e5c34eeab8c5b80392ce7da37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:20 +0000

    Add package pylovelace/2023.1.5/pylovelace-2023.1.5-py3-none-any.whl

---
+++ packages/pylovelace/pylovelace-2023.1.5-py3-none-any.whl/pylovelace/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+PyLovelace
+Copyright (c) 2023 PyLovelace
+All rights reserved.
+
+@Author: nshout
+@File: __init__.py
+"""
+
+__version__ = "2023.1.5"
+__description__ = "Python code protection/obfuscation tool"
