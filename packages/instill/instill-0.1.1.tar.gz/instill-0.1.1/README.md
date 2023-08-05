4671377cced31280a9412e5dc05df6730ed4fc9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package instill/0.1.1/instill-0.1.1.tar.gz

---
+++ packages/instill/instill-0.1.1.tar.gz/instill-0.1.1/README.md
@@ -0,0 +1,2 @@
+# instill 0.1.1
+Instill drops of data in source files
---
+++ packages/instill/instill-0.1.1.tar.gz/instill-0.1.1/src/instill/version.py
@@ -0,0 +1,10 @@
+__all__ = [
+    'get_version',
+]
+
+
+VERSION = '0.1.1'  ## bumpversion!
+
+
+def get_version():
+    return VERSION
