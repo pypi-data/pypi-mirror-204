a4d267611cd4435c7a13213dafedca851c824978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:34 +0000

    Add package Pequena/0.0.25/Pequena-0.0.25-py3-none-any.whl

---
+++ packages/Pequena/Pequena-0.0.25-py3-none-any.whl/Pequena/__init__.py
@@ -0,0 +1,6 @@
+from Pequena.window import init
+from Pequena.window import create_window
+from Pequena.window import expose_function
+
+from Pequena.window import set_closable
+from Pequena.window import get_closable
