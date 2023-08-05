6c4c1ee1d95897ad7f9135dbaadfcf42935b4438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:27 +0000

    Add package python-secrets/23.4.1/python_secrets-23.4.1-py3.9.egg

---
+++ packages/python-secrets/python_secrets-23.4.1-py3.9.egg/psec/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+__version__ = None
+__release__ = '23.4.1'
+
+# Get development version from repository tags.
+try:
+    from setuptools_scm import get_version
+    __version__ = get_version(root='..', relative_to=__file__)
+except (ImportError, LookupError):
+    pass
+
+if __version__ is None:
+    from pkg_resources import get_distribution, DistributionNotFound
+    try:
+        __version__ = get_distribution("psec").version
+    except DistributionNotFound:
+        pass
+
+if __version__ is None:
+    __version__ = __release__
+
+__author__ = 'Dave Dittrich'
+__email__ = 'dave.dittrich@gmail.com'
+
+__all__ = ['__author__', '__email__', '__version__', '__release__']
+
+# vim: set fileencoding=utf-8 ts=4 sw=4 tw=0 et :
