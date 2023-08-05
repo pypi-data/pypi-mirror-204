16cd036146e5239e517e2a7c57c9ac4cc373a20a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:45 +0000

    Add package my-pipeline-package/1.1.1/my-pipeline-package-1.1.1.tar.gz

---
+++ packages/my-pipeline-package/my-pipeline-package-1.1.1.tar.gz/my-pipeline-package-1.1.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="my-pipeline-package",
+    version="1.1.1",
+    description="Build package from python tests.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    zip_safe=True,
+)
