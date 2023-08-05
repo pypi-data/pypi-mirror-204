2a968df58871ae8fb897e77d9c6b19a56b50b377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package exceltotable/0.1/exceltotable-0.1.tar.gz

---
+++ packages/exceltotable/exceltotable-0.1.tar.gz/exceltotable-0.1/exceltotable/db1.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import mysql.connector as msql
+from sqlalchemy import create_engine, types
+import sys
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Show top lines from each file')
+parser.add_argument('path',type=str,help='enter the path you want to generate')
+args = parser.parse_args()
+engine = create_engine('mysql+pymysql://root:Lly486597@localhost/db1') # enter your password and database names here
+print(args)
+for filename in os.listdir(args.path):
+    f = os.path.join(args.path, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+      if(".csv" in f):
+        df = pd.read_csv(f,index_col=False,delimiter=',')
+        df.to_sql(filename.split('/')[-1].split('.')[0],con=engine,index=False,if_exists='replace') # Replace Table_name with your sql table name
