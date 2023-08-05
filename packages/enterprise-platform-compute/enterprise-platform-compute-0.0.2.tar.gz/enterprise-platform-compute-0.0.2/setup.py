d10cd63de2a95e35c1e03a4070856e231999cbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package enterprise-platform-compute/0.0.2/enterprise-platform-compute-0.0.2.tar.gz

---
+++ packages/enterprise-platform-compute/enterprise-platform-compute-0.0.2.tar.gz/enterprise-platform-compute-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="enterprise-platform-compute",
+    version="0.0.2",
+    packages=["compute"],
+    install_requires=[
+        "cloudpickle==2.2.1",
+        "gql[botocore,requests]==3.4.0",
+        "httpx==0.24.0",
+        "networkx==3.1",
+        "polling2==0.5.0",
+        "websockets==11.0.2",
+    ],
+)
