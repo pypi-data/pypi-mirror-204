b45a562a8bf1bbb7a3dfead95102ca6aacb77610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:26 +0000

    Add package periodogram/0.0.1/periodogram-0.0.1.tar.gz

---
+++ packages/periodogram/periodogram-0.0.1.tar.gz/periodogram-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="periodogram",
+    version="0.0.1",
+    author="Daniel Hey",
+    url="https://github.com/danhey/periodogram",
+    packages=["periodogram"],
+    description="Simple periodogram manipulation in Python.",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+)
