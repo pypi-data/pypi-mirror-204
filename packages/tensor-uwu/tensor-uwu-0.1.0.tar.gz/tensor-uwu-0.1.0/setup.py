fdb1701a1ebe116b82a3ecc75b63b46da21272e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:19 +0000

    Add package tensor-uwu/0.1.0/tensor-uwu-0.1.0.tar.gz

---
+++ packages/tensor-uwu/tensor-uwu-0.1.0.tar.gz/tensor-uwu-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tensor-uwu",
+    version="0.1.0",
+    description="An amazing package for arch linux uwu",
+    author="Manish Raj Osti",
+    author_email="manishrajosti1@gmail.com",
+    url="https://github.com/manishrajosti/tensor-uwu",
+    packages=['tensor-uwu']
+)
