b96c54c5738557975e2faee691e60a1fa44ae098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:43 +0000

    Add package domain-admin/1.2.11/domain-admin-1.2.11.tar.gz

---
+++ packages/domain-admin/domain-admin-1.2.11.tar.gz/domain-admin-1.2.11/domain_admin/enums/version_enum.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : version_enum.py
+@Date    : 2023-02-06
+"""
+
+
+class VersionEnum(object):
+    """
+    版本号常量
+    """
+    Version_100 = '1.0.0'
+
+    Version_101 = '1.0.1'
+
+    Version_102 = '1.0.2'
+
+    Version_103 = '1.0.3'
+
+    Version_104 = '1.0.4'
+
+    Version_105 = '1.0.5'
+
+    Version_106 = '1.0.6'
+
+    Version_110 = '1.1.0'
