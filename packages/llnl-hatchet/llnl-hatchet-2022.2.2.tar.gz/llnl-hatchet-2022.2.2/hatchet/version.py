fc6b835ffd06da089cb2af1fb3482eca97e46f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:12 +0000

    Add package llnl-hatchet/2022.2.2/llnl-hatchet-2022.2.2.tar.gz

---
+++ packages/llnl-hatchet/llnl-hatchet-2022.2.2.tar.gz/llnl-hatchet-2022.2.2/hatchet/version.py
@@ -0,0 +1,7 @@
+# Copyright 2017-2022 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+__version_info__ = ("2022", "2", "2")
+__version__ = ".".join(__version_info__)
