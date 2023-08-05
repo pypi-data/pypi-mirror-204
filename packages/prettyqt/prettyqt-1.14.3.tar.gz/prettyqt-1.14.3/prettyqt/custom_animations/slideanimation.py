3581ef4c0f0b33885f4fd2d699ca99a3e227a8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:35 +0000

    Add package prettyqt/1.14.3/prettyqt-1.14.3.tar.gz

---
+++ packages/prettyqt/prettyqt-1.14.3.tar.gz/prettyqt-1.14.3/prettyqt/custom_animations/slideanimation.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from prettyqt import core
+from prettyqt.qt import QtCore
+from prettyqt.utils import datatypes
+
+
+class SlideAnimation(core.PropertyAnimation):
+    def __init__(
+        self, duration: int = 1000, easing: core.easingcurve.TypeStr = "in_out_sine"
+    ):
+        super().__init__()
+        self.set_easing(easing)
+        self.set_start_value(core.Point(0, 0))
+        self.setDuration(duration)
+
+    def set_start_value(self, point: datatypes.PointType):
+        if isinstance(point, tuple):
+            point = core.Point(*point)
+        self.setStartValue(point)
+
+    def set_end_value(self, point: datatypes.PointType):
+        if isinstance(point, tuple):
+            point = core.Point(*point)
+        self.setEndValue(point)
+
+    def apply_to(self, obj: QtCore.QObject, attribute: str = "pos"):
+        super().apply_to(obj, attribute)
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    app = widgets.app()
+    val = SlideAnimation()
---
+++ packages/prettyqt/prettyqt-1.14.3.tar.gz/prettyqt-1.14.3/prettyqt/widgets/graphicsanchorlayout.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from prettyqt import constants, widgets
+from prettyqt.qt import QtCore, QtWidgets
+from prettyqt.utils import InvalidParamError, bidict
+
+
+EDGE = bidict(
+    left=QtCore.Qt.AnchorPoint.AnchorLeft,
+    horizontal_center=QtCore.Qt.AnchorPoint.AnchorHorizontalCenter,
+    right=QtCore.Qt.AnchorPoint.AnchorRight,
+    top=QtCore.Qt.AnchorPoint.AnchorTop,
+    vertical_center=QtCore.Qt.AnchorPoint.AnchorVerticalCenter,
+    bottom=QtCore.Qt.AnchorPoint.AnchorBottom,
+)
+
+EdgeStr = Literal[
+    "left", "horizontal_center", "right", "top", "vertical_center", "bottom"
+]
+
+
+class GraphicsAnchorLayout(widgets.GraphicsLayoutMixin, QtWidgets.QGraphicsAnchorLayout):
+    def add_anchor(
+        self,
+        first_item: QtWidgets.QGraphicsLayoutItem,
+        first_edge: EdgeStr,
+        second_item: QtWidgets.QGraphicsLayoutItem,
+        second_edge: EdgeStr,
+    ) -> QtWidgets.QGraphicsAnchor:
+        return self.addAnchor(
+            first_item, EDGE[first_edge], second_item, EDGE[second_edge]
+        )
+
+    def get_anchor(
+        self,
+        first_item: QtWidgets.QGraphicsLayoutItem,
+        first_edge: EdgeStr,
+        second_item: QtWidgets.QGraphicsLayoutItem,
+        second_edge: EdgeStr,
+    ) -> QtWidgets.QGraphicsAnchor:
+        return self.anchor(first_item, EDGE[first_edge], second_item, EDGE[second_edge])
+
+    def add_anchors(
+        self,
+        first_item: QtWidgets.QGraphicsLayoutItem,
+        second_item: QtWidgets.QGraphicsLayoutItem,
+        orientation: constants.OrientationStr,
+    ):
+        if orientation not in constants.ORIENTATION:
+            raise InvalidParamError(orientation, constants.ORIENTATION)
+        self.addAnchors(
+            first_item, second_item, constants.ORIENTATION[orientation]  # type: ignore
+        )
+
+    def add_corner_anchors(
+        self,
+        first_item: QtWidgets.QGraphicsLayoutItem,
+        first_corner: constants.CornerStr,
+        second_item: QtWidgets.QGraphicsLayoutItem,
+        second_corner: constants.CornerStr,
+    ):
+        self.addCornerAnchors(
+            first_item,
+            constants.CORNER[first_corner],
+            second_item,
+            constants.CORNER[second_corner],
+        )
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    layout = GraphicsAnchorLayout()
