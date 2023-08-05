f43984f7785d29c10cbb557977d205f1cfd7efc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:38 +0000

    Add package fasdr/0.0.5/fasdr-0.0.5.tar.gz

---
+++ packages/fasdr/fasdr-0.0.5.tar.gz/fasdr-0.0.5/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name='fasdr',
+    version='0.0.5',
+    author='David Marx',
+    author_email='david.marx84@gmail.com',
+    description='Simple dense retrieval using SciPy, spaCy, and Sentence-Transformers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/dmarx/fast-and-simple-dense-retrieval/',
+    packages=['fasdr'],
+    install_requires=[
+        'numpy',
+        'spacy',
+        'sentence_transformers',
+        'scipy',
+        'spacy-sentence-bert',
+        'rich',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    entry_points={
+        'console_scripts': [
+            'fasdr=fasdr.cli:main',
+        ],
+    },
+)
