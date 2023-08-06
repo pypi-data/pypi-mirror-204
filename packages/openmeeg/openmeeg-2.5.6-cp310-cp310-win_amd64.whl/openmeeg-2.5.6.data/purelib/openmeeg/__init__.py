ac9bd871fe548ae152e87867cd260459e19573b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:05 +0000

    Add package openmeeg/2.5.6/openmeeg-2.5.6-cp310-cp310-win_amd64.whl

---
+++ packages/openmeeg/openmeeg-2.5.6-cp310-cp310-win_amd64.whl/openmeeg-2.5.6.data/purelib/openmeeg/__init__.py
@@ -0,0 +1,44 @@
+
+
+""""""# start delvewheel patch
+def _delvewheel_init_patch_1_3_6():
+    import os
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'openmeeg.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    if not is_pyinstaller or os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_init_patch_1_3_6()
+del _delvewheel_init_patch_1_3_6
+# end delvewheel patch
+
+from . import _distributor_init
+
+# Here we import as few things as possible to keep our API as limited as
+# possible
+from ._openmeeg_wrapper import (
+    HeadMat,
+    Sensors,
+    Integrator,
+    Head2EEGMat,
+    Head2MEGMat,
+    DipSourceMat,
+    DipSource2MEGMat,
+    GainEEG,
+    GainMEG,
+    GainEEGadjoint,
+    GainMEGadjoint,
+    GainEEGMEGadjoint,
+    Forward,
+    SurfSourceMat,
+    SurfSource2MEGMat,
+    Matrix,
+    SymMatrix,
+)
+from ._version import __version__
+from ._make_geometry import make_geometry, make_nested_geometry, read_geometry
+from ._utils import get_log_level, set_log_level, use_log_level
+
+set_log_level("warning")
