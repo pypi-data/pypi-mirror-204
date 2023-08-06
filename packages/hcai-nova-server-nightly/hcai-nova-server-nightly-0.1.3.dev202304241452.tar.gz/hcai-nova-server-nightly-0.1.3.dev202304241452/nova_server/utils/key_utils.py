c132eac27b5dfb0c40d878549bf6ec0ac5bbf48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:56 +0000

    Add package hcai-nova-server-nightly/0.1.3.dev202304241452/hcai-nova-server-nightly-0.1.3.dev202304241452.tar.gz

---
+++ packages/hcai-nova-server-nightly/hcai-nova-server-nightly-0.1.3.dev202304241452.tar.gz/hcai-nova-server-nightly-0.1.3.dev202304241452/nova_server/utils/key_utils.py
@@ -0,0 +1,24 @@
+import random
+
+def get_key_from_request_form(request_form):
+
+    key = request_form.get("jobID", None)
+
+    if key is None:
+        key == f"local_{random.randint(0, 10 ^ 7)}"
+
+    # id_components = [
+    #     request_form.get('username', None),
+    #     request_form.get('database', None),
+    #     request_form.get('scheme', None),
+    #     request_form.get('streamName', None),
+    #     request_form.get('annotator', None),
+    #     request_form.get('sessions', None).replace(";", "_"),
+    # ]
+    # server_key = '_'.join([x for x in id_components if x])
+
+
+    # serverkey = request_form['username'] + '_' + request_form['database'] + '_' + request_form['scheme'] + '_' + \
+    #     request_form['streamName'] + '_' + request_form['annotator'] + '_' + \
+    #     request_form['sessions'].replace(";", "_")
+    return key
