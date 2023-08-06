e820123732dc28c55be8fa46a9d5c3e564a157b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:04 +0000

    Add package excel-sql-engine/1.6.2/excel-sql-engine-1.6.2.tar.gz

---
+++ packages/excel-sql-engine/excel-sql-engine-1.6.2.tar.gz/excel-sql-engine-1.6.2/PyxlSql/pyxlVersion.py
@@ -0,0 +1,10 @@
+# ---------------------------------------------------------------------------------------------------------------
+# PyxlSQL project
+# This program and library is licenced under the European Union Public Licence v1.2 (see LICENCE)
+# developed by fabien.battini@gmail.com
+# ---------------------------------------------------------------------------------------------------------------
+
+class PyxlVersion:
+    def __init__(self):
+        self.version = '1.6.2 at 04/24/2023 20:06:08'
+
