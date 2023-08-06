685a2ecd3fc3001c9c7f5e3d4baeebdcb2400bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:26 +0000

    Add package pyopenbook/0.7.1a0/pyopenbook-0.7.1a0.tar.gz

---
+++ packages/pyopenbook/pyopenbook-0.7.1a0.tar.gz/pyopenbook-0.7.1a0/setup.py
@@ -0,0 +1,29 @@
+"""setuptools module for PySerum."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="pyopenbook",
+    version="0.7.1a",
+    author="serum-community",
+    description="""Python client library for interacting with the Project Serum DEX.""",
+    install_requires=[
+        "construct>=2.10.56, <3.0.0",
+        "construct-typing>=0.5.1, <1.0.0",
+        "solana>=0.11.3, <1.0.0",
+    ],
+    python_requires=">=3.7, <4",
+    license="MIT",
+    package_data={"pyserum": ["py.typed"]},
+    packages=find_packages(exclude=("tests", "tests.*")),
+    url="https://github.com/blockworks-foundation/pyopenbook",
+    zip_safe=False,  # required per mypy
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
