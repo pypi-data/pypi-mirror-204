7ab2fbc0e2dcb1b74e1f3040c982f4a016f65061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:13 +0000

    Add package pygpt4all/1.0.1/pygpt4all-1.0.1.tar.gz

---
+++ packages/pygpt4all/pygpt4all-1.0.1.tar.gz/pygpt4all-1.0.1/README.md
@@ -0,0 +1,54 @@
+# PyGPT4All
+Official Python CPU inference for [GPT4All](https://github.com/nomic-ai/gpt4all) language models based on [llama.cpp](https://github.com/ggerganov/llama.cpp) and [ggml](https://github.com/ggerganov/ggml)
+
+[![License: MIT](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+[//]: # ([![PyPi version]&#40;https://badgen.net/pypi/v/ptgpt4all&#41;]&#40;https://pypi.org/project/pygpt4all/&#41;)
+
+**NB: Under active development**
+
+# Installation
+
+```bash
+pip install pygpt4all
+```
+
+# Usage
+
+### GPT4All model
+
+Download a GPT4All model from https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/
+
+```python
+from pygpt4all.models.gpt4all import GPT4All
+
+def new_text_callback(text):
+    print(text, end="")
+
+model = GPT4All('./models/ggml-gpt4all-j.bin')
+model.generate("Once upon a time, ", n_predict=55, new_text_callback=new_text_callback)
+```
+
+### GPT4All-J model
+
+Download the GPT4All-J model from https://gpt4all.io/models/ggml-gpt4all-j-v1.2-jazzy.bin
+
+```python
+from pygpt4all.models.gpt4all_j import GPT4All_J
+
+def new_text_callback(text):
+    print(text, end="")
+
+model = GPT4All_J('./models/ggml-gpt4all-j.bin')
+model.generate("Once upon a time, ", n_predict=55, new_text_callback=new_text_callback)
+```
+
+[//]: # (* You can always refer to the [short documentation]&#40;https://nomic-ai.github.io/pyllamacpp/&#41; for more details.)
+
+
+# License
+
+
+
+This project is licensed under the MIT  [License](./LICENSE).
+
---
+++ packages/pygpt4all/pygpt4all-1.0.1.tar.gz/pygpt4all-1.0.1/pygpt4all/models/gpt4all.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+GPT4ALL with `llama.cpp` backend
+"""
+
+__author__ = "abdeladim-s"
+__copyright__ = "Copyright 2023,"
+__license__ = "MIT"
+
+import logging
+import pyllamacpp.model
+from pygptj._logger import set_log_level
+
+
+class GPT4All(pyllamacpp.model.Model):
+    """
+    GPT4All model
+
+    Base: [pyllamacpp.model.Model](.#pyllamacpp.model.Model)
+
+    Example usage
+    ```python
+    from pygpt4all.models.gpt4all import GPT4All
+
+    def new_text_callback(text):
+        print(text, end="")
+
+    model = GPT4All('./models/ggml-gpt4all-j.bin')
+    model.generate("Once upon a time, ", n_predict=55, new_text_callback=new_text_callback)
+    ```
+    """
+
+    def __init__(self,
+                 model_path: str,
+                 n_ctx: int = 512,
+                 n_parts: int = -1,
+                 seed: int = 0,
+                 f16_kv: bool = False,
+                 logits_all: bool = False,
+                 vocab_only: bool = False,
+                 use_mlock: bool = False,
+                 embedding: bool = False,
+                 log_level: int = logging.INFO):
+        """
+        :param model_path: The path to a gpt4all `ggml` model
+        :param n_ctx: context size
+        :param n_parts:
+        :param seed: RNG seed, 0 for random
+        :param f16_kv: use fp16 for KV cache
+        :param logits_all: the llama_eval() call computes all logits, not just the last one
+        :param vocab_only: only load the vocabulary, no weights
+        :param use_mlock: force system to keep model in RAM
+        :param embedding: embedding mode only
+        :param log_level: logging level, set to INFO by default
+        """
+        # set logging level
+        set_log_level(log_level)
+        super(GPT4All, self).__init__(ggml_model=model_path,
+                                      n_ctx=n_ctx,
+                                      n_parts=n_parts,
+                                      seed=seed,
+                                      f16_kv=f16_kv,
+                                      logits_all=logits_all,
+                                      vocab_only=vocab_only,
+                                      use_mlock=use_mlock,
+                                      embedding=embedding,
+                                      log_level=log_level)
+
