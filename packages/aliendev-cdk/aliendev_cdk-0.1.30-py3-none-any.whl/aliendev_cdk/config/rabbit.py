fe95c3acd0c583c19ecdf2399448f7506193ea20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:15 +0000

    Add package aliendev-cdk/0.1.30/aliendev_cdk-0.1.30-py3-none-any.whl

---
+++ packages/aliendev-cdk/aliendev_cdk-0.1.30-py3-none-any.whl/aliendev_cdk/config/rabbit.py
@@ -0,0 +1,24 @@
+import pika
+import configparser
+import json
+
+def produce(queue:str, body:dict):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    username = 'guest'
+    password = 'guest'
+    host = '203.194.113.203'
+    port = '5672'
+    vhost = '/'
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(host, port, vhost, credentials)
+    
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue=queue)
+
+    channel.exchange_declare(exchange="aliendev", exchange_type="topic")
+    channel.queue_bind(queue=queue, exchange="aliendev",routing_key="aliendev-route")
+    channel.basic_publish(exchange="aliendev", routing_key="aliendev-route", body=json.dumps(body))
+    print(f"Sent message '{json.dumps(body)}'")
