983663b674aee65bcbcbb54b85674f1d483e8d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:18 +0000

    Add package drb-driver-image/1.2.0/drb-driver-image-1.2.0.tar.gz

---
+++ packages/drb-driver-image/drb-driver-image-1.2.0.tar.gz/drb-driver-image-1.2.0/drb/drivers/image/__init__.py
@@ -0,0 +1,14 @@
+from .base_node import DrbImageBaseNode, DrbImageSimpleValueNode, \
+    DrbImageFactory, DrbImageListNode
+from . import _version
+
+
+__version__ = _version.get_versions()['version']
+del _version
+
+__all__ = [
+    'DrbImageFactory',
+    'DrbImageBaseNode',
+    'DrbImageListNode',
+    'DrbImageSimpleValueNode',
+]
