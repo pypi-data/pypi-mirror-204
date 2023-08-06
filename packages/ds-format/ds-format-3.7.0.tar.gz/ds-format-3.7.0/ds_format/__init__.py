15ee54a8841a81151bf33d90577b1b94a67fba59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:27 +0000

    Add package ds-format/3.7.0/ds-format-3.7.0.tar.gz

---
+++ packages/ds-format/ds-format-3.7.0.tar.gz/ds-format-3.7.0/ds_format/__init__.py
@@ -0,0 +1,45 @@
+__version__ = '3.7.0'
+
+import os
+mode = os.environ.get('DS_MODE', 'soft')
+output = 'pst'
+
+from .misc import \
+	escape, \
+	unescape, \
+	with_mode
+from .drivers.netcdf import from_netcdf, to_netcdf
+from .op import \
+	attr, \
+	attrs, \
+	copy, \
+	dim, \
+	dims, \
+	find, \
+	findall, \
+	get_attrs, \
+	get_dims, \
+	get_meta, \
+	get_vars, \
+	group_by, \
+	merge, \
+	meta, \
+	rename, \
+	rename_attr, \
+	rename_attr_m, \
+	rename_dim, \
+	rename_dim_m, \
+	rename_m, \
+	require, \
+	rm, \
+	rm_attr, \
+	select, \
+	size, \
+	type_ as type, \
+	var, \
+	vars_ as vars, \
+	time_dt
+from .io import read, readdir, index, write
+from .validate import validate
+from . import cmd
+from . import misc
