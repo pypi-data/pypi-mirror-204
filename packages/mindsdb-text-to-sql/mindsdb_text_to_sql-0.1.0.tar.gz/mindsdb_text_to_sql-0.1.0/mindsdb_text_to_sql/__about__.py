8e55954d07ac2868ed143a0ba2a65b24d1af81e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:54 +0000

    Add package mindsdb-text-to-sql/0.1.0/mindsdb_text_to_sql-0.1.0.tar.gz

---
+++ packages/mindsdb-text-to-sql/mindsdb_text_to_sql-0.1.0.tar.gz/mindsdb_text_to_sql-0.1.0/mindsdb_text_to_sql/__about__.py
@@ -0,0 +1,10 @@
+__title__ = 'mindsdb_text_to_sql'
+__package_name__ = 'mindsdb_text_to_sql'
+__version__ = '0.1.0'
+__description__ = "MindsDB Text to SQL, converts natural language to SQL code."
+__email__ = "minurapunchihewa17@gmail.com"
+__author__ = 'Minura Punchihewa'
+__github__ = 'https://github.com/MinuraPunchihewa/mindsdb-text-to-sql'
+__pypi__ = 'https://pypi.org/project/mindsdb-text-to-sql/'
+__license__ = None
+__copyright__ = None
