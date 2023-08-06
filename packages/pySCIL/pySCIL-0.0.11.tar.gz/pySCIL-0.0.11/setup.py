e16437839296c636b54caabe412e9f558c738042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:17 +0000

    Add package pySCIL/0.0.11/pySCIL-0.0.11.tar.gz

---
+++ packages/pySCIL/pySCIL-0.0.11.tar.gz/pySCIL-0.0.11/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pySCIL",
+    version="0.0.11",
+    author="Mara Schwartz, Esteban Castillo Juarez, Ivan Leon",
+    author_email="maraschwartz@gmail.com, castie2@rpi.edu, leoni@rpi.edu",
+    description="A sociolinguistics package for NLP research.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.rpi.edu/LACAI/pySCIL",
+    project_urls={
+        "Documentation": "https://github.rpi.edu/LACAI/pySCIL",
+        "Source Code": "https://github.rpi.edu/LACAI/pySCIL",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False,
+)
