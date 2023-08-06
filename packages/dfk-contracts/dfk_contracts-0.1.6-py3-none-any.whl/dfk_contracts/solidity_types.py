c5725f6e66efd2dbfdf81fd2bfc43d3c0e6666f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package dfk-contracts/0.1.6/dfk_contracts-0.1.6-py3-none-any.whl

---
+++ packages/dfk-contracts/dfk_contracts-0.1.6-py3-none-any.whl/dfk_contracts/solidity_types.py
@@ -0,0 +1,26 @@
+
+from typing import Sequence, Dict, Tuple, List
+from web3.datastructures import AttributeDict
+from web3.types import TxReceipt, BlockIdentifier
+from eth_typing.evm import HexAddress
+
+# We lose some detail here; Python doesnt have signed/unsigned int differentions
+# or bytes sizes. For now, these type aliase let us use Solidity's native types 
+# as type hints while being a little less particular to Pythons Mypy type analyzer
+address = HexAddress
+bool = bool
+string = str
+uint8 = int
+uint16 = int
+uint32 = int
+uint64 = int
+uint128 = int
+uint256 = int
+int64 = int
+int256 = int
+bytes4 = bytes
+bytes32 = bytes
+# These show up in some ABIs, but there's some extra work we'd need to do
+# to use them in Python since they're already reserved words
+# bytes = bytes
+# tuple = Sequence
