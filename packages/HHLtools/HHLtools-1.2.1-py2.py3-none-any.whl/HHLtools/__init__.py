e09749ccc951372f1443b6fa1bde267bfde44998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:05 +0000

    Add package HHLtools/1.2.1/HHLtools-1.2.1-py2.py3-none-any.whl

---
+++ packages/HHLtools/HHLtools-1.2.1-py2.py3-none-any.whl/HHLtools/__init__.py
@@ -0,0 +1,9 @@
+"""
+Noting here, but you can import my sub-packages
+
+ADT: support some abstract data types
+CN: support some Chinese keywords
+Herror: some of my custom errors
+Hmath: some math functions
+prints: support print with different styles
+"""
