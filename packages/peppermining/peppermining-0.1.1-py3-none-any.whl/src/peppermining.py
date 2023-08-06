c8dafaf8ab651f09e43ed4e7d8ad0359578b4515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:17:11 +0000

    Add package peppermining/0.1.1/peppermining-0.1.1-py3-none-any.whl

---
+++ packages/peppermining/peppermining-0.1.1-py3-none-any.whl/src/peppermining.py
@@ -0,0 +1,11 @@
+class PepperMining:
+    
+    def __init__(self):
+        """
+        """
+
+    def set_event_data(self, _event_data):
+        self.event_data = _event_data
+
+    def get_event_data(self):
+        return self.event_data
