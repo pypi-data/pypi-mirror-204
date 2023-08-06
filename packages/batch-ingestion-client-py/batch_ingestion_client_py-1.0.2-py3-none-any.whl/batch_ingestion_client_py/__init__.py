fb3c07f68a2ec6a7be87d25de77892237ced684d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:14 +0000

    Add package batch-ingestion-client-py/1.0.2/batch_ingestion_client_py-1.0.2-py3-none-any.whl

---
+++ packages/batch-ingestion-client-py/batch_ingestion_client_py-1.0.2-py3-none-any.whl/batch_ingestion_client_py/__init__.py
@@ -0,0 +1,13 @@
+from batch_ingestion_client_py.main import BatchIngestor
+from batch_ingestion_client_py.data import (
+    Data,
+    Entity,
+    Claim,
+    MainSnak,
+    DataValue,
+    ValueInLanguage
+)
+from batch_ingestion_client_py.response import (
+    Response,
+    Revision,
+)
