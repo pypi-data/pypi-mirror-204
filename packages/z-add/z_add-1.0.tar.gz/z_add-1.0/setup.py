29c244822e90f70ecc1286546b5c213b00e28e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:32 +0000

    Add package z-add/1.0/z_add-1.0.tar.gz

---
+++ packages/z-add/z_add-1.0.tar.gz/z_add-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+packages = ['z_add']# 唯一的包名，自己取名
+setup(name='z_add',
+	version='1.0',
+	author='ztd',
+    packages=packages,
+    package_dir={'requests': 'requests'},)
