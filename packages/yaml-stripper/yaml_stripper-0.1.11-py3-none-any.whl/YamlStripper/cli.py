58a48c4872f8f3528272f71619f4ecfa50f0b0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:46 +0000

    Add package yaml-stripper/0.1.11/yaml_stripper-0.1.11-py3-none-any.whl

---
+++ packages/yaml-stripper/yaml_stripper-0.1.11-py3-none-any.whl/YamlStripper/cli.py
@@ -0,0 +1,30 @@
+import click
+from YamlStripper import yaml_stripper
+
+
+@click.command()
+@click.argument("input_file", type=click.File("r"))
+@click.argument("output_file", type=click.File("w"))
+@click.option(
+    "--fields", "-f", multiple=True, help="Fields to remove from the YAML file."
+)
+def main(input_file, output_file, fields):
+    """
+    Strip specified fields from a YAML file.
+
+    INPUT_FILE: The YAML file to read.
+    OUTPUT_FILE: The file to write the stripped YAML to.
+    FIELDS: Fields to remove from the YAML file.
+    """
+    # Load the input YAML file
+    yaml_data = YamlStripper.load_yaml(input_file)
+
+    # Remove specified fields from the YAML data
+    stripped_yaml = YamlStripper.strip_fields(yaml_data, fields)
+
+    # Write the stripped YAML to the output file
+    YamlStripper.dump_yaml(stripped_yaml, output_file)
+
+
+if __name__ == "__main__":
+    main()
