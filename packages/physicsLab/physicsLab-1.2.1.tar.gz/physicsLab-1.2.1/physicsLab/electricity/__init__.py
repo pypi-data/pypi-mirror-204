d5ac78816ed3e56c7c194b495dc515718120d4f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:04 +0000

    Add package physicsLab/1.2.1/physicsLab-1.2.1.tar.gz

---
+++ packages/physicsLab/physicsLab-1.2.1.tar.gz/physicsLab-1.2.1/physicsLab/electricity/__init__.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+# 操作元件
+from physicsLab.electricity.element import *
+# 操作导线
+from physicsLab.electricity.wire import *
+# 元件类
+from physicsLab.electricity.elementsClass import *
+# 元件引脚类
+from physicsLab.electricity.elementPin import *
+# 元件坐标系
+from physicsLab.electricity.elementXYZ import *
