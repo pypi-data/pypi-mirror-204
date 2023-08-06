94d916671b3e0ce934bfae336c2edae0ae0735fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:06 +0000

    Add package dedscumulus/0.0.1/dedscumulus-0.0.1.tar.gz

---
+++ packages/dedscumulus/dedscumulus-0.0.1.tar.gz/dedscumulus-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='dedscumulus',  
+     version='0.0.1',
+     author="BayWa r.e. Data Services GmbH",
+     author_email="no-reply@baywa-re.com",
+     description="Kernel functions for Cumulus",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/nino-baywa/dedscumulus",
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+    package_dir = {"src": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8"
+ )
