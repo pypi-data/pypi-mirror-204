2ec8368fed75315c71f87c343fa3367addc11d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:20 +0000

    Add package robyn/0.28.4/robyn-0.28.4-cp38-none-win_amd64.whl

---
+++ packages/robyn/robyn-0.28.4-cp38-none-win_amd64.whl/robyn/__main__.py
@@ -0,0 +1,50 @@
+import os
+import webbrowser
+
+from .argument_parser import Config
+
+
+def create():
+    project_dir = input("Enter the name of the project directory: ")
+    # Initailize a new Robyn project
+
+    print(f"Creating a new Robyn project '{project_dir}'...")
+
+    # Create a new directory for the project
+    os.makedirs(project_dir, exist_ok=True)
+
+    # Create the main application file
+    app_file_path = os.path.join(project_dir, "app.py")
+    with open(app_file_path, "w") as f:
+        f.write(
+            """
+from robyn import Robyn
+
+app = Robyn(__file__)
+
+@app.get("/")
+def index():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
+
+            """
+        )
+
+    print(f"New Robyn project created in '{project_dir}' ")
+
+
+def docs():
+    print("Opening Robyn documentation... | Offline docs coming soon!")
+    webbrowser.open("https://sansyrox.github.io/robyn/#/")
+
+
+if __name__ == "__main__":
+    config = Config()
+    if config.create:
+        create()
+
+    if config.docs:
+        docs()
