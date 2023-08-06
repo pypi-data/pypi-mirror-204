72493ba9fd181adb561880cf5f9af97e514268c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:10 +0000

    Add package chesscomwrapper/0.0.6/chesscomwrapper-0.0.6.tar.gz

---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.6.tar.gz/chesscomwrapper-0.0.6/README.md
@@ -0,0 +1,2 @@
+## chesscomwrapper 
+Looong documentation **about** this chess.com wrapper. It's a bit outdated, but still useful.
---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.6.tar.gz/chesscomwrapper-0.0.6/app/chesscomwrapper/src/teammatchboard.py
@@ -0,0 +1,11 @@
+from .handlers.chesscomhandlers.teammatchboardhandler import TeamMatchBoardHandler
+
+
+class TeamMatchBoard(object):
+    def __init__(self, boardUrl) -> None:
+        self.boardUrl = boardUrl
+
+    def getInfo(self):
+        self.info = TeamMatchBoardHandler().getInfo(self.boardUrl)
+
+
