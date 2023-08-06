bb47fe4ae2f68fb376dc5123ad58068932f0433e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:05 +0000

    Add package AMAS-sb/0.0.4/AMAS-sb-0.0.4.tar.gz

---
+++ packages/AMAS-sb/AMAS-sb-0.0.4.tar.gz/AMAS-sb-0.0.4/AMAS/reaction_annotation.py
@@ -0,0 +1,242 @@
+# reaction_annotation.py
+"""
+<Annotation for Reaction>
+reaction_annotation creates and predicts
+annotation of libsbml reactions,
+using existing  species annotations or,
+annotations predicted via species_annotation.
+"""
+
+from AMAS import constants as cn
+from AMAS import tools
+
+import compress_pickle
+import libsbml
+import numpy as np
+import operator
+import os
+import pandas as pd
+import pickle
+
+
+with open(os.path.join(cn.REF_DIR, 'data2ref_mat.lzma'), 'rb') as handle:
+  REF_DAT = compress_pickle.load(handle)
+
+# first of list is list of columns
+cols = REF_DAT[0]
+# second, list of indices
+inds = REF_DAT[1]
+# third, list of index (column, [non-zero rows])
+ref_mat_pairs = REF_DAT[2]
+REF_MAT = pd.DataFrame(0, index=inds, columns=cols)
+for val in ref_mat_pairs:
+  REF_MAT.iloc[val[1], val[0]] = 1
+
+REACTION_RF = compress_pickle.load(os.path.join(cn.REF_DIR, 'reactions_rf_fitted.lzma'))
+
+class ReactionAnnotation(object):
+
+  def __init__(self, libsbml_fpath=None, 
+               inp_tuple=None):
+  
+    """
+    Parameters
+    ----------
+    libsbml_fpath: str
+        File path of an SBMl model (.xml)
+    inp_tuple: tuple
+        ({reaction_id: [unique components (that is, species) of that reaction]},
+         {reaction_id: Rhea terms})
+    """
+    # self.exist_annotation stores 
+    # existing KEGG Reaction or Rhea annotations in the model.
+    # If none exists, set None.
+    if libsbml_fpath is not None:
+      reader = libsbml.SBMLReader()
+      document = reader.readSBML(libsbml_fpath)
+      self.model = document.getModel()
+      self.exist_annotation = tools.extractExistingReactionAnnotation(inp_model=self.model)
+      self.reaction_components = {val.getId():list(set([k.species for k in val.getListOfReactants()]+\
+                                                       [k.species for k in val.getListOfProducts()])) \
+                                  for val in self.model.getListOfReactions()}
+    elif inp_tuple is not None:
+      self.model = None
+      self.reaction_components = inp_tuple[0]
+      self.exist_annotation = inp_tuple[1]
+    else:
+      self.model = None
+      self.reaction_components = None
+      self.exist_annotation = None
+    # Attributes after prediction
+    self.candidates = None
+    self.query_df = None
+
+  def getReactionComponents(self,
+                            inp_reaction):
+    """
+    Get component of reactions in species IDs
+    (both reactants and products)
+    of a reaction. 
+
+    Parameters
+    ----------
+    reaction_id: str/libsbml.Reaction
+
+
+    Returns
+    -------
+    None/str-list (list of species IDs)
+    """
+    if isinstance(inp_reaction, libsbml.Reaction):
+      one_reaction = inp_reaction
+    elif isinstance(inp_reaction, str):
+      one_reaction = self.model.getReaction(inp_reaction)
+    else:
+      return None
+    reactants = [val.species for val in one_reaction.getListOfReactants()]
+    products = [val.species for val in one_reaction.getListOfProducts()]
+    r_components = list(set(reactants + products))
+    return r_components
+
+  def predictAnnotation(self,
+                        inp_spec_dict,
+                        inp_reac_list=None,
+                        inp_ref_mat=REF_MAT,
+                        update=False):
+    """
+    Predict 1) reaction annotation candidates 
+    and 2) match score of them
+    using species dict (argument) etc.
+    Also updates several class attributes as a result. 
+  
+    Parameters
+    ----------
+    dict: inp_spec_dict
+        Dictionoary, {species id: formula(str-list)}
+    inp_reac_list: str-list
+        IDs of reactions to predict. If default, will do all reactions
+    inp_ref_mat: pd.DataFrame
+        Reference matrix
+    update: bool
+        Whether to save results as class attrributes or just return them.
+        If True, only match_score is returned (and values are updated).
+        If False, all relevant information is returned
+
+    Returns
+    -------
+    : dict
+        {'candidates': {reactionID: [candidates in RHEA]},
+         'match_score': {reactionID: [(Rhea ID, match score: float between 0.0-1.0),]}
+         'query_df': query_df}
+    """
+    # get libsbml.reaction and their IDs
+    if inp_reac_list is not None:
+      reaction_ids = inp_reac_list
+    else:
+      reaction_ids = list(self.reaction_components.keys())
+    # get dictionary of reaction ID: species component
+    r2pred_spec_formulas = dict()
+    for one_rid in reaction_ids:
+      r2pred_spec_formulas[one_rid] = {one_spec:inp_spec_dict[one_spec] \
+                                       for one_spec in self.reaction_components[one_rid]}
+    # prepare query df for prediction
+    query_df = pd.DataFrame(0, 
+                            index=inp_ref_mat.columns,
+                            columns=reaction_ids)
+    for one_rid in reaction_ids:
+      one_set_species = r2pred_spec_formulas[one_rid]
+      # for each species element of the select reaction
+      for one_spec_key in one_set_species.keys():
+        one_spec = one_set_species[one_spec_key]
+        # For each one_rid, set the values 1.0
+        query_df.loc[[val for val in one_spec if val in query_df.index], one_rid] = 1
+    multi_mat = inp_ref_mat.dot(query_df)
+    maxes = multi_mat.max()
+    #
+    # Collect candidates and calculate confidence score
+    pred_cands = dict()
+    pred_match_score = dict()
+    for one_rid in maxes.index:
+      one_multi = multi_mat.loc[:,one_rid]
+      candidates = one_multi[one_multi==maxes[one_rid]].index
+      # cand_data; (number of element matches, candidates)
+      pred_cands[one_rid] = candidates
+      # Now, match_scpre (calculated per each candidate) => replaced as a tuple
+      match_score_per_cand = []
+      for one_cand in candidates:
+        if one_cand in cn.REF_RHEA2MASTER.keys():
+          num_matches = maxes[one_rid]
+          num_maxpos_matches = len(inp_ref_mat.loc[one_cand, :].to_numpy().nonzero()[0])
+          match_score_per_cand.append((one_cand, np.round(num_matches/num_maxpos_matches, cn.ROUND_DIGITS)))
+      match_score_per_cand.sort(key=operator.itemgetter(1), reverse=True)
+      pred_match_score[one_rid] = match_score_per_cand
+    if update:
+      self.candidates = pred_match_score
+      self.query_df = query_df
+    #
+    return {'candidates': pred_cands,
+            'match_score': pred_match_score,
+            'query_df': query_df}
+
+
+  # Develop a method to evaluate results using fitted model
+  def evaluatePredictedReactionAnnotation(self, pred_result,
+                                          fitted_model=REACTION_RF):
+    """
+    Evaluate the quality of annotation;
+    for each individual species.
+    pred_result includes predicted results,
+    a result of self.predictAnnotation().
+    All informaton needed for prediction 
+    is supposed to come from inp_dict, 
+    not the stored reaction class itself.
+  
+    Parameters
+    ---------
+    pred_result: dict
+        {'candidates': {reactionID: [candidates in RHEA]},
+         'match_score': {reactionID: [(Rhea ID, match score: float between 0.0-1.0),]}
+         'query_df': query_df}
+
+    Returns
+    -------  
+    dict {reaction_id: probability-of-prediction-including-correct-value}
+        Information of how algorithm is confident about the result
+    """
+    candidates_dict = pred_result[cn.CANDIDATES]
+    match_score_dict = pred_result[cn.MATCH_SCORE]
+    mean_rheas_num_dict = {one_k: np.mean([self.getRheaElementNum(val) \
+                                           for val in candidates_dict[one_k]]) \
+                           for one_k in candidates_dict.keys()}
+    num_reac_comp_dict = {one_k: len(self.reaction_components[one_k]) \
+                          for one_k in candidates_dict.keys()}
+    num_candidates = {one_k: len(candidates_dict[one_k]) \
+                          for one_k in candidates_dict.keys()}
+    mean_match_scores = {one_k: np.mean([val[1] for val in match_score_dict[one_k]]) \
+                          for one_k in candidates_dict.keys()}
+    df2pred = pd.DataFrame([mean_rheas_num_dict,
+                            num_reac_comp_dict,
+                            num_candidates,
+                            mean_match_scores]).T
+    cred_pred = fitted_model.predict_proba(df2pred)
+    prob_1_dict = {val: cred_pred[idx][1] for idx, val in enumerate(df2pred.index)}
+    return prob_1_dict
+
+  def getRheaElementNum(self,
+                        inp_rhea,
+                        inp_df=REF_MAT):
+    """
+    Get Number of elements of
+    the given rhea term.
+    
+    Parameters
+    ----------
+    inp_rhea: str
+    
+    Returns
+    -------
+    : int
+    """
+    return len(inp_df.loc[inp_rhea, :].to_numpy().nonzero()[0])
+
+
---
+++ packages/AMAS-sb/AMAS-sb-0.0.4.tar.gz/AMAS-sb-0.0.4/AMAS/species_annotation.py
@@ -0,0 +1,358 @@
+# species_annotation.py
+"""
+<Annotation for Species>
+species_annotation creates and predicts
+annotation of libsbml species,
+mainly using editdistance method.
+(can be changed in future)
+"""
+
+
+from AMAS import constants as cn
+from AMAS import tools
+
+import collections
+import compress_pickle
+import editdistance
+import itertools
+import libsbml
+import numpy as np
+import operator
+import os
+import pandas as pd
+import pickle
+import re
+import warnings
+
+
+with open(os.path.join(cn.REF_DIR, 'chebi_low_synonyms_comp.lzma'), 'rb') as f:
+  CHEBI_LOW_SYNONYMS = compress_pickle.load(f)
+CHARCOUNT_COMB_DF = compress_pickle.load(os.path.join(cn.REF_DIR, 'charcount_df_scaled.lzma'),
+                                         compression="lzma")
+CHARCOUNT_DF = CHARCOUNT_COMB_DF.iloc[:, :-2]
+CHEBI_DF = CHARCOUNT_COMB_DF.iloc[:, -2:]
+# A trained random forest model 
+# suppress warning to ignore warnings about new sklearn versions
+warnings.filterwarnings("ignore")
+SPECIES_RF = compress_pickle.load(os.path.join(cn.REF_DIR, 'species_rf_fitted.lzma'))
+
+
+class SpeciesAnnotation(object):
+
+  def __init__(self, libsbml_fpath=None,
+               inp_tuple=None):
+
+    """
+    Parameters
+    ----------
+    libsbml_fpath: str
+        File path of an SBMl (.xml) model
+
+    inp_tuple: tuple 
+        Tuple of model information,
+        first element (index 0) is information on 
+        species names,
+        second element (index 1) is existing 
+        ChEBI information.
+        ({species_id: species_display_name},
+         {species_id: ChEBI terms})
+    """
+
+    # self.exist_annotation stores existing CHEBI annotations in the model
+    # If none exists, set None
+    if libsbml_fpath is not None:
+      reader = libsbml.SBMLReader()
+      document = reader.readSBML(libsbml_fpath)
+      self.model = document.getModel()
+      self.names = {val.getId():val.name for val in self.model.getListOfSpecies()}
+      self.exist_annotation = tools.extractExistingSpeciesAnnotation(self.model)
+      exist_annotation_formula_raw = {k:tools.transformCHEBIToFormula(self.exist_annotation[k], cn.REF_CHEBI2FORMULA) \
+                                      for k in self.exist_annotation.keys()}
+      self.exist_annotation_formula = {val:exist_annotation_formula_raw[val] for val in exist_annotation_formula_raw.keys() \
+                                       if exist_annotation_formula_raw[val]}
+
+    # inp_tuple: ({species_id:species_name}, {species_id: [CHEBI annotations]})
+    elif inp_tuple is not None:
+      self.model = None
+      self.names = inp_tuple[0]
+      self.exist_annotation = inp_tuple[1]
+      exist_annotation_formula_raw = {k:tools.transformCHEBIToFormula(inp_tuple[1][k], cn.REF_CHEBI2FORMULA) \
+                                      for k in inp_tuple[1].keys()}
+      self.exist_annotation_formula = {val:exist_annotation_formula_raw[val] for val in exist_annotation_formula_raw.keys() \
+                                       if exist_annotation_formula_raw[val]}
+    else:
+      self.model = None
+      self.names = None
+      self.exist_annotation = None
+      self.exist_annotation_formula = None
+    # Below are predicted annotations in dictionary format
+    # Once created, each will be {species_ID: float/str-list}
+    self.candidates = dict()
+    self.formula = dict()
+      
+  def predictAnnotationByEditDistance(self, inp_str):
+    """
+    Predict annotation using the argument string 
+    and Levenshtein edit distance method. 
+
+    Parameters
+    ----------
+    inp_str: str
+        String to predict CHEBI annotation
+
+    Returns
+    -------
+    dict
+        {'name_used': str,
+         'chebi': [list-ChEBI],
+         'match_score': [(ChEBI, float)],
+         'formula': [list-formula]} 
+    """
+    one_result = dict()
+    # For now, choose the terms that are included in the CHEBI-formula mapping reference
+    dist_dict_min = {one_k:np.min([editdistance.eval(inp_str.lower(), val) for val in CHEBI_LOW_SYNONYMS[one_k]]) \
+                     for one_k in CHEBI_LOW_SYNONYMS.keys() if one_k in cn.REF_CHEBI2FORMULA.keys()}
+    min_min_dist = np.min([dist_dict_min[val] for val in dist_dict_min.keys()])
+    min_min_chebis = [one_k for one_k in dist_dict_min.keys() \
+                      if dist_dict_min[one_k]==min_min_dist and one_k in cn.REF_CHEBI2FORMULA.keys()]
+    # Results are sorted based on match_score (average of 1 - (editdistance/len_synonyms)
+    res_tuple = [(one_chebi,
+                  np.round(np.max([1.0-editdistance.eval(inp_str.lower(), val)/len(val) \
+                                    for val in CHEBI_LOW_SYNONYMS[one_chebi]]), cn.ROUND_DIGITS)) \
+                 for one_chebi in min_min_chebis] 
+    res_tuple.sort(key=operator.itemgetter(1), reverse=True)
+    #  CHEBI part is added, because we want a sorted list after computing res_tuple
+    one_result[cn.NAME_USED] = inp_str
+    one_result[cn.CHEBI] = [val[0] for val in res_tuple]
+    one_result[cn.MATCH_SCORE] = res_tuple
+    min_min_formula = list(set([cn.REF_CHEBI2FORMULA[val] for val in min_min_chebis]))
+    one_result[cn.FORMULA] = min_min_formula
+    return one_result
+
+  # Methods to use Cosine Similarity
+  def getCountOfIndividualCharacters(self, inp_str):
+    """
+    Get a list of characters
+    between a-z and 0-9. 
+  
+    Parameters
+    ----------
+    inp_str: str
+  
+    Returns
+    -------
+    : list
+    """
+    return collections.Counter(itertools.chain(*re.findall('[a-z0-9]+', inp_str.lower())))
+
+  def prepareCounterQuery(self,
+                          specs,
+                          ref_cols,
+                          use_id=True):
+    """
+    Prepare a query vector, which will be used  
+    as a vector for predictor variables.
+    Input will be a list of
+    IDs using which names_used will be determined. 
+    In addition, querys will also be scaled
+    by the length of each vector. 
+  
+    There is 'use_id' option, so
+    if False, directly use the string
+    instead of searching for used_name. 
+  
+    Parameters
+    ----------
+    list-str: specs
+        IDs of species
+    ref_cols: list-str
+        Column names to use
+    use_id: bool
+        If False, directly use the string
+        If True, use getNameToUse
+      
+    Returns
+    -------
+    : pandas.DataFrame
+    : dict
+    """
+    name_used = dict()
+    query_mat = pd.DataFrame(0, index=ref_cols, columns=specs)
+    for one_spec in specs:
+      if use_id:
+        name2use = self.getNameToUse(one_spec)
+        # characters are lowered in getCountOfIndividualCharacters()
+        char_counts = self.getCountOfIndividualCharacters(name2use)
+        name_used[one_spec] = name2use
+      else:
+        name2use = one_spec
+        # characters are lowered in getCountOfIndividualCharacters()
+        char_counts = self.getCountOfIndividualCharacters(name2use)
+        name_used[one_spec] = name2use
+      for one_char in char_counts:
+        query_mat.loc[one_char, one_spec] = char_counts[one_char] 
+    # Now, scale it using the vector distance
+    div_row = query_mat.apply(lambda col : np.sqrt(np.sum([val**2 for val in col])), axis = 0)
+    norm_query = query_mat.divide(div_row, axis=1)
+    return norm_query, name_used
+
+  def predictAnnotationByCosineSimilarity(self,
+                                          inp_strs=None,
+                                          inp_ids=None,
+                                          ref_df=CHARCOUNT_DF,
+                                          chebi_df=CHEBI_DF):
+    """
+    Predict annotation by taking cosine distance 
+    of character count vectors.
+  
+    Parameters
+    ----------
+    inp_strs: list-str
+        Strings that will directly used
+        for prediction
+    inp_ids: list-str
+        IDs with which name2use will be
+        determined
+    ref_df: DataFrame
+        Reference database
+    chebi_df: DataFrame
+        ChEBI information sharing the index with ref_df    
+
+    Returns
+    -------
+    : dict/None
+        {'name_used': str,
+         'chebi': [list-ChEBI],
+         'match_score': [(ChEBI, float)],
+         'formula': [list-formula]} 
+      if no name/ID is given, return None
+    """
+    if inp_ids:
+      one_query, name_used = self.prepareCounterQuery(specs=inp_ids,
+                                                      ref_cols=ref_df.columns,
+                                                      use_id=True)
+    elif inp_strs:
+      one_query, name_used = self.prepareCounterQuery(specs=inp_strs,
+                                                      ref_cols=ref_df.columns,
+                                                      use_id=False)  
+    else:
+      return None
+    multi_mat = ref_df.dot(one_query)
+    max_val = multi_mat.max()
+    result = dict()
+    for one_spec in one_query.columns:
+      one_res = dict()
+      one_res[cn.NAME_USED] = name_used[one_spec]
+      cand_index = multi_mat[abs(multi_mat[one_spec]-max_val[one_spec])<cn.TOLERANCE].index
+      one_res[cn.CHEBI] = list(set(chebi_df.loc[cand_index, cn.CHEBI]))
+      one_res[cn.MATCH_SCORE] = [(val, np.round(max_val[one_spec], cn.ROUND_DIGITS)) \
+                                 for val in one_res[cn.CHEBI]]
+      one_res[cn.FORMULA] = list(set([cn.REF_CHEBI2FORMULA[val] for val in one_res[cn.CHEBI] \
+                                      if val in cn.REF_CHEBI2FORMULA.keys()])) 
+      result[one_spec] = one_res
+    return result
+
+  def getNameToUse(self, inp_id):
+    """
+    Get name to use;
+    If .name is not '', use it;
+    otherwise use ID
+  
+    Parameters
+    ----------
+    inp_id: ID of model element
+  
+    Returns
+    -------
+    str
+    """
+    species_name = self.names[inp_id]
+    if len(species_name) > 0:
+      res_name = species_name
+    else:
+      res_name = inp_id
+    return res_name
+
+  def evaluatePredictedSpeciesAnnotation(self,
+                                         pred_result=None,
+                                         fitted_model=SPECIES_RF):
+    """
+    Predict the probability of 
+    the candidate set including the 'true'
+    annotation. 
+    
+    Parameters
+    ---------
+    pred_result: dict 
+        Result of prediction of one species.
+        {'name_used':str, 'chebi':[str-chebi],
+         'match_score': [chebi_tuples],
+         'formula': [str-formula]}
+
+
+    Returns
+    -------
+    : float
+    """
+    name_length = len(pred_result[cn.NAME_USED])
+    num_candidates = len(pred_result[cn.CHEBI])
+    match_score = np.mean([val[1] for val in pred_result[cn.MATCH_SCORE]])
+    num_formulas = len(pred_result[cn.FORMULA])
+    proba_correct = fitted_model.predict_proba([[name_length,
+                                                 num_candidates,
+                                                 match_score,
+                                                 num_formulas]])[0][1]
+    return proba_correct
+
+
+  def updateSpeciesWithRecommendation(self, inp_recom):
+    """
+    Update species_annotation class using
+    Recommendation namedtuple.
+  
+    self.candidates is a sorted list of tuples,
+    (chebi_id: match_score)
+    self.formula is a unsorted list of unique formulas
+  
+    Parameters
+    ----------
+    inp_recom: Recommendation
+       A namedtuple. Created by recom.getSpeciesAnnotation
+  
+    Returns
+    -------
+    None
+    """
+    self.candidates.update({inp_recom.id: inp_recom.candidates})
+    formulas2update = list(set([cn.REF_CHEBI2FORMULA[val[0]] \
+                                for val in inp_recom.candidates \
+                                if val[0] in cn.REF_CHEBI2FORMULA.keys()]))
+    self.formula.update({inp_recom.id: formulas2update})
+    return None
+
+  def updateSpeciesWithDict(self, inp_dict):
+    """
+    A direct way of updating species annotations,
+    using ChEBI terms.
+    As match scores are given
+    when exact matches are found, 
+    match scores were given as 1.0. 
+  
+    Parameters
+    ----------
+    inp_dict: dict
+        {species_id: [chebi terms]}
+  
+    Returns
+    -------
+    None
+    """
+    info2upd_candidates = {k:[(val, 1.0) for val in inp_dict[k]] for k in inp_dict.keys()}
+    info2upd_formula = {k:[cn.REF_CHEBI2FORMULA[chebi] for chebi in inp_dict[k]] for k in inp_dict.keys()}
+    self.candidates.update(info2upd_candidates)
+    self.formula.update(info2upd_formula)
+
+
+
+
+
