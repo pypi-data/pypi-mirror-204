7df500d7adeb3a8101ac9e42aaf67519251b6fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:52 +0000

    Add package cmdgpt/0.4.1/cmdgpt-0.4.1.tar.gz

---
+++ packages/cmdgpt/cmdgpt-0.4.1.tar.gz/cmdgpt-0.4.1/README.md
@@ -0,0 +1,85 @@
+<div align="right">
+  <!-- 语言: -->
+  简体中文 | <a title="English" href="README-en.md">English</a>
+</div>
+
+<img height="128" align="left" src="https://user-images.githubusercontent.com/70903329/228310136-b27cbe83-e7e1-4560-ba75-1b418ba9e5a0.png" alt="Logo">
+
+# CMDGPT | 用自然语言操作命令行
+
+[![Release](https://github.com/MZhao-ouo/CMDGPT/actions/workflows/release.yml/badge.svg)](https://github.com/MZhao-ouo/CMDGPT/actions/workflows/release.yml) 
+[![Version](https://img.shields.io/pypi/v/cmdgpt?label=Release%20Version)](https://pypi.org/project/cmdgpt/) 
+[![Pypi](https://img.shields.io/pypi/dm/cmdgpt?logo=pypi)](https://pypi.org/project/cmdgpt/) 
+
+![cmdgpt演示-_online-video-cutter com_](https://user-images.githubusercontent.com/70903329/227725280-2d22322e-accd-4371-8f1b-51a698566e64.gif)
+
+## 安装
+
+```sh
+pip install cmdgpt
+```
+
+**如果遇到报错 `command not found: cmdgpt`**
+
+请将以下内容添加到您的`/.bashrc`或`/.zshrc`文件中：
+```sh
+export PATH=$PATH:~/.local/bin
+```
+然后运行`source ~/.bashrc`或`source ~/.zshrc`重新加载文件。
+
+## 用法
+
+**设置 OpenAI api-key:**
+```sh
+cmdgpt --key "sk-xxx……xxx"
+```
+
+**自然语言交互**
+```sh
+cmdgpt "创建一个名为www的文件夹，里面有一个index.html文件"
+```
+
+**Chat模式**
+```sh
+cmdgpt --chat
+```
+
+**指定api_host**
+
+你只需要填host即可，例如：
+```sh
+cmdgpt --api_host "api.openai.com"
+```
+
+## 已知问题
+- 无法使用`cd`和`source`等命令。
+
+## 本地调试
+1. Clone本仓库
+```sh
+git clone https://github.com/MZhao-ouo/CMDGPT.git
+cd CMDGPT
+```
+
+2. 修改代码，代码位于cmdgpt文件夹
+3. 运行:
+
+先卸载旧版本
+```sh
+pip uninstall cmdgpt -y
+```
+
+生成python模块
+```sh
+python setup.py sdist bdist_wheel
+```
+
+安装本地python模块（注意版本号要与./cmdgpt/VERSION一致，且不小于当前版本）
+```sh
+pip install dist/cmdgpt-0.2.3-py3-none-any.whl
+```
+
+或者你可以一键执行（注意版本号要与./cmdgpt/VERSION一致，且不小于当前版本）
+```sh
+pip uninstall cmdgpt -y | python setup.py sdist bdist_wheel && pip install dist/cmdgpt-0.2.3-py3-none-any.whl
+```
---
+++ packages/cmdgpt/cmdgpt-0.4.1.tar.gz/cmdgpt-0.4.1/cmdgpt/functions.py
@@ -0,0 +1,127 @@
+from .openai_func import *
+from .utils import *
+import datetime
+import subprocess
+
+messages = [
+    {"role": "system", "content": exec_prompt},
+    {"role": "user", "content": prepare_prompt()}
+]
+    
+# execute query
+def exec_query(query):
+    messages.append({"role": "user", "content": query})
+    response = get_chat_response(messages, temperature=0,apiurl=COMPLETIONS_URL)
+    response_contents = decode_chat_response(response)
+    try_exec(response_contents)
+    
+def explain(command):
+    messages.append({"role": "assistant", "content": command})
+    messages.append({"role": "user", "content": "PEC2MZHAO"})
+    response = get_chat_response(messages, temperature=0,apiurl=COMPLETIONS_URL)
+    response_contents = decode_chat_response(response)
+    print(full_column_str("\rExplanation"))
+    for chunk in response_contents:
+        print(chunk, end="", flush=True)
+    print()
+
+def try_exec(response_contents):
+    cmd = ""
+    print(f"{Fore.BLUE}CMDGPT:{Style.RESET_ALL} ")
+    print("> Run(R), Explain(E), Cancel(C)", end="", flush=True)
+    print("\x1b[1A\r\x1b[8C", end="")
+    for chunk in response_contents:
+        print(Fore.YELLOW + chunk + Style.RESET_ALL, end="", flush=True)
+        cmd += chunk
+    print("\x1b[1B\r\x1b[32C", end="")
+    if "MZHAO" in cmd:
+        print("\r" + Fore.RED + "Please provide a valuable description." + Style.RESET_ALL)
+        return
+    if "\n" in cmd:
+        print("\r" + "It is a multi-line command. Please execute it manually.")
+        return
+    while True:
+        pressed_key = get_key()
+        if pressed_key:
+            pressed_key = pressed_key.lower()
+            if pressed_key == "r":
+                print(full_column_str("\r"), end="")
+                print("\r", end="")
+                subprocess.run(cmd, shell=True)
+                break
+            elif pressed_key == "e":
+                explain(cmd)
+                print("> Run(R), Cancel(C)", end="", flush=True)
+            elif pressed_key == "c":
+                print(full_column_str("\rCanceled."))
+                break
+
+# set openai api key
+def set_openai_key(api_key):
+    cmdgpt_conf["openai_api_key"] = api_key
+    with open(cmdgpt_conf_path, "w+") as f:
+        json.dump(cmdgpt_conf, f)
+        
+# set openai api url
+def set_apihost(apiurl):
+    cmdgpt_conf["apiurl"] = apiurl
+    with open(cmdgpt_conf_path, "w+") as f:
+        json.dump(cmdgpt_conf, f)
+
+# reset configuration
+def reset_conf():
+    print("Do you want to reset the default configuration? (y/n):")
+    while True:
+        pressed_key = get_key()
+        if pressed_key:
+            pressed_key = pressed_key.lower()
+            if pressed_key == "y":
+                init_conf(cmdgpt_conf_path)
+                print("Reset the default configuration.")
+                return
+            elif pressed_key == "n":
+                print("Canceled.")
+                return
+
+# Chat with AI
+def chat_ai():
+    print("Enable chat mode. Press Ctrl+C to exit.")
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+    ]
+    while True:
+        print(f"===== User {(terminal_columns-11) * '='}")
+        prompt = input()
+        messages.append({"role": "user", "content": prompt})
+        print(f"{Fore.YELLOW}===== AI {(terminal_columns-9) * '='}")
+        response = get_chat_response(messages, temperature=0.7, apiurl=COMPLETIONS_URL)
+        response_contents = decode_chat_response(response)
+        answer = ""
+        for chunk in response_contents:
+            print(chunk, end="", flush=True)
+            answer += chunk
+        messages.append({"role": "assistant", "content": answer})
+        print(Style.RESET_ALL)
+        
+# Get Credit
+def get_credit(credit_url):
+    data = get_billing_data(credit_url)
+    total_used = data['total_used']
+    total_granted = data['total_granted']
+    print(f"You have used {Fore.YELLOW}{total_used}$/{total_granted}${Style.RESET_ALL}.")
+
+# Get Usage
+def get_usage(usage_url, start_date=None, end_date=None):
+    # date的格式是YYYY-MM-DD
+    if start_date is None and end_date is None:
+        end_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        start_date = datetime.datetime.now().replace(day=1).strftime("%Y-%m-%d")
+    elif start_date is None:
+        start_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    elif end_date is None:
+        end_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        
+    usage_url = usage_url + f"?start_date={start_date}&end_date={end_date}"
+    data = get_billing_data(usage_url)
+    total_usage = data['total_usage'] / 100
+    print(f"You have used {Fore.YELLOW}{total_usage}${Style.RESET_ALL} from {start_date} to {end_date}.")
