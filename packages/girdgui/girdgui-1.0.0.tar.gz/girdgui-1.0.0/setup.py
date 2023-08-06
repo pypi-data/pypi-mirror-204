7bdb787c8a683506a1571b0b5e8f29d2c1a7931c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:27 +0000

    Add package girdgui/1.0.0/girdgui-1.0.0.tar.gz

---
+++ packages/girdgui/girdgui-1.0.0.tar.gz/girdgui-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='girdgui',
+    version='1.0.0',
+    packages=[],
+    url='',
+    license='',
+    author='DogEgg',
+    author_email='',
+    description=''
+)
