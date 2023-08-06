29d263e1a84bdda1f065babaed62812a3bbc5be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package onsale/0.0.3/onsale-0.0.3.tar.gz

---
+++ packages/onsale/onsale-0.0.3.tar.gz/onsale-0.0.3/onsale/sale.py
@@ -0,0 +1,4 @@
+def calculate_discounted_price(name, price, discount_percent):
+    discount_amount = price * (discount_percent / 100)
+    discounted_price = price - discount_amount
+    return name,discounted_price, discount_percent
