e3c77c95d51373f774ab41a00a305ff177d46388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:00 +0000

    Add package winavsos/0.0.0.1/winavsos-0.0.0.1.tar.gz

---
+++ packages/winavsos/winavsos-0.0.0.1.tar.gz/winavsos-0.0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name="winavsos",
+    version="0.0.0.1",
+    packages=find_packages(),
+    install_requires=required_packages,
+    description="An Automated Vulnerabilty Scanner",
+    author="Tomiwa Ibikunle",
+    author_email="tomzy2506@gmail.com",
+    license="MIT License",
+    url="https://github.com/Tomzy2506/AVSOS",
+    package_data={
+    	"avsos": ["config.ini", "service-names-port-numbers.csv", "amass.exe"],
+    	},
+    entry_points={
+        "console_scripts": [
+            "winavsos=winavsos.main:main",
+        ],
+    },
+)
