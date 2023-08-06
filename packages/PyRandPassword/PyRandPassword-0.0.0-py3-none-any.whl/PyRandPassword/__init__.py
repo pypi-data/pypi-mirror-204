6d34f292d0f07581cdf704eccd78f4a35632f87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:19 +0000

    Add package PyRandPassword/0.0.0/PyRandPassword-0.0.0-py3-none-any.whl

---
+++ packages/PyRandPassword/PyRandPassword-0.0.0-py3-none-any.whl/PyRandPassword/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+sys.stdout.write(
+    'This is not an official version, this is a placeholder to occupy a module name, but please believe me,'
+    ' PyRandPassword has a prototype, and the beta version will be officially released soon,'
+    ' so I am afraid that the module name will be occupied.'
+    ' Requires extensive revisions, delaying release of PyRandPassword.'
+)
+sys.stdout.flush()
