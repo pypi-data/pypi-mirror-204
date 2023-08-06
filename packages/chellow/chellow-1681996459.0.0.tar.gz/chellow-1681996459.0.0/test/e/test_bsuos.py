7623d29a8046d30176040bd424f2f9eca6dfbfe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:29 +0000

    Add package chellow/1681996459.0.0/chellow-1681996459.0.0.tar.gz

---
+++ packages/chellow/chellow-1681996459.0.0.tar.gz/chellow-1681996459.0.0/test/e/test_bsuos.py
@@ -0,0 +1,29 @@
+import pytest
+
+from chellow.e.bsuos import _find_file_type, _process_url
+
+
+@pytest.mark.parametrize(
+    "filename,ext",
+    [
+        ['filename="bsuos.csv"', "csv"],
+        ['inline; filename="Current_RF_BSUoS_Data_175.xls"', "xls"],
+        ["inline; filename=Current_RF_BSUoS_Data_175.xls", "xls"],
+    ],
+)
+def test_find_file_type_csv(filename, ext):
+    assert _find_file_type(filename) == ext
+
+
+def test_process_url_csv(mocker, sess):
+    res = mocker.Mock()
+    res.text = "Date"
+    res.status_code = 200
+    res.reason = "okay"
+    res.headers = {"Content-Disposition": 'filename="bsuos.csv"'}
+    logger = mocker.Mock()
+    contract = mocker.Mock()
+    url = mocker.Mock()
+    s = mocker.Mock()
+    s.get = mocker.Mock(return_value=res)
+    _process_url(logger, sess, url, contract, s)
