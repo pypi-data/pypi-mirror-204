9895114afc0afec69b273ffd189651eb858b9c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:27 +0000

    Add package pash-annotations/0.2.1/pash-annotations-0.2.1.tar.gz

---
+++ packages/pash-annotations/pash-annotations-0.2.1.tar.gz/pash-annotations-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# from distutils.core import setup, find_packages
+from setuptools import setup, find_packages
+
+from pathlib import Path
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+##
+## TODO: Rename the package names (and the name of the whole library)
+##
+setup(name='pash-annotations',
+      version='0.2.1',
+      py_modules=['pash_annotations.util_flag_option',
+                  'pash_annotations.util_new',
+                  'pash_annotations.util_standard',
+                  'pash_annotations.annotation_cli'],
+      packages=['pash_annotations',
+                'pash_annotations.annotation_generation',
+                'pash_annotations.annotation_generation.annotation_generators',
+                'pash_annotations.annotation_generation.datatypes',
+                'pash_annotations.annotation_generation.datatypes.parallelizability',
+                'pash_annotations.config',
+                'pash_annotations.datatypes',
+                'pash_annotations.parser'],
+      package_data={'pash_annotations': ['parser/command_flag_option_info/data/*.json']},
+      ## Necessary for the markdown to be properly rendered
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      python_requires='>=3.8',
+      include_package_data=True,
+      )
