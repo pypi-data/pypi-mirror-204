e8ae312b8c60cd51895e0829e7a9e4974088013a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:49 +0000

    Add package stock-price-plot/0.0.1/stock_price_plot-0.0.1.tar.gz

---
+++ packages/stock-price-plot/stock_price_plot-0.0.1.tar.gz/stock_price_plot-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='stock_price_plot',
+    version='0.0.1',
+    description='This is a library that graphs a stock plot',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Luke Abbatessa',
+    author_email='labbatessa14@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='stock',
+    packages=find_packages(),
+    install_requires=['plotly>=5.10.0',
+                      'pandas>=1.4.3',
+    ],
+)
