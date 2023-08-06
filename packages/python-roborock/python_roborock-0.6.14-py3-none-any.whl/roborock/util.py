f178137276ded048ea6118a355cc6744b36cf1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:11 +0000

    Add package python-roborock/0.6.14/python_roborock-0.6.14-py3-none-any.whl

---
+++ packages/python-roborock/python_roborock-0.6.14-py3-none-any.whl/roborock/util.py
@@ -0,0 +1,25 @@
+import asyncio
+import functools
+from asyncio import AbstractEventLoop
+
+
+def get_running_loop_or_create_one() -> AbstractEventLoop:
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    return loop
+
+
+def run_sync():
+    loop = get_running_loop_or_create_one()
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            return loop.run_until_complete(func(*args, **kwargs))
+
+        return wrapped
+
+    return decorator
