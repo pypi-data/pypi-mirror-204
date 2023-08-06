2fa4c0aa70dd7362d422e79f888853f9b8264f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:00 +0000

    Add package dash-data-prep/0.0.2/dash_data_prep-0.0.2.tar.gz

---
+++ packages/dash-data-prep/dash_data_prep-0.0.2.tar.gz/dash_data_prep-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='dash_data_prep',
+    version='0.0.2',
+    description='This is a library that gathers VADER sentiment scores for files',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Luke Abbatessa',
+    author_email='labbatessa14@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='VADER sentiment',
+    packages=find_packages(),
+    install_requires=['textquisite>=0.0.2',
+                      'textquisite-parsers>=0.0.2'
+    ],
+)
