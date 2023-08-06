400c6305a0628b73cbe94184d74e4a39b08110ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:52 +0000

    Add package homeassistant-stubs/2023.4.6/homeassistant_stubs-2023.4.6-py3-none-any.whl

---
+++ packages/homeassistant-stubs/homeassistant_stubs-2023.4.6-py3-none-any.whl/homeassistant-stubs/components/ampio/const.pyi
@@ -0,0 +1,6 @@
+from _typeshed import Incomplete
+from typing import Final
+
+ATTRIBUTION: Final[str]
+CONF_STATION_ID: Final[str]
+SCAN_INTERVAL: Final[Incomplete]
---
+++ packages/homeassistant-stubs/homeassistant_stubs-2023.4.6-py3-none-any.whl/homeassistant-stubs/components/tts/media_source.pyi
@@ -0,0 +1,29 @@
+from . import SpeechManager as SpeechManager
+from .const import DOMAIN as DOMAIN
+from _typeshed import Incomplete
+from homeassistant.components.media_player import BrowseError as BrowseError, MediaClass as MediaClass
+from homeassistant.components.media_source import BrowseMediaSource as BrowseMediaSource, MediaSource as MediaSource, MediaSourceItem as MediaSourceItem, PlayMedia as PlayMedia, Unresolvable as Unresolvable
+from homeassistant.core import HomeAssistant as HomeAssistant, callback as callback
+from homeassistant.exceptions import HomeAssistantError as HomeAssistantError
+from homeassistant.helpers.network import get_url as get_url
+from typing import TypedDict
+
+async def async_get_media_source(hass: HomeAssistant) -> TTSMediaSource: ...
+def generate_media_source_id(hass: HomeAssistant, message: str, engine: str | None = ..., language: str | None = ..., options: dict | None = ..., cache: bool | None = ...) -> str: ...
+
+class MediaSourceOptions(TypedDict):
+    engine: str
+    message: str
+    language: str | None
+    options: dict | None
+    cache: bool | None
+
+def media_source_id_to_kwargs(media_source_id: str) -> MediaSourceOptions: ...
+
+class TTSMediaSource(MediaSource):
+    name: str
+    hass: Incomplete
+    def __init__(self, hass: HomeAssistant) -> None: ...
+    async def async_resolve_media(self, item: MediaSourceItem) -> PlayMedia: ...
+    async def async_browse_media(self, item: MediaSourceItem) -> BrowseMediaSource: ...
+    def _provider_item(self, provider_domain: str, params: str | None = ...) -> BrowseMediaSource: ...
