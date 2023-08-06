2cc9bcdf95206e426ed9446e76664142812f2068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:17 +0000

    Add package robotframework-templateddata/1.4.0/robotframework-templateddata-1.4.0.tar.gz

---
+++ packages/robotframework-templateddata/robotframework-templateddata-1.4.0.tar.gz/robotframework-templateddata-1.4.0/README.rst
@@ -0,0 +1,143 @@
+.. Badges
+
+|License|
+
+TemplatedData
+=============
+.. contents::
+   :local:
+
+Introduction
+------------
+TemplatedData is Robot Framework library for dynamically generated test data using robot variables and Jinja templating.
+
+Requirements
+------------
+Python 3.6+ and Robot Framework 3.2.1+.
+
+TemplatedData will most likely work on other versions but it's not tested on them.
+
+Installation
+------------
+You can install TemplateData by running::
+
+    pip install robotframework-templateddata
+
+Usage
+--------
+You need to import TemplatedData as library first::
+
+    *** Settings ***
+    Library    TemplatedData
+
+TemplatedData will replace all occurences of robot variables (${var}) in file or variable using current robot context
+and scopes. All test data in below examples are saved under 'test_data.txt' files
+
+Test data::
+
+    my variable is ${var}
+
+Robot code::
+
+    ${var}     Set Variable    ${10}
+    ${data}    Get Templated Data From Path    test_data.txt
+    Log    ${data} # it should print `my variable is 10`
+
+Default values
+~~~~~~~~~~~~~~~~~~
+
+If the variable is not found it will be replaced with empty string. You can override that behaviour::
+
+    ${data}    Get Templated Data From Path    test_data.txt    default_empty=${5}
+    Log    ${data} # it should print `my variable is 5`
+
+You can also set default value of variable with `:` symbol.
+
+Test data::
+
+    my variable is ${var} and ${var2:some string}
+
+Robot code::
+
+    ${var}     Set Variable    ${10}
+    ${data}    Get Templated Data From Path    test_data.txt
+    Log    ${data} # it should print `my variable is 10 and some string`
+
+Return value type
+~~~~~~~~~~~~~~~~~~
+
+Return value can be either text/string (default) or json.
+
+Test data::
+
+    { "key": "${var}" }
+
+Robot code::
+
+    ${data}    Get Templated Data From Path    test_data.txt    var=value    return_type=json
+    Log    ${data} # it should print `{ "key": "value" }` and ${data} will be of type json
+   
+Jinja templating
+~~~~~~~~~~~~~~~~~~
+
+TemplatedData can also render the Jinja templates using Robot Framework variables. To enable Jinja template pass ``jinja_template`` as argument to 
+library import or method call::
+
+    *** Settings ***
+   Library    TemplatedData  jinja_template=${True}
+   
+   OR
+   
+   *** Keywords ***
+   Load Data
+       ${data}    Get Templated Data From Path    data.template    jinja_template=${True}
+
+Test data (Jinja template)::
+
+   {
+       "accounts": [
+           { "id": {{ ${account_id:5} }} },
+           { "id2": "{{ ${account_id2} }}" }
+           ],
+       "users": [
+   {%- for user, amount in ${users.items()} %}
+         {
+           "name": "{{ user }}",
+           "amount": {{ amount }}
+         }{{ "," if not loop.last }}
+   {%- endfor %}
+       ]
+   }
+
+Robot code::
+ 
+    ${data}    Get Templated Data From Path    data.template    jinja_template=${True}
+    
+Example data output::
+ 
+    {
+       "accounts": [
+           { "id": 10 },
+           { "id2": "10" }
+           ],
+       "users": [
+         {
+           "name": "bartek",
+           "amount": 5
+         },
+         {
+           "name": "tymoteusz",
+           "amount": 10
+         },
+         {
+           "name": "pawel",
+           "amount": -1
+         }
+       ]
+   }
+
+.. Badges links
+
+.. |License|
+   image:: https://img.shields.io/pypi/l/robotframework-robocop
+   :alt: PyPI - License
---
+++ packages/robotframework-templateddata/robotframework-templateddata-1.4.0.tar.gz/robotframework-templateddata-1.4.0/TemplatedData/__init__.py
@@ -0,0 +1,147 @@
+import json
+
+from jinja2 import Environment, BaseLoader, select_autoescape
+from robot.api import logger
+from robot.errors import VariableError
+from robot.libraries.BuiltIn import BuiltIn
+
+
+class TemplatedData:
+    def __init__(
+        self,
+        default_empty="",
+        jinja_template=False,
+        return_type="text",
+        ignore_missing=False,
+    ):
+        self.default_empty = default_empty
+        self.jinja_template = jinja_template
+        self.return_type = return_type
+        self.ignore_missing = ignore_missing
+
+    @staticmethod
+    def normalize(value):
+        return value.lower()
+
+    def get_templated_data(self, template, **kwargs):
+        default_empty = kwargs.pop("default_empty", self.default_empty)
+        jinja_template = kwargs.pop("jinja_template", self.jinja_template)
+        return_type = kwargs.pop("return_type", self.return_type)
+        ignore_missing = kwargs.pop("ignore_missing", self.ignore_missing)
+        logger.debug(f"Template:\n{template}")
+        overwrite_values = {self.normalize(arg): value for arg, value in kwargs.items()}
+        templated_vars = {}
+        elements = _search_variables(template, default_empty, ignore_missing)
+        template = resolve(elements, overwrite_values, jinja_template, templated_vars)
+        if jinja_template:
+            r_template = Environment(loader=BaseLoader(), autoescape=select_autoescape(['html', 'htm', 'xml'])).from_string(template)
+            replaced_data = r_template.render(templated_vars=templated_vars)
+        else:
+            replaced_data = template
+        logger.debug(f"Rendered template:\n{replaced_data}")
+        return self.return_data_with_type(replaced_data, return_type)
+
+    def get_templated_data_from_path(self, path, encoding="utf-8", **kwargs):
+        with open(path, encoding=encoding) as f:
+            template = f.read()
+        return self.get_templated_data(template, **kwargs)
+
+    @staticmethod
+    def return_data_with_type(data, data_type):
+        if data_type == "json":
+            return json.loads(data)
+        return data
+
+
+class Variable:
+    def __init__(self, string, default_empty, ignore_missing):
+        self.raw_name = f"${{{string}}}"
+        self.ignore_missing = ignore_missing
+        elements = _search_variables(string, default_empty, ignore_missing)
+
+        for index, elem in enumerate(elements):
+            if isinstance(elem, str):
+                if ":" in elem:
+                    pre, suf = elem.split(":", maxsplit=1)
+                    self.value = elements[:index]
+                    self.default = elements[index + 1 :]
+                    if pre:
+                        self.value.append(pre)
+                    if suf:
+                        self.default.insert(0, suf)
+                    break
+        else:
+            self.value = elements
+            self.default = default_empty
+
+    def resolve(self, overwrite_values, jinja_template, templated_vars):
+        value = resolve(self.value, overwrite_values, jinja_template, templated_vars)
+        raw_name, *attrs = value.split(".", maxsplit=1)
+        if raw_name in overwrite_values:
+            default = overwrite_values[raw_name]
+        else:
+            built_in = BuiltIn()
+            if self.ignore_missing:
+                name = built_in._get_var_name(f"${{{raw_name}}}")
+                try:
+                    default = built_in._variables.replace_scalar(name)
+                except VariableError:
+                    default = self.raw_name
+            else:
+                default_value = resolve(
+                    self.default, overwrite_values, jinja_template, templated_vars
+                )
+                default = built_in.get_variable_value(
+                    f"${{{raw_name}}}", default=default_value
+                )
+
+        templated_vars[raw_name] = default
+        if jinja_template:
+            var_replaced = f'templated_vars["{raw_name}"]'
+            if attrs:
+                var_replaced += "." + attrs[0]
+        else:
+            var_replaced = str(default)
+        return var_replaced
+
+
+def resolve(elements, overwrite_values, jinja_template, templated_vars):
+    new_elements = []
+    for elem in elements:
+        if isinstance(elem, Variable):
+            elem = elem.resolve(overwrite_values, jinja_template, templated_vars)
+        new_elements.append(elem)
+    return "".join(new_elements)
+
+
+def _search_variables(string, default_empty, ignore_missing):
+    """return list in form of [string, Variable, string, Variable.. ] .
+    Following string: "my value is ${value}." will return: ["my value is", Variable("${value}"), .]
+    """
+    elements = []
+    if not string:
+        return elements
+    while True:
+        var_start = string.find("${")
+        if var_start < 0:
+            if string:
+                elements.append(string)
+            break
+        if var_start:
+            elements.append(string[:var_start])
+        string = string[var_start + 2 :]
+        bracket = 1
+        for index, char in enumerate(string):
+            if char == "{":
+                bracket += 1
+            elif char == "}":
+                bracket -= 1
+            if not bracket:
+                elements.append(Variable(string[:index], default_empty, ignore_missing))
+                string = string[index + 1 :]
+                break
+        else:
+            if string:
+                elements.append(string)
+            break
+    return elements
