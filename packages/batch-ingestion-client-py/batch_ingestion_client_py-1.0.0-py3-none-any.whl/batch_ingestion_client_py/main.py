5707d10949b43c1aa760d5aaa47fdefdd5e1f607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:34 +0000

    Add package batch-ingestion-client-py/1.0.0/batch_ingestion_client_py-1.0.0-py3-none-any.whl

---
+++ packages/batch-ingestion-client-py/batch_ingestion_client_py-1.0.0-py3-none-any.whl/batch_ingestion_client_py/main.py
@@ -0,0 +1,22 @@
+from data import Data
+from response import Response
+import requests
+import json
+
+
+class BatchIngestor:
+    def __init__(
+        self,
+        base_url: str,
+    ):
+        route = "/w/rest.php/BatchIngestion/v0/batchcreate"
+        self.url = base_url + route
+
+    def ingest(self, data: Data):
+        json_data = data.serialize()
+        response = requests.post(self.url, json=json_data)
+        success = response.status_code == 200
+        if not success:
+            raise Exception(response.text)
+        json_res = json.loads(response.text)
+        return Response.parse(json_res)
