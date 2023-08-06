5625488d1d086b7a84f21d8e78fd2f3f4674ac25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:57 +0000

    Add package PySDM/2.20/PySDM-2.20.tar.gz

---
+++ packages/PySDM/PySDM-2.20.tar.gz/PySDM-2.20/tests/unit_tests/test_particulator.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring
+from ..backends_fixture import backend_class
+from .dummy_particulator import DummyParticulator
+
+assert hasattr(backend_class, "_pytestfixturefunction")
+
+
+class TestParticulator:  # pylint: disable=too-few-public-methods
+    @staticmethod
+    # pylint: disable=redefined-outer-name
+    def test_observer(backend_class):
+        class Observer:  # pylint: disable=too-few-public-methods
+            def __init__(self, particulator):
+                self.steps = 0
+                self.particulator = particulator
+                self.particulator.observers.append(self)
+
+            def notify(self):
+                self.steps += 1
+                assert self.steps == self.particulator.n_steps
+
+        steps = 33
+        particulator = DummyParticulator(backend_class, 44)
+        observer = Observer(particulator)
+        particulator.run(steps)
+
+        assert observer.steps == steps
