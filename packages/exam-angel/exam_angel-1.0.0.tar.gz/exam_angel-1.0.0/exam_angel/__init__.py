fb2e2799c939e8dd86e0d4c58e8b6756b6171a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:54 +0000

    Add package exam-angel/1.0.0/exam_angel-1.0.0.tar.gz

---
+++ packages/exam-angel/exam_angel-1.0.0.tar.gz/exam_angel-1.0.0/exam_angel/__init__.py
@@ -0,0 +1,11 @@
+# klasa która pobiera wartości i wypisuje ją
+
+class ExamAngel:
+    def __init__(self):
+        self.x = 1
+        self.y = 2
+    def dodaj(self):
+        return self.x + self.y
+
+def test():
+    print('test')
