2a698b87f7aa388875a0ed9c3e9379ed238998e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:56 +0000

    Add package xmlxnat/0.0.0/xmlxnat-0.0.0.tar.gz

---
+++ packages/xmlxnat/xmlxnat-0.0.0.tar.gz/xmlxnat-0.0.0/xmlxnat/__init__.py
@@ -0,0 +1,12 @@
+"""
+XML parsing tools used in Keenan's XNAT toolbox.
+"""
+
+__all__ =\
+(
+    "parse_buffer",
+    "parse_file"
+)
+__version__ = (0, 0, 0)
+
+from xmlxnat.main import parse_buffer, parse_file
