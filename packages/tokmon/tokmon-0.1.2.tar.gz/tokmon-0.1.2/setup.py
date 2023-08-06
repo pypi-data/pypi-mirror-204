5b4532eb1a0524b8611cdfdc868d52c2c95c5490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:26 +0000

    Add package tokmon/0.1.2/tokmon-0.1.2.tar.gz

---
+++ packages/tokmon/tokmon-0.1.2.tar.gz/tokmon-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of the README.md file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='tokmon',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'mitmproxy',
+        'tiktoken',
+    ],
+    package_data={
+        "tokmon": ["pricing.json"],
+    },
+    entry_points={
+        'console_scripts': [
+            "tokmon = tokmon.cli:cli",
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
