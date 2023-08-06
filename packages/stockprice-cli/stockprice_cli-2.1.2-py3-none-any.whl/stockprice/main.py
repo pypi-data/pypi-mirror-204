8ed12c0da964dc1227cb7dd6f9a3009d5416bca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:06 +0000

    Add package stockprice-cli/2.1.2/stockprice_cli-2.1.2-py3-none-any.whl

---
+++ packages/stockprice-cli/stockprice_cli-2.1.2-py3-none-any.whl/stockprice/main.py
@@ -0,0 +1,36 @@
+import sys
+
+from stockprice.calculate_price_movement import calculate_price_movement
+
+
+def main(args: list = sys.argv) -> None:
+    if len(args) == 1:
+        print("Please provide at least one stock ticker.")
+        print("Example: stockprice tsla,aapl")
+        return
+    raw_inputs = args[1:]
+    stock_tickers = []
+    for input in raw_inputs:
+        input = input.upper()
+        if input[-1] == ",":
+            input = input[:-1]
+
+        stock_tickers.extend(input.split(","))
+    for ticker in stock_tickers:
+        try:
+            (
+                current_price,
+                currency,
+                percentage_change_1day,
+                percentage_change_7day,
+                percentage_change_30day,
+            ) = calculate_price_movement(ticker)
+            summary = f"{ticker} -- Current price: {current_price:.2f} {currency} -- Daily change: {percentage_change_1day:.2f}%, 7-day change: {percentage_change_7day:.2f}%, 30-day change: {percentage_change_30day:.2f}%"
+
+            print(summary)
+        except ValueError as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
