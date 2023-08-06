55c1de25702eb39c26eae2246c8f641ce1797209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:36 +0000

    Add package multicall-zksync/0.1.2/multicall_zksync-0.1.2.tar.gz

---
+++ packages/multicall-zksync/multicall_zksync-0.1.2.tar.gz/multicall_zksync-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multicall_zksync']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['eth_retry>=0.1.8,<0.2.0', 'web3>=5.27,<6.0']
+
+setup_kwargs = {
+    'name': 'multicall-zksync',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'JIVIN0902',
+    'author_email': 'jivinvaidya@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
