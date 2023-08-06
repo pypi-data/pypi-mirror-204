0b8e86ebf8e21311145a9ad9eb9172a50692308d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:06 +0000

    Add package machkit/0.1.0/machkit-0.1.0-py3-none-any.whl

---
+++ packages/machkit/machkit-0.1.0-py3-none-any.whl/datappkit/common/__init__.py
@@ -0,0 +1,5 @@
+from datappkit.common import config
+
+__all__ = [
+    'config',
+]
---
+++ packages/machkit/machkit-0.1.0-py3-none-any.whl/datappkit/controller/file_controller.py
@@ -0,0 +1,10 @@
+from datappkit.services.file_service import FileService
+
+
+class FileController:
+
+    def __init__(self):
+        self.service = FileService()
+
+    def download_cli(self, cli_type, target_path):
+        return self.service.download_cli(cli_type, target_path)
