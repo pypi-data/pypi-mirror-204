211611bd2a816b87e43a0b3301d915fc2ef82654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package sqlstrings/0.1.2/sqlstrings-0.1.2.tar.gz

---
+++ packages/sqlstrings/sqlstrings-0.1.2.tar.gz/sqlstrings-0.1.2/README.md
@@ -0,0 +1,11 @@
+# sqlstrings
+Package generates SQL query strings in multiple dialects. It is also able to take in csv files and convert them into statements for easy upload of csv files into databases.
+
+# Installation
+Run the following command to install:
+```python
+pip install sqlstrings
+```
+
+# Contribute
+Repo: https://github.com/alikellaway/sqlstrings
---
+++ packages/sqlstrings/sqlstrings-0.1.2.tar.gz/sqlstrings-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pathlib import Path
+
+setup(
+    name='sqlstrings',
+    version='0.1.2',    
+    description='Generates SQL query strings in multiple dialects.',
+    long_description= (Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type = 'text/markdown',
+    keywords='sql strings generate query',
+    url='https://github.com/alikellaway/sqlstrings',
+    author='Ali Kellaway',
+    author_email='ali.kellaway139@gmail.com',
+    license='MIT',
+    # package_dir={'':''},
+    packages=['sqlstrings'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',         
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+    ]
+)
