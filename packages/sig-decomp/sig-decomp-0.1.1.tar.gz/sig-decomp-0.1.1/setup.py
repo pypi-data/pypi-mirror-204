f97e526106d55fa32d2ed4db40b1f6f54aa5783d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:27 +0000

    Add package sig-decomp/0.1.1/sig-decomp-0.1.1.tar.gz

---
+++ packages/sig-decomp/sig-decomp-0.1.1.tar.gz/sig-decomp-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="sig-decomp",
+    version="0.1.1",
+    description="Optimzation-based signal decomposition",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    setup_requires=["setuptools>=18.0"],
+    install_requires=[
+        "cvxpy",
+        "matplotlib", 
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "qss"
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
