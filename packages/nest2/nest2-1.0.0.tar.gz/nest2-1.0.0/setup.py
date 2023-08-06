a167284ea6a7767a21b03e04e85d45ae6a54af84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:54 +0000

    Add package nest2/1.0.0/nest2-1.0.0.tar.gz

---
+++ packages/nest2/nest2-1.0.0.tar.gz/nest2-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name = 'nest2',
+    version = '1.0.0',
+    py_model = ['nest2'],
+    author = 'htq',
+    author_email = '',
+    url = ' ',
+    description = ' ',
+    )
