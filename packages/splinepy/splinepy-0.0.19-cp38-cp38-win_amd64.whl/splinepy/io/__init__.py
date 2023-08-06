ceae66ea1b6876cc2713e688bbce6fb693fadc26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:01 +0000

    Add package splinepy/0.0.19/splinepy-0.0.19-cp38-cp38-win_amd64.whl

---
+++ packages/splinepy/splinepy-0.0.19-cp38-cp38-win_amd64.whl/splinepy/io/__init__.py
@@ -0,0 +1,12 @@
+from splinepy.io import gismo, iges, irit, json, mfem, npz, vtk, xml
+
+__all__ = [
+    "mfem",
+    "json",
+    "npz",
+    "xml",
+    "iges",
+    "irit",
+    "vtk",
+    "gismo",
+]
