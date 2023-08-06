7f00815ef303f36747483a13f7c850ded793d426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:21 +0000

    Add package airthings-exporter/0.0.3/airthings_exporter-0.0.3-py3-none-any.whl

---
+++ packages/airthings-exporter/airthings_exporter-0.0.3-py3-none-any.whl/airthings/main.py
@@ -0,0 +1,25 @@
+import argparse
+import time
+from prometheus_client import start_http_server, REGISTRY
+from airthings.CloudCollector import CloudCollector
+
+parser = argparse.ArgumentParser(
+    prog='airthings-exporter',
+    description='Prometheus exporter for Airthings devices')
+parser.add_argument('--client-id')
+parser.add_argument('--client-secret')
+parser.add_argument('--device-id', action='append')
+args = parser.parse_args()
+
+REGISTRY.register(CloudCollector(args.client_id, args.client_secret, args.device_id))
+
+
+def main():
+    start_http_server(8000)
+    print('Now listening on port 8000')
+    while True:
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
