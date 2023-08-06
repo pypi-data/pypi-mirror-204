76c733e07c8899a1f1f01ccdf271157fa0f66f61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:00 +0000

    Add package cca-zoo/1.17.1/cca_zoo-1.17.1.tar.gz

---
+++ packages/cca-zoo/cca_zoo-1.17.1.tar.gz/cca_zoo-1.17.1/cca_zoo/models/_stochastic/__init__.py
@@ -0,0 +1,14 @@
+from ._eigengame import PLSEigenGame, CCAEigenGame, RCCAEigenGame
+from ._ghagep import PLSGHAGEP, CCAGHAGEP, RCCAGHAGEP
+
+# from ._stochasticpls import PLSStochasticPower
+
+__all__ = [
+    # "PLSStochasticPower",
+    "PLSGHAGEP",
+    "CCAGHAGEP",
+    "RCCAGHAGEP",
+    "PLSEigenGame",
+    "CCAEigenGame",
+    "RCCAEigenGame",
+]
