24bbf9bfde6e397d5cc22f0c1bc2ab709536c2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:42 +0000

    Add package pyopenbook/0.7.2a0/pyopenbook-0.7.2a0-py3-none-any.whl

---
+++ packages/pyopenbook/pyopenbook-0.7.2a0-py3-none-any.whl/pyserum/utils.py
@@ -0,0 +1,31 @@
+from solana.publickey import PublicKey
+from solana.rpc.api import Client
+from solders.account import Account
+from solders.rpc.responses import GetAccountInfoResp
+from spl.token.constants import WRAPPED_SOL_MINT
+
+from pyserum._layouts.market import MINT_LAYOUT
+
+
+def parse_bytes_data(res: GetAccountInfoResp) -> bytes:
+    if not isinstance(res.value, Account):
+        raise Exception("Cannot load byte data.")
+    return res.value.data
+
+
+def load_bytes_data(addr: PublicKey, conn: Client) -> bytes:
+    res = conn.get_account_info(addr)
+    return parse_bytes_data(res)
+
+
+def parse_mint_decimals(bytes_data: bytes) -> int:
+    return MINT_LAYOUT.parse(bytes_data).decimals
+
+
+def get_mint_decimals(conn: Client, mint_pub_key: PublicKey) -> int:
+    """Get the mint decimals for a token mint"""
+    if mint_pub_key == WRAPPED_SOL_MINT:
+        return 9
+
+    bytes_data = load_bytes_data(mint_pub_key, conn)
+    return parse_mint_decimals(bytes_data)
