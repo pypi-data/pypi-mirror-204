544a2a0b40627c5af03eaa67f32a98beb56ae08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:03 +0000

    Add package athena-ai/0.0.9/athena_ai-0.0.9-py3-none-any.whl

---
+++ packages/athena-ai/athena_ai-0.0.9-py3-none-any.whl/athena/api.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from athena.input_processor import process_input
+
+app = Flask(__name__)
+CORS(app) #, origins=["http://localhost:3000"]) 
+
+
+@app.route('/api/v1/athena', methods=['POST'])
+def athena_chat():
+    data = request.get_json(force=True)
+    user_input = data.get('input')
+    username = data.get('username', None)
+
+    if user_input:
+        response = process_input(user_input, username)
+        return jsonify({'response': response})
+    else:
+        return jsonify({'error': 'Missing input'}), 400
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
