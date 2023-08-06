a1579f7a7c5f106365c52b39b09342f3f2bf6d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:04 +0000

    Add package zcbot-predict-sdk/0.0.8/zcbot-predict-sdk-0.0.8.tar.gz

---
+++ packages/zcbot-predict-sdk/zcbot-predict-sdk-0.0.8.tar.gz/zcbot-predict-sdk-0.0.8/zcbot_predict_sdk/service/base.py
@@ -0,0 +1,10 @@
+from ..client import PredictClient, PredictClientHolder
+
+
+class BaseService(object):
+    """
+    基础服务
+    """
+
+    def __init__(self, celery_client: PredictClient = None):
+        self.celery_client = celery_client or PredictClientHolder.get_default_instance()
