adfaa1e05aca598b30240ddbff3f223f405763ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:13 +0000

    Add package MegEngine/1.12.3/MegEngine-1.12.3-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/MegEngine/MegEngine-1.12.3-cp310-cp310-manylinux2014_x86_64.whl/megenginelite/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .base import *
+from .base import version as __version__
+from .global_setting import *
+from .network import *
+from .struct import *
+from .tensor import *
+from .utils import *
