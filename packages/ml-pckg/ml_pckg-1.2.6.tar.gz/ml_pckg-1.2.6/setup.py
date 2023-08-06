b65f54b4fd04a64eb678b8938d943947a8682bbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:56 +0000

    Add package ml-pckg/1.2.6/ml_pckg-1.2.6.tar.gz

---
+++ packages/ml-pckg/ml_pckg-1.2.6.tar.gz/ml_pckg-1.2.6/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+import re
+with open('setup.py', 'r') as f:
+    setup_file = f.read()
+
+current_version = '1.2.6'
+
+setup(
+    name='ml_pckg',
+    packages=['ml_pckg'],
+    version=current_version,
+    license='MIT',
+    description='Machine Learning Models',
+    author='Gleb Maksimov',
+    author_email='glebmaksimov092@gmail.com',
+    download_url=f'https://github.com/Glebmaksimov/ml_pckg/archive/refs/tags/{current_version}.tar.gz',
+    keywords=['ML', 'FROM SWCRATCH', 'ADVANCED'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'seaborn',
+        'IPython'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
