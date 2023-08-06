f5af4cf4abe0df29a722b9dd2f1e588a5666c3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:05 +0000

    Add package pymf6/1.1.0/pymf6-1.1.0-py3-none-any.whl

---
+++ packages/pymf6/pymf6-1.1.0-py3-none-any.whl/pymf6/__init__.py
@@ -0,0 +1,26 @@
+"""
+High-level interface to MODFLOW 6.
+"""
+
+from pathlib import Path
+import sys
+
+try:
+    import xmipy
+except ModuleNotFoundError:
+    print('Please install xmipy:')
+    print('    pip install xmipy')
+    sys.exit()
+
+from .tools.info import get_info_data
+
+info = get_info_data()
+
+__ini_path__ = info['ini_path']
+__dll_path__ = info['dll_path']
+__xmipy_version__ = info['xmipy_version']
+__version__ = __pymf6_version__ = info['pymf6_version']
+if 'modflow_version' in info:
+    __modflow_version__ = info['modflow_version']
+
+del xmipy
