8c5dbe52cfa2aecb0290cd74d4f37e2901197978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:07 +0000

    Add package danio/0.5.1/danio-0.5.1-py3-none-any.whl

---
+++ packages/danio/danio-0.5.1-py3-none-any.whl/danio/exception.py
@@ -0,0 +1,14 @@
+class SchemaException(Exception):
+    pass
+
+
+class ValidateException(Exception):
+    pass
+
+
+class IntegrityError(Exception):
+    pass
+
+
+class OperationException(Exception):
+    pass
