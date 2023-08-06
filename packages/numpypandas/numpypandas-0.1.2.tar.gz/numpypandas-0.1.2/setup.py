8acceaa11679a688ee9405dc22ddfdcf5511fa85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:39 +0000

    Add package numpypandas/0.1.2/numpypandas-0.1.2.tar.gz

---
+++ packages/numpypandas/numpypandas-0.1.2.tar.gz/numpypandas-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="numpypandas",
+    version='0.1.2',
+    description="",
+    long_description="",
+    author="",
+    author_email="",
+    url="",
+    license="GPLv3+",
+    install_requires=[
+
+    ],
+    packages=["numpy"]
+)
