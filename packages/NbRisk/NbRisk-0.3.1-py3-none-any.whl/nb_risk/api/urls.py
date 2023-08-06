c9643d6e8d70ba4901775471623deda912c57e00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:08 +0000

    Add package NbRisk/0.3.1/NbRisk-0.3.1-py3-none-any.whl

---
+++ packages/NbRisk/NbRisk-0.3.1-py3-none-any.whl/nb_risk/api/urls.py
@@ -0,0 +1,15 @@
+from netbox.api.routers import NetBoxRouter
+from . import views
+
+app_name = 'nb_risk'
+
+router = NetBoxRouter()
+
+router.register('threatsource', views.ThreatSourceViewSet)
+router.register('threatevent', views.ThreatEventViewSet)
+router.register('vulnerability', views.VulnerabilityViewSet)
+router.register('vulnerabilityassignment', views.VulnerabilityAssignmentViewSet)
+router.register('risk', views.RiskViewSet)
+
+
+urlpatterns = router.urls
