3bce6493969a39207bf8057d531caef788f89168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:03 +0000

    Add package py-yprinciple-gen/0.2.9/py_yprinciple_gen-0.2.9.tar.gz

---
+++ packages/py-yprinciple-gen/py_yprinciple_gen-0.2.9.tar.gz/py_yprinciple_gen-0.2.9/tests/test_python.py
@@ -0,0 +1,23 @@
+'''
+Created on 2022-12-02
+
+@author: wf
+'''
+from tests.basetest import Basetest
+from meta.metamodel import Property
+from yprinciple.smw_targets import PythonTarget
+
+class TestPython(Basetest):
+    """
+    test python handling
+    """
+    
+    def test_Types(self):
+        """
+        test the type conversion
+        """
+        pt=PythonTarget(None)
+        prop=Property()
+        prop.type="Types/Number"
+        ptype=pt.pythonPropType(prop)
+        self.assertEqual("float",ptype)
