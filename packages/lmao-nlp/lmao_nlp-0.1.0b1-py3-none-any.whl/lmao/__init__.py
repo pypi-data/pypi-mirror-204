6a5ea33d3593ca49ace7f04d3704cb4665810e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package lmao-nlp/0.1.0b1/lmao_nlp-0.1.0b1-py3-none-any.whl

---
+++ packages/lmao-nlp/lmao_nlp-0.1.0b1-py3-none-any.whl/lmao/__init__.py
@@ -0,0 +1,4 @@
+from . import adapters, clients, factory, prompters, tasks
+from .clients import AnthropicChatHistory, AnthropicClient, OpenAIChatHistory, OpenAIClient
+
+__version__ = "0.1.0-beta.1"
