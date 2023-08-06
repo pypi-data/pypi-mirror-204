48bd69f0fd312b71808d87757e924a4499ffd545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:40 +0000

    Add package system-superset-driver/0.0.4/system_superset_driver-0.0.4.tar.gz

---
+++ packages/system-superset-driver/system_superset_driver-0.0.4.tar.gz/system_superset_driver-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="system_superset_driver",version="0.0.4",packages=find_packages(),
+      entry_points={
+          "shillelagh.adapter": [
+              "system = apm_adapter:SystemAPI",
+          ],
+          "sqlalchemy.dialects": [
+              "system = dialect:SystemDialect",
+          ],
+      },
+
+      )
