999a75f154db25bda168888d4a94bdcfe2b20bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package vutils-validator/0.1.3/vutils_validator-0.1.3-py2.py3-none-any.whl

---
+++ packages/vutils-validator/vutils_validator-0.1.3-py2.py3-none-any.whl/vutils/validator/__init__.pyi
@@ -0,0 +1,8 @@
+#
+# File:    ./src/vutils/validator/__init__.pyi
+# Author:  Jiří Kučera <sanczes AT gmail.com>
+# Date:    2022-05-03 21:26:33 +0200
+# Project: vutils-validator: Data validation utilities
+#
+# SPDX-License-Identifier: MIT
+#
