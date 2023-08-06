09e78c1572af324851e4b16ebe9b3fbdb8c95802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:00 +0000

    Add package yaml-stripper/0.1.2/yaml-stripper-0.1.2.tar.gz

---
+++ packages/yaml-stripper/yaml-stripper-0.1.2.tar.gz/yaml-stripper-0.1.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yaml-stripper",
+    version="0.1.2",
+    author="Wagner Silva",
+    author_email="wra.silva@gmail.com",
+    description="A command-line tool for removing specified fields from YAML files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/your_username/yaml-stripper",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/vavasilva/YamlStripper",
+        "Documentation": "https://github.com/vavasilva/YamlStripper",
+        "Source Code": "https://github.com/vavasilva/YamlStripper",
+        "Homepage": "https://github.com/vavasilva/YamlStripper"
+    },
+    entry_points={
+        'console_scripts': ['yaml-stripper=yaml_stripper:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
