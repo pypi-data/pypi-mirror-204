c0c723c917d96d34be8740359dbf52e577e17a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:54 +0000

    Add package robotoff/1.26.0/robotoff-1.26.0-py3-none-any.whl

---
+++ packages/robotoff/robotoff-1.26.0-py3-none-any.whl/robotoff/cli/insights.py
@@ -0,0 +1,90 @@
+import contextlib
+import gzip
+import sys
+from pathlib import Path
+from typing import Iterable, Optional
+
+import _io
+import click
+import dacite
+import orjson
+import tqdm
+
+from robotoff.off import get_barcode_from_path
+from robotoff.prediction.ocr import OCRResult, extract_predictions
+from robotoff.prediction.ocr.core import ocr_content_iter
+from robotoff.types import Prediction, PredictionType, ProductIdentifier, ServerType
+from robotoff.utils import get_logger, gzip_jsonl_iter, jsonl_iter
+
+logger = get_logger(__name__)
+
+
+def run_from_ocr_archive(
+    input_path: Path,
+    prediction_type: PredictionType,
+    server_type: ServerType,
+    output: Optional[Path] = None,
+):
+    predictions = tqdm.tqdm(
+        generate_from_ocr_archive(input_path, prediction_type, server_type), desc="OCR"
+    )
+    output_f: _io._TextIOBase
+    need_decoding = False
+
+    if output is not None:
+        if output.suffix == ".gz":
+            output_f = gzip.open(output, "wb")
+        else:
+            output_f = output.open("wb")
+    else:
+        output_f = sys.stdout
+        need_decoding = True
+
+    with contextlib.closing(output_f):
+        for prediction in predictions:
+            raw_data = orjson.dumps(prediction.to_dict()) + b"\n"
+            data = raw_data.decode("utf-8") if need_decoding else raw_data
+            output_f.write(data)
+
+
+def generate_from_ocr_archive(
+    input_path: Path, prediction_type: PredictionType, server_type: ServerType
+) -> Iterable[Prediction]:
+    json_iter = (
+        gzip_jsonl_iter(input_path)
+        if input_path.suffix == ".gz"
+        else jsonl_iter(input_path)
+    )
+    for source_image, ocr_json in ocr_content_iter(json_iter):
+        if source_image is None:
+            continue
+
+        barcode: Optional[str] = get_barcode_from_path(source_image)
+
+        if barcode is None:
+            click.echo(
+                f"cannot extract barcode from source {source_image}",
+                err=True,
+            )
+            continue
+
+        ocr_result: Optional[OCRResult] = OCRResult.from_json(ocr_json)
+
+        if ocr_result is None:
+            continue
+
+        yield from extract_predictions(
+            ocr_result,
+            prediction_type,
+            product_id=ProductIdentifier(barcode=barcode, server_type=server_type),
+            source_image=source_image,
+        )
+
+
+def insights_iter(file_path: Path) -> Iterable[Prediction]:
+    for prediction in jsonl_iter(file_path):
+        yield dacite.from_dict(
+            data_class=Prediction,
+            data=prediction,
+            config=dacite.Config(cast=[PredictionType]),
+        )
