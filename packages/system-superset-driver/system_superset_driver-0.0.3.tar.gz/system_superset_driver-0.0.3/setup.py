96e2b82cb7de4ef71f6241aa83309bc87d71ae3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:55 +0000

    Add package system-superset-driver/0.0.3/system_superset_driver-0.0.3.tar.gz

---
+++ packages/system-superset-driver/system_superset_driver-0.0.3.tar.gz/system_superset_driver-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="system_superset_driver",version="0.0.3",packages=find_packages(),
+      entry_points={
+          "shillelagh.adapter": [
+              "system = apm_adapter:SystemAPI",
+          ],
+
+      },
+
+      )
