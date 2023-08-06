2b0c821ca927294bd213447a4dd1bb2ed3cdce57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:17 +0000

    Add package oaklib/0.5.3/oaklib-0.5.3.tar.gz

---
+++ packages/oaklib/oaklib-0.5.3.tar.gz/oaklib-0.5.3/src/oaklib/parsers/gaf_association_parser.py
@@ -0,0 +1,30 @@
+"""Parser for GAF association formats"""
+from dataclasses import dataclass, field
+from typing import List, Union
+
+from oaklib.datamodels.association import Association, NegatedAssociation
+from oaklib.parsers.parser_base import ColumnReference
+from oaklib.parsers.xaf_association_parser import XafAssociationParser
+
+
+@dataclass
+class GafAssociationParser(XafAssociationParser):
+    """Parsers for GAF format."""
+
+    subject_prefix_column: ColumnReference = field(default_factory=lambda: ColumnReference(0))
+    subject_column: ColumnReference = field(default_factory=lambda: ColumnReference(1))
+    predicate_column: ColumnReference = field(default_factory=lambda: ColumnReference(3))
+    object_column: ColumnReference = field(default_factory=lambda: ColumnReference(4))
+    publications_column: ColumnReference = field(default_factory=lambda: ColumnReference(5))
+    primary_knowledge_source_column: ColumnReference = field(
+        default_factory=lambda: ColumnReference(14)
+    )
+    expected_object_prefixes = ["GO"]
+
+    def post_process(
+        self, association: Association
+    ) -> List[Union[Association, NegatedAssociation]]:
+        if association.predicate and "not" in association.predicate.lower():
+            # in future this may return a NegatedAssociation
+            return [NegatedAssociation(**association.__dict__)]
+        return super().post_process(association)
