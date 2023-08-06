d9c47298f0e265c8db2dbf7dfb878e14232de989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:00 +0000

    Add package datagen-tech/1.10.5rc3/datagen_tech-1.10.5rc3.tar.gz

---
+++ packages/datagen-tech/datagen_tech-1.10.5rc3.tar.gz/datagen_tech-1.10.5rc3/src/datagen/api/catalog/attributes.py
@@ -0,0 +1,16 @@
+from datagen_protocol.schema.attributes import *  # noqa: F401 F403
+
+
+class AllOf(list):
+    def __init__(self, *attributes):
+        super().__init__(attributes)
+
+
+class AnyOf(list):
+    def __init__(self, *attributes):
+        super().__init__(attributes)
+
+
+class Exactly(list):
+    def __init__(self, *attributes):
+        super().__init__(attributes)
