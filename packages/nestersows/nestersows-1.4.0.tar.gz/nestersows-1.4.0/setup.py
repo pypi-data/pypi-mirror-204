ea0316ebae25a848c9c0a944e79724eaeabd356a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:16 +0000

    Add package nestersows/1.4.0/nestersows-1.4.0.tar.gz

---
+++ packages/nestersows/nestersows-1.4.0.tar.gz/nestersows-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = 'nestersows',
+    version = '1.4.0',
+    py_modules = ['nestersows'],
+    author = 'jaytj005',
+    author_email = 'jaytj005@yahoo.com',
+    url = 'https://www.w3schools.com',
+    description = 'A simple printer of nested lists'
+
+    )
