a33ab0b6e4190c5e464a53c42da8a9aff9c27776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:06 +0000

    Add package meili-sdk/0.5.7/meili-sdk-0.5.7.tar.gz

---
+++ packages/meili-sdk/meili-sdk-0.5.7.tar.gz/meili-sdk-0.5.7/meili_sdk/websockets/models/__init__.py
@@ -0,0 +1,5 @@
+from .docking_routine_message import DockingRoutineMessage
+from .message import Message
+from .move_message import MoveMessage
+from .task import Task
+from .traffic_control_action_message import PathReroutingMessage, SlowDownMessage
---
+++ packages/meili-sdk/meili-sdk-0.5.7.tar.gz/meili-sdk-0.5.7/meili_sdk/websockets/models/move_message.py
@@ -0,0 +1,20 @@
+from meili_sdk.models.base import BaseModel
+
+__all__ = ("MoveMessage",)
+
+
+class MoveMessage(BaseModel):
+    type: str
+    location: dict
+
+    @property
+    def is_charge(self):
+        return self.type == "move_charge"
+
+    @property
+    def is_inside(self):
+        return "x" in self.location
+
+    @property
+    def is_outdoor(self):
+        return not self.is_inside
