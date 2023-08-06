24b890df5084e7e0e682ed1136a4dfbcec54d715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:20 +0000

    Add package buildington/1.3/buildington-1.3.tar.gz

---
+++ packages/buildington/buildington-1.3.tar.gz/buildington-1.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+	name="buildington",
+	version="1.3",
+	author="RixTheTyrunt",
+	author_email="rixthetyrunt@gmail.com",
+	description="Building websites like React, and hosting 'em like Flask!",
+	packages=["buildington"],
+	classifiers=[
+		"Environment :: Console",
+		"Framework :: Flask",
+		"Operating System :: OS Independent",
+		"Topic :: Internet :: WWW/HTTP :: HTTP Servers",
+		"Topic :: Internet :: WWW/HTTP :: Site Management",
+		"Topic :: Internet :: WWW/HTTP :: WSGI :: Server"
+	],
+	install_requires=["flask"]
+)
