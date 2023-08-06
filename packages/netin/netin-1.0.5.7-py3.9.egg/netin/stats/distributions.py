e559a05fb06d020f01dbd3c8529f37cc93b8cc25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:24 +0000

    Add package netin/1.0.5.7/netin-1.0.5.7-py3.9.egg

---
+++ packages/netin/netin-1.0.5.7-py3.9.egg/netin/stats/distributions.py
@@ -0,0 +1,71 @@
+from typing import Union, Set, List, Tuple
+
+import numpy as np
+import pandas as pd
+import powerlaw
+
+
+def get_pdf(df: pd.DataFrame, x: str, total: float) -> Tuple[np.ndarray, np.ndarray]:
+    """Compute the probability density of the input data.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        DataFrame that contains the data.
+    x : str
+        The column name of the data.
+    total : float
+        The total amount by which to normalize the data.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray]
+        Two arrays holding the values and their probability.
+    """
+    values = df.groupby(x).size()
+    xs = values.index.values
+    ys = values.values / total
+    return xs, ys
+
+
+def get_cdf(df: pd.DataFrame, x: str, total: float = None) -> (np.ndarray, np.ndarray):
+    xs = np.sort(df[x].values)
+    n = xs.size
+    ys = np.arange(1, n + 1) / n
+    return xs, ys
+
+
+def get_ccdf(df: pd.DataFrame, x: str, total: float = None) -> (np.ndarray, np.ndarray):
+    xs, ys = get_cdf(df, x, total)
+    return xs, 1 - ys
+
+
+def get_disparity(df: pd.DataFrame, x: str, total: float = None) -> (np.ndarray, np.ndarray):
+    from netin.stats import ranking
+    from netin.utils import constants as const
+
+    gx, gy = get_gini_coefficient(df, x, total)
+    fx, fy = get_fraction_of_minority(df, x, total)
+    f_m = df.query("class_label == @const.MINORITY_LABEL").shape[0] / df.shape[0]
+
+    inequality_y = ranking.get_ranking_inequality(gy)
+    inequity_x = ranking.get_ranking_inequity(f_m, fy)
+    return inequity_x, inequality_y
+
+
+def get_fraction_of_minority(df: pd.DataFrame, x: str, total: float = None) -> (np.ndarray, np.ndarray):
+    from netin.stats import ranking
+    xs, ys = ranking.get_fraction_of_minority_in_ranking(df, x)
+    return xs, ys
+
+
+def get_gini_coefficient(df: pd.DataFrame, x: str, total: float = None) -> (np.ndarray, np.ndarray):
+    from netin.stats import ranking
+    xs, ys = ranking.get_gini_in_ranking(df, x)
+    return xs, ys
+
+
+def fit_power_law(data: Union[np.array, Set, List], discrete: bool = True,
+                  xmin: Union[None, int, float] = None, xmax: Union[None, int, float] = None, **kwargs) -> powerlaw.Fit:
+    fit = powerlaw.Fit(data, discrete=discrete, xmax=xmax, xmin=xmin, **kwargs)
+    return fit
