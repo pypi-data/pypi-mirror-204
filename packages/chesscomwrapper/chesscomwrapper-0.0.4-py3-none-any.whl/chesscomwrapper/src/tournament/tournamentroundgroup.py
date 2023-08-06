510391d62acdfdb1d52bb048f1052ad703808bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:50 +0000

    Add package chesscomwrapper/0.0.4/chesscomwrapper-0.0.4-py3-none-any.whl

---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.4-py3-none-any.whl/chesscomwrapper/src/tournament/tournamentroundgroup.py
@@ -0,0 +1,11 @@
+from ..chesscomhandlers.roundinfohandler import RoundInfoHandler
+
+
+class TournamentRoundGroup(object):
+    def __init__(self, url) -> None:
+        self.url = url
+
+    def getInfo(self):
+        self.info = RoundInfoHandler().getRoundGroupInfo(self.url)
+
+        pass
---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.4-py3-none-any.whl/chesscomwrapper/src/tournament/tournamentroundgroupinfo.py
@@ -0,0 +1,80 @@
+# "fair_play_removals": [],
+#     "games": [
+#         {
+#             "url": "https://www.chess.com/game/daily/151868796",
+#             "pgn": "[Event \"33rd Chess.com Quick Knockouts (1401-1600) - Round 4\"]\n[Site \"Chess.com\"]\n[Date \"2016.12.10\"]\n[Round \"-\"]\n[White \"Drooo82\"]\n[Black \"Chicken_Forever\"]\n[Result \"0-1\"]\n[Tournament \"https://www.chess.com/tournament/-33rd-chesscom-quick-knockouts-1401-1600\"]\n[CurrentPosition \"8/5k2/4p3/2P1P3/1p3K1p/8/8/8 w - - 1 52\"]\n[Timezone \"UTC\"]\n[ECO \"B52\"]\n[ECOUrl \"https://www.chess.com/openings/Sicilian-Defense-Canal-Main-Line-Sokolsky-Variation-5...Nc6-6.Nc3-Nf6-7.O-O\"]\n[UTCDate \"2016.12.10\"]\n[UTCTime \"19:33:59\"]\n[WhiteElo \"1799\"]\n[BlackElo \"2015\"]\n[TimeControl \"1/86400\"]\n[Termination \"Chicken_Forever won by resignation\"]\n[StartTime \"19:33:59\"]\n[EndDate \"2017.01.04\"]\n[EndTime \"06:20:56\"]\n[Link \"https://www.chess.com/game/daily/151868796\"]\n\n1. e4 c5 2. Nf3 d6 3. Bb5+ Bd7 4. Bxd7+ Qxd7 5. c4 Nc6 6. Nc3 Nf6 7. O-O g6 8. d4 cxd4 9. Nxd4 Bg7 10. Nc2 O-O 11. b3 a5 12. Re1 Ng4 13. Bb2 f5 14. h3 Nh6 15. Rb1 Rad8 16. Qd2 f4 17. f3 g5 18. Nd5 Bxb2 19. Rxb2 Nf7 20. Nd4 Nfe5 21. Kh1 e6 22. Nc3 h5 23. Ncb5 Rf6 24. Nxc6 bxc6 25. Nd4 g4 26. Qf2 c5 27. Nb5 Nd3 28. Qd2 Nxe1 29. Qxe1 Qg7 30. Qxa5 Rd7 31. Qc3 Qf8 32. a4 Rg6 33. a5 gxh3 34. gxh3 Qd8 35. Qe1 Rg3 36. Rg2 Rdg7 37. Rxg3 Rxg3 38. Kh2 Qf6 39. Qf2 Qa1 40. Nxd6 Qxa5 41. e5 Qa1 42. Ne4 Rxh3+ 43. Kxh3 Qh1+ 44. Qh2 Qxf3+ 45. Ng3 Qxg3+ 46. Qxg3+ fxg3 47. Kxg3 Kf7 48. Kf3 Kg6 49. Kf4 h4 50. b4 cxb4 51. c5 Kf7 0-1\n",
+#             "time_control": "1/86400",
+#             "end_time": 1483510856,
+#             "rated": true,
+#             "fen": "8/5k2/4p3/2P1P3/1p3K1p/8/8/8 w - - 1 52",
+#             "start_time": 1481398439,
+#             "time_class": "daily",
+#             "rules": "chess",
+#             "white": {
+#                 "rating": 1799,
+#                 "result": "resigned",
+#                 "@id": "https://api.chess.com/pub/player/drooo82",
+#                 "username": "Drooo82",
+#                 "uuid": "58dc1d1e-c3c9-11e2-8028-000000000000"
+#             },
+#             "black": {
+#                 "rating": 2015,
+#                 "result": "win",
+#                 "@id": "https://api.chess.com/pub/player/chicken_forever",
+#                 "username": "Chicken_Forever",
+#                 "uuid": "6741f874-20f5-11e6-8044-000000000000"
+#             },
+#             "tournament": "https://api.chess.com/pub/tournament/-33rd-chesscom-quick-knockouts-1401-1600"
+#         },
+#         {
+#             "url": "https://www.chess.com/game/daily/151868798",
+#             "pgn": "[Event \"33rd Chess.com Quick Knockouts (1401-1600) - Round 4\"]\n[Site \"Chess.com\"]\n[Date \"2016.12.10\"]\n[Round \"-\"]\n[White \"Chicken_Forever\"]\n[Black \"Drooo82\"]\n[Result \"1/2-1/2\"]\n[Tournament \"https://www.chess.com/tournament/-33rd-chesscom-quick-knockouts-1401-1600\"]\n[CurrentPosition \"rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2\"]\n[Timezone \"UTC\"]\n[ECO \"C40\"]\n[ECOUrl \"https://www.chess.com/openings/Kings-Pawn-Opening-Kings-Knight-Variation\"]\n[UTCDate \"2016.12.10\"]\n[UTCTime \"19:34:00\"]\n[WhiteElo \"2019\"]\n[BlackElo \"1775\"]\n[TimeControl \"1/86400\"]\n[Termination \"Game drawn by agreement\"]\n[StartTime \"19:34:00\"]\n[EndDate \"2016.12.11\"]\n[EndTime \"12:27:07\"]\n[Link \"https://www.chess.com/game/daily/151868798\"]\n\n1. e4 e5 2. Nf3 1/2-1/2\n",
+#             "time_control": "1/86400",
+#             "end_time": 1481459227,
+#             "rated": true,
+#             "fen": "rnbqkbnr/pppp1ppp/8/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+#             "start_time": 1481398440,
+#             "time_class": "daily",
+#             "rules": "chess",
+#             "white": {
+#                 "rating": 2019,
+#                 "result": "agreed",
+#                 "@id": "https://api.chess.com/pub/player/chicken_forever",
+#                 "username": "Chicken_Forever",
+#                 "uuid": "6741f874-20f5-11e6-8044-000000000000"
+#             },
+#             "black": {
+#                 "rating": 1775,
+#                 "result": "agreed",
+#                 "@id": "https://api.chess.com/pub/player/drooo82",
+#                 "username": "Drooo82",
+#                 "uuid": "58dc1d1e-c3c9-11e2-8028-000000000000"
+#             },
+#             "tournament": "https://api.chess.com/pub/tournament/-33rd-chesscom-quick-knockouts-1401-1600"
+#         }
+#     ],
+#     "players": [
+#         {
+#             "username": "drooo82",
+#             "points": 0.5,
+#             "is_advancing": false,
+#             "tie_break": 0.75
+#         },
+#         {
+#             "username": "chicken_forever",
+#             "points": 1.5,
+#             "is_advancing": true,
+#             "tie_break": 0.75
+#         }
+#     ]
+
+
+from ..player.playergames import ChesscomGame
+from ..tournament.tournamentroundplayer import TournamentRoundPlayer
+
+
+class TournamentRoundGroupInfo(object):
+    def __init__(self, data):
+        self.fair_play_removals = data.get('fair_play_removals', None)
+        self.games = [ChesscomGame(gamedata) for gamedata in data.get('games', [])]
+        self.players = [TournamentRoundPlayer(playerdata) for playerdata in data.get('players', [])]
