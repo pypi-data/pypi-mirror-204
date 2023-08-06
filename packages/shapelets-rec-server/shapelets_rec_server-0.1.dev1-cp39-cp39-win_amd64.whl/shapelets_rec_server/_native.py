0f23af2beaeb7e1da1c6a83ae0e65eb1f89594f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:26 +0000

    Add package shapelets-rec-server/0.1.dev1/shapelets_rec_server-0.1.dev1-cp39-cp39-win_amd64.whl

---
+++ packages/shapelets-rec-server/shapelets_rec_server-0.1.dev1-cp39-cp39-win_amd64.whl/shapelets_rec_server/_native.py
@@ -0,0 +1,13 @@
+from ._shapelets_rec_server_extension import (
+    JSONStore,
+    zmq_version,
+    simdjson_active_runtime,
+    simdjson_threaded,
+    simdjson_version, 
+    Bitmap,
+    ScanOperation,
+    ExtractorType,
+    Extractor, 
+    DynamicScan,
+    try_me
+)
