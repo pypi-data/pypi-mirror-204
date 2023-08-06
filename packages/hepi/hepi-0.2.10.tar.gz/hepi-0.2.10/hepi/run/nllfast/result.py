4fa92fd61c6f95821fe4457b7b5d528086db04ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:08 +0000

    Add package hepi/0.2.10/hepi-0.2.10.tar.gz

---
+++ packages/hepi/hepi-0.2.10.tar.gz/hepi-0.2.10/hepi/run/nllfast/result.py
@@ -0,0 +1,23 @@
+from hepi.results import Result
+
+
+class NLLFastResult(Result):
+    """
+    (N)NLL-fast Result Data.
+    """
+
+    def __init__(
+        self,
+        lo,
+        nlo,
+        nlo_plus_nll,
+        scale_err_up,
+        scale_err_down,
+        pdf_err_up,
+        pdf_err_down,
+    ):
+        Result.__init__(self, lo, nlo, nlo_plus_nll)
+        self.scale_err_up = scale_err_up
+        self.scale_err_down = scale_err_down
+        self.pdf_err_up = pdf_err_up
+        self.pdf_err_down = pdf_err_down
