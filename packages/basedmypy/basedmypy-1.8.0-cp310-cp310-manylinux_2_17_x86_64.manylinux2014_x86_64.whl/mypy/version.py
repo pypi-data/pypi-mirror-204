ac6960a944cfb4c76f35eaff0a9acf1df393935f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:31 +0000

    Add package basedmypy/1.8.0/basedmypy-1.8.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/basedmypy/basedmypy-1.8.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/mypy/version.py
@@ -0,0 +1,4 @@
+from mypy.versionutil import VersionInfo
+__version__ = "1.2.0"
+based_version_info = VersionInfo(major=1, minor=8, patch=0, release_level='final', iteration=0, mypy_version='1.2.0', mypy_release_level='final')
+__based_version__ = "1.8.0"
