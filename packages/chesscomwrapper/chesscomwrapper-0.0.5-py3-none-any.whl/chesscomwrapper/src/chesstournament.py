a3f6755b0faf8ae166b469af1b74dbcaaa79731b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:09 +0000

    Add package chesscomwrapper/0.0.5/chesscomwrapper-0.0.5-py3-none-any.whl

---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.5-py3-none-any.whl/chesscomwrapper/src/chesstournament.py
@@ -0,0 +1,15 @@
+from .handlers.chesscomhandlers.tournamenthandler import TournamentHandler
+
+
+class Tournament(object):
+    def __init__(self, id):
+        self.id = id
+        
+    def getInfo(self):
+        self.info = TournamentHandler().getInfo(self.id)
+
+    def getRounds(self):
+        self.rounds = TournamentHandler().getRounds(self.id)
+
+
+
