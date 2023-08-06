06aa0dfb332aa1ac5fe8c812333ca2616d316281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:17 +0000

    Add package chataimaker/1.2/chataimaker-1.2.tar.gz

---
+++ packages/chataimaker/chataimaker-1.2.tar.gz/chataimaker-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+os.chdir(r'C:\Users\JustBNF\Desktop\Secret~\Python\Modules\ChatAIMaker')
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Education",
+    "Operating System :: Microsoft :: Windows :: Windows 10",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3"
+]
+
+setup(
+    name = "chataimaker",
+    version = "1.2",
+    description = "Package, that can help make you AI chat bot",
+    long_description = open("README.txt").read() + '\n\n\n' + open("CHANGELOG.txt").read(),
+    url="",
+    author="Boynfriend",
+    author_email="justbnf@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords='',
+    packages=find_packages(),
+    install_requires=['nltk', 'numpy', 'tflearn', 'tensorflow']
+)
