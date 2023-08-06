3c0723ddec72f5122760dcce2a7cd14293329154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:28 +0000

    Add package git-notion-pretty/1.0.0/git_notion_pretty-1.0.0-py2.py3-none-any.whl

---
+++ packages/git-notion-pretty/git_notion_pretty-1.0.0-py2.py3-none-any.whl/git_notion_pretty/cli.py
@@ -0,0 +1,30 @@
+"""
+Git Notion
+
+Usage:
+  git_notion.py [options]
+
+Options:
+    -h --help                       Show this screen.
+    -s --page-structure=<value>     The structure of the pages in Notion. (nested|flat) [default: nested]
+    -d --dir-name=<value>           The name of the directory to save the documents to.
+    -f --use-file-name              Use the file name as the page title, instead of the first h1 in the file.
+"""
+
+import sys
+from docopt import docopt
+import git_notion
+
+def main():
+    """Console script for git_notion"""
+    args = docopt(__doc__)
+
+    # convert all the arguments to the correct type
+    if args["--dir-name"] == "None":
+        args["--dir-name"] = None
+
+    git_notion.sync_to_notion(args["--dir-name"], args["--use-file-name"], page_structure=args["--page-structure"])
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
