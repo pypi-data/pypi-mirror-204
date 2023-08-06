28f59a4a519e90abefc804683b44cde75011ad9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:58 +0000

    Add package prodia-python/1.0/prodia-python-1.0.tar.gz

---
+++ packages/prodia-python/prodia-python-1.0.tar.gz/prodia-python-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prodia-python',
+version='1.0',
+description='Prodia API Python Wrapper',
+long_description='README.md',
+author='yoou3-cyber',
+author_email='zenafey@eugw.ru',
+packages=['prodia'],
+license='MIT',
+install_requires=['requests'])
