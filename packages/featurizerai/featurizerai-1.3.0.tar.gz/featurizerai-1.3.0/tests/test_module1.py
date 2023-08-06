5346baaf61e99a3cfc6eb3814cb5751c64f3e399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:47 +0000

    Add package featurizerai/1.3.0/featurizerai-1.3.0.tar.gz

---
+++ packages/featurizerai/featurizerai-1.3.0.tar.gz/featurizerai-1.3.0/tests/test_module1.py
@@ -0,0 +1,25 @@
+from ..src.featurizerai.features.create_stream_source import create_stream_source
+
+def main():
+    kafka_connection = {
+        "bootstrap.servers": "buraks-air.lan:9092",
+        "group.id": "your-group-id",
+        "auto.offset.reset": "earliest"
+    }
+
+    # Optional: Provide custom MongoDB connection settings
+    mongo_connection = {
+        'uri': "mongodb+srv://admin:6lHqq9LqgwDlninK@cluster0.aqtcyq2.mongodb.net/?retryWrites=true&w=majority",
+        'database': 'kafkastream',
+        'rawcollection': 'rawdata',
+        'collection': 'sparkaggregate'
+    }
+
+    # Initialize the FeaturizerAI SDK with the Kafka and Mongo connections
+    featurizer_ai = create_stream_source(kafka_connection, mongo_connection)
+
+    # Run the FeaturizerAI processing
+    featurizer_ai.run()
+
+if __name__ == "__main__":
+    main()
