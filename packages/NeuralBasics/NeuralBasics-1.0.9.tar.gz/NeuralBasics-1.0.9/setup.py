ba4bcc15ec75e780652e652b02745dd8b181fe92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:34 +0000

    Add package NeuralBasics/1.0.9/NeuralBasics-1.0.9.tar.gz

---
+++ packages/NeuralBasics/NeuralBasics-1.0.9.tar.gz/NeuralBasics-1.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NeuralBasics",
+    version="1.0.9",
+    package_dir={"": "src"},
+    packages=find_packages(exclude=['test'], where='src'),
+    description='Neural network basics module. Number detection on images.',
+    author='Henry Cook',
+    author_email='henryscookolaizola@gmail.com',
+    install_requires=['numpy', 'alive_progress', 'Pillow', 'matplotlib', 'idx2numpy']
+)
