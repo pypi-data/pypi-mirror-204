38ce64440ad8eed80cc6e09cc3aba02ba21e1766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:25 +0000

    Add package kmRead/0.1.7/kmRead-0.1.7.tar.gz

---
+++ packages/kmRead/kmRead-0.1.7.tar.gz/kmRead-0.1.7/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: lixuecheng
+# Mail: lixuechengde@163.com
+# Created Time: 2021年8月23日
+#############################################
+
+
+from setuptools import setup, find_packages
+
+
+
+setup(
+name = "kmRead",
+version = "0.1.7",
+keywords = ["pip", "km","mind","csv"],
+description = "把km、zxm和xmind文件转换成表格文件",
+long_description = "把km、zxm和xmind文件转换成表格文件",
+license = "MIT Licence",
+
+url = "https://gitee.com/tuboyou/km-read",
+author = "lixuecheng",
+author_email = "lixuechengde@163.com",
+packages = find_packages(),
+include_package_data = True,
+platforms = "any",
+install_requires = ['openpyxl'],
+py_modules=['kmRead'],
+entry_points={
+'console_scripts': ['km=kmRead.kmRead:main','kme=kmRead.kmRead:maine','kmi=kmRead.kmRead:mainInit']
+}
+)
+import os
+os.system("kmi")
