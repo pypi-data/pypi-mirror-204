26e996860e786b8e17df30be5952014a0675be72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:26 +0000

    Add package Products.CMFPlone/6.0.4/Products.CMFPlone-6.0.4-py3-none-any.whl

---
+++ packages/Products.CMFPlone/Products.CMFPlone-6.0.4-py3-none-any.whl/Products/CMFPlone/tests/testControlPanelScripts.py
@@ -0,0 +1,28 @@
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from Products.CMFPlone.tests.PloneTestCase import PloneTestCase
+
+
+class TestAccessControlPanelScripts(PloneTestCase):
+    '''Yipee, functional tests'''
+
+    def afterSetUp(self):
+        self.portal_path = self.portal.absolute_url(1)
+        self.basic_auth = f'{SITE_OWNER_NAME}:{SITE_OWNER_PASSWORD}'
+
+    def testUserInformation(self):
+        '''Test access to user details.'''
+        response = self.publish('%s/@@user-information?userid=%s' %
+                                (self.portal_path, TEST_USER_ID),
+                                self.basic_auth)
+
+        self.assertEqual(response.getStatus(), 200)
+
+    def testUserPreferences(self):
+        '''Test access to user details.'''
+        response = self.publish('%s/@@user-preferences?userid=%s' %
+                                (self.portal_path, TEST_USER_ID),
+                                self.basic_auth)
+
+        self.assertEqual(response.getStatus(), 200)
