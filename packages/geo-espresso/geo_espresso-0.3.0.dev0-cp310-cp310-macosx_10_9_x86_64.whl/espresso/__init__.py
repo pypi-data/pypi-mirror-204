36009f9f6b049f4245e67574bdd875dd89a360ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:37 +0000

    Add package geo-espresso/0.3.0.dev0/geo_espresso-0.3.0.dev0-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/geo-espresso/geo_espresso-0.3.0.dev0-cp310-cp310-macosx_10_9_x86_64.whl/espresso/__init__.py
@@ -0,0 +1,67 @@
+r"""
+
+EspressoProblem
+---------------
+
+The essential idea of Espresso is to have exactly same interface for different 
+inversion test problems, so we list all the standard methods and attributes once in 
+this API reference page. Check out documentation for :class:`EspressoProblem` for 
+details.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+
+    EspressoProblem
+
+List the problems
+-----------------
+
+To programatically see what problems are available in Espresso, these functions are 
+helpful:
+
+.. autofunction:: list_problems
+.. autofunction:: list_problem_names
+
+Utility functions
+-----------------
+
+Some utility functions are there to help contributors load data and calculate things.
+Check out documentation for submodule :mod:`espresso.utils` for details.
+
+.. autosummary::
+    :toctree: generated/
+    :nosignatures:
+    
+    espresso.utils
+
+"""
+
+from ._espresso_problem import EspressoProblem
+
+
+from ._version import __version__
+
+
+__all__ = [
+    "EspressoProblem",
+]
+
+# from .example_name import ExampleName
+
+# from .list_problems import list_problem_names, list_problems
+# __all__ += list_problem_names()
+# __all__ += ["list_problem_names", "list_problems"]
+
+from ._magnetotelluric_1D import Magnetotelluric1D
+from ._slug_test import SlugTest
+from ._fm_wavefront_tracker import FmWavefrontTracker
+from ._gravity_density import GravityDensity
+from ._pumping_test import PumpingTest
+from ._xray_tracer import XrayTracer
+from ._receiver_function import ReceiverFunction
+from ._simple_regression import SimpleRegression
+
+from .list_problems import list_problem_names, list_problems
+__all__ += list_problem_names()
+__all__ += ['list_problem_names', 'list_problems']
