5b7b30bed96c8d9dae86537a92c7c7c3d95d64d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:22 +0000

    Add package upytl/0.0.9/upytl-0.0.9-py3-none-any.whl

---
+++ packages/upytl/upytl-0.0.9-py3-none-any.whl/upytl/__init__.py
@@ -0,0 +1,23 @@
+from . import html
+from .core import Tag, MetaTag, Component, Slot, SlotTemplate, UHelper, UPYTL, Template, gtag, SlotsEnum
+from .xtemplate import XTemplate
+
+
+__all__ = (
+    'html',
+    'Tag',
+    'MetaTag',
+    'Component',
+    'Slot',
+    'SlotTemplate',
+    'UHelper',
+    'UPYTL',
+    'Template',
+    'XTemplate',
+    'gtag',
+    'SlotsEnum',
+)
+
+__author__ = "Kucherov Valery <valq7711@gmail.com>"
+__license__ = "MIT"
+__version__ = "0.0.9"
