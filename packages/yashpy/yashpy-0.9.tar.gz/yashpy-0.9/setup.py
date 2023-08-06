097caa49edd9f0f971fba37d66cdcd1993f49971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:07 +0000

    Add package yashpy/0.9/yashpy-0.9.tar.gz

---
+++ packages/yashpy/yashpy-0.9.tar.gz/yashpy-0.9/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+setup(name="yashpy",version="0.9",description="this is mathematics perps lybrary",
+      long_description="this is very cool",
+      author="yshirsath466@gmail.com",
+      packages=['yashpy'],install_requires=[])
