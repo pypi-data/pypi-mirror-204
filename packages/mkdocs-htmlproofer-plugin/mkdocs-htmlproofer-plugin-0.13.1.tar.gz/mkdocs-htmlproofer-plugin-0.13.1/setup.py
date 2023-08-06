9d63f6a59d03470a9b0a1488850ba1762b20e44e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:24 +0000

    Add package mkdocs-htmlproofer-plugin/0.13.1/mkdocs-htmlproofer-plugin-0.13.1.tar.gz

---
+++ packages/mkdocs-htmlproofer-plugin/mkdocs-htmlproofer-plugin-0.13.1.tar.gz/mkdocs-htmlproofer-plugin-0.13.1/setup.py
@@ -0,0 +1,47 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(fname: str):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='mkdocs-htmlproofer-plugin',
+    version='0.13.1',
+    description='A MkDocs plugin that validates URL in rendered HTML files',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs python markdown',
+    url='https://github.com/manuzhang/mkdocs-htmlproofer-plugin',
+    author='Manu Zhang',
+    author_email='owenzhang1990@gmail.com',
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'mkdocs>=0.17',
+        'Markdown',
+        'requests',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    entry_points={
+        'mkdocs.plugins': [
+            'htmlproofer = htmlproofer.plugin:HtmlProoferPlugin'
+        ]
+    }
+)
