81f20ec4dcb1dc725f5d448bf0a6d8dcc0e3ccdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:54 +0000

    Add package sqlstrings/0.1.3/sqlstrings-0.1.3.tar.gz

---
+++ packages/sqlstrings/sqlstrings-0.1.3.tar.gz/sqlstrings-0.1.3/sqlstrings/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["csv_handling", "postgre", "transact", "value_handling"]
+import sqlstrings.csv_handling
+import sqlstrings.value_handling
+import sqlstrings.transact
+import sqlstrings.postgre
