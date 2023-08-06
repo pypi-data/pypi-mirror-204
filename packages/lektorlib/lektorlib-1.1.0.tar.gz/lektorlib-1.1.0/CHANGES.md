c963b50890a896ff2514211f785d11ec58386a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:36 +0000

    Add package lektorlib/1.1.0/lektorlib-1.1.0.tar.gz

---
+++ packages/lektorlib/lektorlib-1.1.0.tar.gz/lektorlib-1.1.0/CHANGES.md
@@ -0,0 +1,21 @@
+## Changelog
+
+### Release 1.1.0 (2023-04-20)
+
+Fixes for Lektor 3.4 (in pre-release).
+
+We now test under Lektor 3.3 and Lektor 3.4 (pre-release).
+
+### Release 1.0 (2022-01-28)
+
+Drop support for python < 3.7. Test under python 3.10.
+
+### Release 0.1 (2021-02-05)
+
+No code changes.
+
+Release is now classified as "production/stable".
+
+### Release 0.1a1 (2020-05-05)
+
+Initial release.
---
+++ packages/lektorlib/lektorlib-1.1.0.tar.gz/lektorlib-1.1.0/lektorlib/context.py
@@ -0,0 +1,42 @@
+"""Helpers
+
+"""
+from contextlib import contextmanager
+
+from lektor.context import (
+    get_ctx,
+    Context,
+    )
+
+
+@contextmanager
+def disable_dependency_recording():
+    """Disable dependency recording within context
+
+    We do this by pushing a new context onto the context stack.  The
+    new context proxies all access — except for depency reporting —
+    back to the original context.
+
+    """
+    ctx = get_ctx()
+    if ctx is None:
+        yield
+    else:
+        with DependencyIgnoringContextProxy(ctx):
+            yield
+
+
+class DependencyIgnoringContextProxy(Context):
+    __slots__ = ['_ctx']
+
+    def __init__(self, ctx):
+        self._ctx = ctx
+
+    def __getattr__(self, attr):
+        return getattr(self._ctx, attr)
+
+    def record_dependency(self, filename, affects_url=None):
+        pass
+
+    def record_virtual_dependency(self, virtual_source):
+        pass
