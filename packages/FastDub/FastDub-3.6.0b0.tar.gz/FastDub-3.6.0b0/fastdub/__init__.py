5afb3dae76979c745117a4875910c8b79d4cc402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:41 +0000

    Add package FastDub/3.6.0b0/FastDub-3.6.0b0.tar.gz

---
+++ packages/FastDub/FastDub-3.6.0b0.tar.gz/FastDub-3.6.0b0/fastdub/__init__.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from multiprocessing import cpu_count
+
+__all__ = ('PrettyViewPrefix', 'GlobalSettings')
+
+
+class GlobalSettings:
+    __slots__ = ()
+    threads_count = cpu_count()
+    language = 'ru'
+    watermark = ''
+
+
+class PrettyViewPrefix:
+    """Class for representing a number as units"""
+    __slots__ = ()
+
+    @staticmethod
+    def from_any(size: float, div: float, base: str, prefixes) -> str:
+        for prefix in prefixes[:-1]:
+            if size < div:
+                return f'{size:,g}{prefix}{base}'
+            size /= div
+        return f'{size:,g}{prefixes[-1]}{base}'
+
+    @classmethod
+    def from_bytes(cls, size: float) -> str:
+        return cls.from_any(size, 1000., 'B', ('', 'k', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'))
+
+    @classmethod
+    def from_seconds(cls, size: float) -> str:
+        return cls.from_any(size, 60., '', 'smh')
