582f7099b57454af3d6aa08e1d2ef5b5e1071288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:30 +0000

    Add package ta-cmi/2.0.1/ta-cmi-2.0.1.tar.gz

---
+++ packages/ta-cmi/ta-cmi-2.0.1.tar.gz/ta-cmi-2.0.1/ta_cmi/channel.py
@@ -0,0 +1,32 @@
+from .const import UNITS_DE, UNITS_EN, ChannelMode, ChannelType, Languages
+
+
+class Channel:
+    """Class to display a data point"""
+
+    def __init__(self) -> None:
+        """Initialize and parse json to get properties."""
+        self.mode: ChannelType | None = None
+        self.type: ChannelMode = ChannelMode.ANALOG
+        self.index: int = 0
+        self.value: float = 0
+        self.unit: str = ""
+
+    def get_unit(self, language: Languages = Languages.EN) -> str:
+        """Get the unit of the channel."""
+        if language == Languages.EN:
+            return UNITS_EN.get(self.unit, "Unknown")
+        else:
+            return UNITS_DE.get(self.unit, "Unknown")
+
+    def __eq__(self, other) -> bool:
+        return (
+            self.mode == other.mode
+            and self.type == other.type
+            and self.index == other.index
+            and self.value == other.value
+            and self.unit == other.unit
+        )
+
+    def __repr__(self) -> str:
+        return f"Channel {self.index}: Type: {self.type}, Mode: {self.mode}, Value: {self.value} {self.get_unit()}"
