81e315b69b7624df3930da1bfa12d9c71f834817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:59 +0000

    Add package perspdf/1.0/perspdf-1.0-py3-none-any.whl

---
+++ packages/perspdf/perspdf-1.0-py3-none-any.whl/perspdf/pdf2txt.py
@@ -0,0 +1,15 @@
+class Convert:
+    """This class convert pdf to txt."""
+
+    def txt(self, path):
+        """This function get the pdf path and convert it to a txt.
+
+            parameter:
+            path(str): It take the pdf path address.
+
+            Returns:
+            it returns a txt type file.
+
+
+        """
+        print("converting")
