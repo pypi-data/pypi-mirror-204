33b4a7fed3a2fac9c8d77ba224bc10100244e6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:01 +0000

    Add package kozh-fig-pac2/0.1/kozh_fig_pac2-0.1.tar.gz

---
+++ packages/kozh-fig-pac2/kozh_fig_pac2-0.1.tar.gz/kozh_fig_pac2-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kozh_fig_pac2',
+      version='0.1',
+      description='circle, tiangle, square',
+      packages=['kozh_fig_pac2'],
+      author_email='alexeykozhakin@gmail.com',
+      zip_safe=False)
