b8285d33b9b5d6e262b510c042f8985a6a1e24dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package image-to-arduino/1.0/image_to_arduino-1.0.tar.gz

---
+++ packages/image-to-arduino/image_to_arduino-1.0.tar.gz/image_to_arduino-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+REQUIREMENTS = [i.strip() for i in open("/home/wiktor/Image_Converter_App/requirements.txt").readlines()]
+LONG_DESCRIPTION = 'About Image converter GUI App to arduino oled display ssd1306 128x64'
+setup(
+   name='image_to_arduino',
+   version='1.0',
+   description='Image converter to arduino',
+   license="MIT",
+   author='WiktorK02',
+   author_email='wiktor.kidon@hotmail.com',
+   url="https://github.com/WiktorK02/Image_Converter_App.git",
+   long_description_content_type="text/markdown",
+   long_description=LONG_DESCRIPTION,
+   packages=find_packages('src', exclude=['image_to_arduino']),
+   install_requires=REQUIREMENTS, 
+
+)
