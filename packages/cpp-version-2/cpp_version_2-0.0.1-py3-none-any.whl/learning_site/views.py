cc74d4896fa46c2d29a31676ef8913dd7836566c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:11 +0000

    Add package cpp-version-2/0.0.1/cpp_version_2-0.0.1-py3-none-any.whl

---
+++ packages/cpp-version-2/cpp_version_2-0.0.1-py3-none-any.whl/learning_site/views.py
@@ -0,0 +1,31 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+from django.shortcuts import render
+from .import forms
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def suggestion_view(request):
+    form = forms.SuggestionForm()
+    if request.method == 'POST':
+        form = forms.SuggestionForm(request.POST)
+
+
+        print(form)
+        print(form.is_valid())
+        if form.is_valid():
+            print('send email started')
+            # send_mail(
+            #     'Suggestion from {}'.format(form.cleaned_data['name']), # Subject
+            #     form.cleaned_data['suggestion'],  # Body
+            #     '{name} <{email}>'.format(**form.cleaned_data),   # From email
+            #     ['sulkeaditi@gmail.com'],  # Sent To
+            # )
+            messages.add_message(request, messages.SUCCESS, 'Thanks For Your Suggestion!')
+            return HttpResponseRedirect(reverse('suggestion'))
+    return render(request, 'suggestion_form.html', {'form': form})
