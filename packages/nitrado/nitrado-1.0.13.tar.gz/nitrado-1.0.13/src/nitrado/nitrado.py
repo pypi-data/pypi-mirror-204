e4ec46bef89fc3a711822af5ca33299271aca99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:32 +0000

    Add package nitrado/1.0.13/nitrado-1.0.13.tar.gz

---
+++ packages/nitrado/nitrado-1.0.13.tar.gz/nitrado-1.0.13/src/nitrado/nitrado.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+from .lib import Client
+from .service import Service
+from .gameserver import GameServer
+from pathlib import Path
+
+
+def initialize(api_key: str):
+    """Saves the Nitrado API key in a local .env file"""
+    if not Path('.env').exists():
+        Path('.env').touch()
+    with open('.env', 'r+') as r:
+        content = []
+        found = False
+        for line in r.readlines():
+            if line.find(f'{Client.ENV_NAME}=') == 0:
+                content += [f"{Client.ENV_NAME}={api_key}\n"]
+                found = True
+            else:
+                content += [line]
+        if not found:
+            content = [f"{Client.ENV_NAME}={api_key}\n"] + content
+        r.seek(0)
+        r.write(''.join(content))
+        load_dotenv()
+
+
+def services() -> list[Service]:
+    return Service.all()
+
+
+def gameservers() -> list[GameServer]:
+    return GameServer.all()
+
+
+def gameserver_by_service_id(service_id: int) -> GameServer:
+    return GameServer.find_by_id(service_id)
+
+
+def service_by_id(service_id: int) -> Service:
+    return Service.find_by_id(service_id)
+
+
