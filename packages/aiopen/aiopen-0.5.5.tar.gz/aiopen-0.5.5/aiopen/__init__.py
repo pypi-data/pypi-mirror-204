f839c7528e3214aa319d5f24c3cce5238fb33ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:47 +0000

    Add package aiopen/0.5.5/aiopen-0.5.5.tar.gz

---
+++ packages/aiopen/aiopen-0.5.5.tar.gz/aiopen-0.5.5/aiopen/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""`aiopen` ~ Async version of python's built in open -- based on aiofiles"""
+from aiopen.__about__ import __version__
+from aiopen.core import aiopen
+from funkify import funkify
+
+funkify(aiopen, key="aiopen")
+
+__all__ = (
+    "__version__",
+    "aiopen",
+)
