09145876d8b091e1a8cff077a8c8b9819c209191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:35 +0000

    Add package neatmesh/0.0.7/neatmesh-0.0.7.tar.gz

---
+++ packages/neatmesh/neatmesh-0.0.7.tar.gz/neatmesh-0.0.7/src/neatmesh/_common.py
@@ -0,0 +1,73 @@
+"""neatmesh common constants and functions"""
+
+from meshio import Mesh as MeshioMesh
+
+meshio_3d_elements = {
+    "tetra",
+    "tetra10",
+    "hexahedron",
+    "hexahedron20",
+    "hexahedron24",
+    "hexahedron27",
+    "wedge",
+    "wedge12",
+    "wedge15",
+    "pyramid",
+    "pyramid13",
+    "pyramid14",
+}
+
+meshio_type_to_alpha = {
+    "vertex": "vertex",
+    "line": "line",
+    "line3": "line",
+    "triangle": "triangle",
+    "triangle6": "triangle",
+    "triangle7": "triangle",
+    "quad": "quad",
+    "quad8": "quad",
+    "quad9": "quad",
+    "tetra": "tetra",
+    "tetra10": "tetra",
+    "hexahedron": "hexahedron",
+    "hexahedron20": "hexahedron",
+    "hexahedron24": "hexahedron",
+    "hexahedron27": "hexahedron",
+    "wedge": "wedge",
+    "wedge12": "wedge",
+    "wedge15": "wedge",
+    "pyramid": "pyramid",
+    "pyramid13": "pyramid",
+    "pyramid14": "pyramid",
+}
+
+meshio_2d_elements = {
+    "triangle",
+    "quad",
+}
+
+meshio_1d_elements = {
+    "vertex",
+    "line",
+    "line3",
+}
+
+
+def is_3d_mesh(mesh: MeshioMesh) -> bool:
+    """Check if a meshio.mesh is 3-dimensional"""
+    for cell_block in mesh.cells:
+        # first 3D element type is enough.
+        if meshio_type_to_alpha[cell_block.type] in meshio_3d_elements:
+            return True
+    return False
+
+
+def is_2d_mesh(mesh: MeshioMesh) -> bool:
+    """Check if a meshio.mesh is 2-dimensional"""
+    if is_3d_mesh(mesh):
+        return False
+
+    for cell_block in mesh.cells:
+        if meshio_type_to_alpha[cell_block.type] in meshio_2d_elements:
+            return True
+    return False
