7b7ad57705891e8c269218e9837e579dca266021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:05 +0000

    Add package lovehim/0.53/lovehim-0.53-py3-none-any.whl

---
+++ packages/lovehim/lovehim-0.53-py3-none-any.whl/lovehim/__main__.py
@@ -0,0 +1,18 @@
+import argparse
+import openai
+from .get_verse import generate_verse
+
+# Set up the OpenAI API key
+openai.api_key = "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"
+
+# Parse the command-line arguments
+parser = argparse.ArgumentParser(description='Get a Bible verse based on a topic')
+parser.add_argument('topic', nargs='+', help='the topic you want the verse to be about')
+args = parser.parse_args()
+
+# Call the generate_verse function with the user-specified topic
+topic = ' '.join(args.topic)
+verse = generate_verse(topic)
+
+# Print out the resulting Bible verse
+print(verse)
