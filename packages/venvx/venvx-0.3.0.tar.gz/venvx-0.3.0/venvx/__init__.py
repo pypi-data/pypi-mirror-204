9bf1714dfbbce08a6703a3e9821a41f471fe3b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:25 +0000

    Add package venvx/0.3.0/venvx-0.3.0.tar.gz

---
+++ packages/venvx/venvx-0.3.0.tar.gz/venvx-0.3.0/venvx/__init__.py
@@ -0,0 +1,11 @@
+'''
+Easy-to-use virtual environment manager for bash and zsh
+
+:copyright: (c) 2023 Ralf Luetkemeier
+:license: MIT, see LICENSE for more details.
+'''
+
+__version__ = "0.3.0"
+
+
+from venvx.virtualenv import VenvError, VirtualEnv as Venv   # noqa: F401
