b81979dd5d5091d34e30fac8de45b85269158895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:23 +0000

    Add package pyfbsdk-stub-generator/1.0.0/pyfbsdk-stub-generator-1.0.0.tar.gz

---
+++ packages/pyfbsdk-stub-generator/pyfbsdk-stub-generator-1.0.0.tar.gz/pyfbsdk-stub-generator-1.0.0/pyfbsdk_stub_generator/base_content/pyfbsdk.pyi
@@ -0,0 +1,12 @@
+"""
+Stub file generated for MotionBuilder {MOTIONBUILDER_VERSION} using:
+https://github.com/nils-soderman/pyfbsdk-stub-generator
+"""
+# pylint: disable=all
+from __future__ import annotations
+from typing import overload, Any
+import callbackframework
+class Enumeration:
+    __slots__:tuple
+    names:dict
+    values:dict
