c7e682260cd49f152341b4ea9e2d072406c04c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:52 +0000

    Add package prettyqt/1.17.0/prettyqt-1.17.0-py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-1.17.0-py3-none-any.whl/prettyqt/core/metamethod.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import Literal
+
+from prettyqt.qt import QtCore
+from prettyqt.utils import bidict
+
+
+ACCESS = bidict(
+    private=QtCore.QMetaMethod.Access.Private,
+    protected=QtCore.QMetaMethod.Access.Protected,
+    public=QtCore.QMetaMethod.Access.Public,
+)
+
+AccessStr = Literal["private", "protected", "public"]
+
+METHOD_TYPE = bidict(
+    method=QtCore.QMetaMethod.MethodType.Method,
+    signal=QtCore.QMetaMethod.MethodType.Signal,
+    slot=QtCore.QMetaMethod.MethodType.Slot,
+    constructor=QtCore.QMetaMethod.MethodType.Constructor,
+)
+
+MethodTypeStr = Literal["method", "signal", "slot", "constructor"]
+
+
+class MetaMethod:
+    def __init__(self, metamethod: QtCore.QMetaMethod):
+        self.item = metamethod
+
+    def __getattr__(self, val):
+        return getattr(self.item, val)
+
+    def __bool__(self):
+        return self.item.isValid()
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.get_name()!r})"
+
+    def get_access(self) -> AccessStr:
+        return ACCESS.inverse[self.item.access()]
+
+    def get_method_type(self) -> MethodTypeStr:
+        return METHOD_TYPE.inverse[self.item.methodType()]
+
+    def get_method_signature(self) -> str:
+        return self.item.methodSignature().data().decode()
+
+    def get_name(self) -> str:
+        return self.item.name().data().decode()
+
+
+if __name__ == "__main__":
+    from prettyqt import core
+
+    metaobj = core.Object.get_metaobject()
+    method = metaobj.get_method("objectNameChanged")
+    print(method.get_name())
---
+++ packages/prettyqt/prettyqt-1.17.0-py3-none-any.whl/prettyqt/gui/textframe.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from prettyqt import gui
+from prettyqt.qt import QtGui
+
+
+class TextFrame(gui.TextObjectMixin, QtGui.QTextFrame):
+    def __repr__(self):
+        return f"{type(self).__name__}()"
+
+    def get_first_cursor_position(self) -> gui.TextCursor:
+        return gui.TextCursor(self.firstCursorPosition())
+
+    def get_last_cursor_position(self) -> gui.TextCursor:
+        return gui.TextCursor(self.lastCursorPosition())
+
+
+if __name__ == "__main__":
+    doc = gui.TextDocument()
+    frame = TextFrame(doc)
