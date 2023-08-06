f8682278ec4b38e37fade7399008f8008fc999ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:34 +0000

    Add package sql-test-kit/0.2.3/sql_test_kit-0.2.3.tar.gz

---
+++ packages/sql-test-kit/sql_test_kit-0.2.3.tar.gz/sql_test_kit-0.2.3/sql_test_kit/__init__.py
@@ -0,0 +1,7 @@
+from sql_test_kit.table import Table
+from sql_test_kit.column import Column
+from sql_test_kit.query_interpolation import (
+    InterpolationData,
+    replace_table_names_in_string_by_data_literals,
+    get_data_literals_query,
+)
