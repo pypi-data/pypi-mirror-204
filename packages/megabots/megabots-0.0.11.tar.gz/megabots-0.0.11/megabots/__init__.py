ae27ade1d3aed942f0ee9ed9cd72a0553b1a1d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:58 +0000

    Add package megabots/0.0.11/megabots-0.0.11.tar.gz

---
+++ packages/megabots/megabots-0.0.11.tar.gz/megabots-0.0.11/megabots/__init__.py
@@ -0,0 +1,10 @@
+from megabots.vectorstore import VectorStore, vectorstore
+from megabots.memory import Memory, memory
+from megabots.bot import Bot, bot
+from megabots.prompt import prompt
+from megabots.utils import create_api, create_interface
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
