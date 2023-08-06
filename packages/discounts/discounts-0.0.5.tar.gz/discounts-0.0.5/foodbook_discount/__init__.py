98d949a723421e072bdf9f3397f676e4d12ee72e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:33 +0000

    Add package discounts/0.0.5/discounts-0.0.5.tar.gz

---
+++ packages/discounts/discounts-0.0.5.tar.gz/discounts-0.0.5/foodbook_discount/__init__.py
@@ -0,0 +1,15 @@
+import discounts
+
+class Discount:
+    def __init__(self, percentage):
+        self.percentage = percentage
+        
+    def apply_discount(self, category):
+        for item in category.items:
+            item.price = item.price * (1 - self.percentage / 100)
+
+def apply_discount_to_(category, discount):
+    discount.apply_discount(category)
+
+
+
