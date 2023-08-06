856e66fd92407027dd71b24514d09ea3f74630d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:55 +0000

    Add package SocketSwap/0.1.4/SocketSwap-0.1.4-py2.py3-none-any.whl

---
+++ packages/SocketSwap/SocketSwap-0.1.4-py2.py3-none-any.whl/SocketSwap/context_manager.py
@@ -0,0 +1,31 @@
+"""
+Module to wrap the TCP proxy in a context manager daemon process
+"""
+import multiprocessing
+import socketswap.proxy as proxy
+import time
+import logging
+
+logger = logging.getLogger("socketswap")
+logger.addHandler(logging.NullHandler())
+
+
+class SocketSwapContext:
+    
+    def __init__(self, *args):
+        self.args = args
+    
+    def __enter__(self):
+        logger.info("Starting Proxy Server")
+        self.proxy_process = multiprocessing.Process(target=proxy.start_local_proxy, args=(self.args))
+        self.proxy_process.daemon = True
+        self.proxy_process.start()
+        time.sleep(1)
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        logger.info("Exiting proxy server")
+        self.proxy_process.terminate()
+        
+
+        
