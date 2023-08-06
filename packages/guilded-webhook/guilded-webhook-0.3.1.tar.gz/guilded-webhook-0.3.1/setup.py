bc3ed8e942f827418df752e09a2a2b5278bd2081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:04 +0000

    Add package guilded-webhook/0.3.1/guilded-webhook-0.3.1.tar.gz

---
+++ packages/guilded-webhook/guilded-webhook-0.3.1.tar.gz/guilded-webhook-0.3.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import pathlib
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+setup(name='guilded-webhook',
+    version='0.3.1',
+    description='guilded-webhook is a basic wrapper for guilded\'s webhooks.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='anytarseir67',
+    url='https://github.com/anytarseir67/guilded-webhook',
+    license="GPLv3",
+    packages=['guilded_webhook'],
+    install_requires=requirements,
+    )
