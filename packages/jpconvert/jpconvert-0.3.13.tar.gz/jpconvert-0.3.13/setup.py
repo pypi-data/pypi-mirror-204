3452abef091d0cd485843af58b62a83be8159fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:23 +0000

    Add package jpconvert/0.3.13/jpconvert-0.3.13.tar.gz

---
+++ packages/jpconvert/jpconvert-0.3.13.tar.gz/jpconvert-0.3.13/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='jpconvert',
+    version='0.3.13',
+    author='Eric Tröbs',
+    author_email='eric.troebs@tu-ilmenau.de',
+    description='macros for Jupyter Notebooks',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/erictroebs/jpconvert',
+    project_urls={
+        'Bug Tracker': 'https://github.com/erictroebs/jpconvert/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.6',
+    install_requires=[
+        'requests==2.28.2',
+        'python-magic==0.4.27'
+    ]
+)
