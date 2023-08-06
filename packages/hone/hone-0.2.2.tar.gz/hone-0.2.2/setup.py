93478eeff95b279ca5a1b7f11033d4ea19a91d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:52 +0000

    Add package hone/0.2.2/hone-0.2.2.tar.gz

---
+++ packages/hone/hone-0.2.2.tar.gz/hone-0.2.2/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='hone',
+    version='0.2.2',
+    author_email='chamkdev@gmail.com',
+    license='MIT',
+    description='Convert CSV to automatically nested JSON.',
+    keywords="convert csv auto nested json",
+    url='https://github.com/chamkank/hone',
+    project_urls={
+        "Source Code": 'https://github.com/chamkank/hone'
+    },
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.0',
+    entry_points={'console_scripts': ['hone=hone.__main__:main']},
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
+)
