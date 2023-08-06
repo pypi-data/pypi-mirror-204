f887af890e61d2b7648287c502711638a67c1186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package AEEGGOOSS/0.2/AEEGGOOSS-0.2.tar.gz

---
+++ packages/AEEGGOOSS/AEEGGOOSS-0.2.tar.gz/AEEGGOOSS-0.2/code/__init__.py
@@ -0,0 +1,4 @@
+import os
+class a:
+    def ae(m,b):
+        return {'Aegos':m+b}
---
+++ packages/AEEGGOOSS/AEEGGOOSS-0.2.tar.gz/AEEGGOOSS-0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name= 'AEEGGOOSS',
+    version='0.2',
+    author= 'AegosCOOOD',
+    description='Gamaa',
+    packages= setuptools.find_packages(),
+    install_requirs=[],
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
