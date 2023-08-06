b7bfc0ff4e6635274994d36f464ab48fb4180866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:31 +0000

    Add package sqlstrings/0.1.4/sqlstrings-0.1.4-py3-none-any.whl

---
+++ packages/sqlstrings/sqlstrings-0.1.4-py3-none-any.whl/sqlstrings/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["csv_handling", "postgre", "transact", "value_handling"]
+from sqlstrings import csv_handling
+from sqlstrings import value_handling
+from sqlstrings import transact
+from sqlstrings import postgre
