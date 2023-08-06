0e3ddb08d30b5173ad1effb221405f9ebf780dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:03 +0000

    Add package realnet/0.0.90/realnet-0.0.90-py3.10.egg

---
+++ packages/realnet/realnet-0.0.90-py3.10.egg/realnet/runner/http/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, Response, redirect, render_template, render_template_string, request, jsonify, Blueprint, send_file, session
+from flask_cors import CORS
+from flask_bootstrap import Bootstrap
+
+import json
+import jinja2
+
+import logging
+
+from realnet.core.config import Config
+from .auth import config_oauth
+from .router import router_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+def create_app(contextProvider):
+    logging.basicConfig(level=logging.DEBUG)
+
+    cfg = Config()
+
+    app = Flask(__name__)
+    CORS(app)
+    # import realnet_server.wsgi
+
+    app.secret_key = cfg.get_app_secret()
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+    app.config['REALNET_CONTEXT_PROVIDER'] = contextProvider
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+
+    app.jinja_loader = jinja2.ChoiceLoader([
+        app.jinja_loader,
+        jinja2.PackageLoader('realnet') # in the same folder will search the 'templates' folder
+    ])
+
+    app.jinja_env.filters['from_json'] = json.dumps
+    
+    bootstrap = Bootstrap(app)
+
+    config_oauth(app)
+
+    app.register_blueprint(router_bp)
+
+    return app
