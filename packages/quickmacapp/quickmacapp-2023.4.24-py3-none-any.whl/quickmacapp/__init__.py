133d05ff5c80d9ae6d679e3fefe132e0909083b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:53 +0000

    Add package quickmacapp/2023.4.24/quickmacapp-2023.4.24-py3-none-any.whl

---
+++ packages/quickmacapp/quickmacapp-2023.4.24-py3-none-any.whl/quickmacapp/__init__.py
@@ -0,0 +1,13 @@
+from ._quickapp import Actionable, Status, mainpoint, menu, quit
+from ._interactions import ask, choose, answer
+
+__all__ = [
+    "Actionable",
+    "Status",
+    "mainpoint",
+    "menu",
+    "quit",
+    "ask",
+    "choose",
+    "answer",
+]
