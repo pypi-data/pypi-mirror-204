80034a289e8e5a9ef70068c908a2ceaaac25d910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:20 +0000

    Add package NikeCA/0.1.80/NikeCA-0.1.80.tar.gz

---
+++ packages/NikeCA/NikeCA-0.1.80.tar.gz/NikeCA-0.1.80/src/NikeCA.py
@@ -0,0 +1,15 @@
+
+from NikeSF import Snowflake
+from NikeQA import QA
+from Dashboards.Dashboards import Dashboards
+from Dashboards.InclusionExclusion.InclusionExclusion import InclusionExclusion
+from Dashboards.Telemetry import Telemetry
+
+class NikeCA(
+    Snowflake
+    , QA
+    , Dashboards
+    , InclusionExclusion
+    # , Telemetry
+):
+    pass
