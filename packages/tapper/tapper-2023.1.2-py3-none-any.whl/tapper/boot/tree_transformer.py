6c317486b90902aac84027002e64bafa47ea559d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:00 +0000

    Add package tapper/2023.1.2/tapper-2023.1.2-py3-none-any.whl

---
+++ packages/tapper/tapper-2023.1.2-py3-none-any.whl/tapper/boot/tree_transformer.py
@@ -0,0 +1,111 @@
+"""
+Transform Tree - user-configured hierarchical API structure,
+into Shadow Tree - identically structured tree with useful for runtime fields.
+"""
+from functools import partial
+from typing import Any
+
+from tapper.model import keyboard
+from tapper.model import mouse
+from tapper.model.tap_tree import Group
+from tapper.model.tap_tree import Tap
+from tapper.model.tap_tree_shadow import SGroup
+from tapper.model.tap_tree_shadow import STap
+from tapper.model.types_ import Action
+from tapper.model.types_ import KwTriggerConditions
+from tapper.model.types_ import SendFn
+from tapper.model.types_ import TriggerConditionFn
+from tapper.model.types_ import TriggerStr
+from tapper.parser.trigger_parser import TriggerParser
+
+all_symbols = [*keyboard.get_keys().keys(), *mouse.get_keys().keys()]
+
+
+def find_property(prop_name: str, group: Group) -> Any:
+    if (result := getattr(group, prop_name)) is not None:
+        return result
+    return find_property(prop_name, group._parent)  # type: ignore
+
+
+def transform_trigger_conditions(
+    possible_conditions: KwTriggerConditions, trigger_conditions: dict[str, Any]
+) -> list[TriggerConditionFn]:
+    result = []
+    for name, user_supplied_value in trigger_conditions.items():
+        if name not in possible_conditions:
+            raise ValueError(
+                f"Condition '{name}' not recognised. Add it to config.kw_trigger_conditions"
+            )
+        fn = possible_conditions[name]
+        result.append(partial(fn, user_supplied_value))
+    return result
+
+
+class TreeTransformer:
+    send: SendFn
+    trigger_parser: TriggerParser
+    possible_trigger_conditions: KwTriggerConditions
+
+    def __init__(
+        self,
+        send: SendFn,
+        trigger_parser: TriggerParser,
+        conditions: KwTriggerConditions,
+    ) -> None:
+        self.send = send  # type: ignore
+        self.trigger_parser = trigger_parser
+        self.possible_trigger_conditions = conditions
+
+    def transform(self, group: Group) -> SGroup:
+        result = SGroup()
+        result.original = group
+        result.trigger_conditions = transform_trigger_conditions(
+            self.possible_trigger_conditions, group.trigger_conditions
+        )
+
+        for child in group._children:
+            if isinstance(child, Group):
+                result.add(self.transform(child))
+            elif isinstance(child, Tap):
+                result.add(self._transform_tap(child))
+            elif isinstance(child, dict):
+                result.add(*self._transform_dict(child, group))
+            else:
+                raise TypeError(f"{child}")
+        return result
+
+    def _transform_tap(self, tap: Tap) -> STap:
+        trigger = self.trigger_parser.parse(tap.trigger)
+        if (executor := tap.executor) is None:
+            executor = find_property("executor", tap._parent)
+        if (suppress_trigger := tap.suppress_trigger) is None:
+            suppress_trigger = find_property("suppress_trigger", tap._parent)
+        action = self.to_action(tap.action)
+        result = STap(trigger, action, executor, suppress_trigger)
+        result.original = tap
+        result.trigger_conditions = transform_trigger_conditions(
+            self.possible_trigger_conditions, tap.trigger_conditions
+        )
+
+        return result
+
+    def _transform_dict(
+        self, taps: dict[TriggerStr, Action | str], parent: Group
+    ) -> list[STap]:
+        result = []
+        executor = find_property("executor", parent)
+        suppress_trigger = find_property("suppress_trigger", parent)
+        for trigger_str, action in taps.items():
+            trigger = self.trigger_parser.parse(trigger_str)
+            action = self.to_action(action)
+            stap = STap(trigger, action, executor, suppress_trigger)
+            stap.trigger_conditions = []
+            result.append(stap)
+        return result
+
+    def to_action(self, action: Action | str) -> Action:
+        if isinstance(action, str):
+            if action in all_symbols and action not in keyboard.chars_en:
+                action = f"$({action})"
+            action = partial(self.send, action)  # type: ignore
+        return action
---
+++ packages/tapper/tapper-2023.1.2-py3-none-any.whl/tapper/controller/send_processor.py
@@ -0,0 +1,112 @@
+import time
+from typing import Callable
+
+from tapper.controller.keyboard.kb_api import KeyboardController
+from tapper.controller.mouse.mouse_api import MouseController
+from tapper.model import constants
+from tapper.model import keyboard
+from tapper.model import mouse
+from tapper.model.errors import SendError
+from tapper.model.send import CursorMoveInstruction
+from tapper.model.send import KeyInstruction
+from tapper.model.send import SendInstruction
+from tapper.model.send import SleepInstruction
+from tapper.model.send import WheelInstruction
+from tapper.parser.send_parser import SendParser
+
+
+class SendCommandProcessor:
+    """Highest-level component for "send" command."""
+
+    os: str
+    parser: SendParser
+    kb_controller: KeyboardController
+    mouse_controller: MouseController
+    sleep_fn: Callable[[float], None] = time.sleep
+    default_interval: Callable[[], float]
+
+    def __init__(
+        self,
+        os: str,
+        parser: SendParser,
+        kb_controller: KeyboardController,
+        mouse_controller: MouseController,
+        default_interval: Callable[[], float],
+    ) -> None:
+        self.os = os
+        self.parser = parser
+        self.kb_controller = kb_controller
+        self.mouse_controller = mouse_controller
+        self.default_interval = default_interval  # type: ignore
+
+    @classmethod
+    def from_none(cls) -> "SendCommandProcessor":
+        """To be filled during init."""
+        return SendCommandProcessor(None, None, None, None, None)  # type: ignore
+
+    def send(
+        self, command: str, interval: float | None = None, speed: float = 1
+    ) -> None:
+        """
+        Entry point, processes the command and sends instructions.
+
+        :param command: What to send.
+        :param interval: Time before each key/button press.
+        :param speed: All sleep commands are divided by this number. Does not influence interval.
+        """
+        interval_ = interval or self.default_interval()  # type: ignore
+        instructions: list[SendInstruction] = self.parser.parse(
+            command, self.shift_down()
+        )
+        for index, instruction in enumerate(instructions):
+            if isinstance(instruction, KeyInstruction):
+                if (
+                    instruction.dir in [constants.KeyDir.DOWN, constants.KeyDir.CLICK]
+                    and interval_
+                    and index != 0
+                ):
+                    self.sleep_fn(interval_)  # type: ignore
+                self._send_key_instruction(instruction)
+            elif isinstance(instruction, WheelInstruction):
+                self.mouse_controller.press(instruction.wheel_symbol)
+            elif isinstance(instruction, CursorMoveInstruction):
+                self.mouse_controller.move(*instruction.xy, instruction.relative)
+            elif isinstance(instruction, SleepInstruction):
+                self.sleep_fn(instruction.time / speed)  # type: ignore  # https://github.com/python/mypy/issues/5485
+            else:
+                raise SendError
+
+    def shift_down(self) -> str | None:
+        """Determines which shift is down, if any."""
+        for shift in ["left_shift", "right_shift"]:
+            if self.kb_controller.pressed(shift):
+                return shift
+        return None
+
+    def _send_key_instruction(self, ki: KeyInstruction) -> None:
+        symbol = ki.symbol
+        cmd: KeyboardController | MouseController
+        if symbol in keyboard.get_keys(self.os):
+            cmd = self.kb_controller
+        elif symbol in mouse.get_keys():
+            cmd = self.mouse_controller
+        else:
+            raise SendError
+
+        if ki.dir == constants.KeyDir.DOWN:
+            cmd.press(symbol)
+        elif ki.dir == constants.KeyDir.UP:
+            cmd.release(symbol)
+        elif ki.dir == constants.KeyDir.CLICK:
+            cmd.press(symbol)
+            cmd.release(symbol)
+        elif ki.dir == constants.KeyDir.ON:
+            if not cmd.toggled(symbol):
+                cmd.press(symbol)
+                cmd.release(symbol)
+        elif ki.dir == constants.KeyDir.OFF:
+            if cmd.toggled(symbol):
+                cmd.press(symbol)
+                cmd.release(symbol)
+        else:
+            raise SendError
