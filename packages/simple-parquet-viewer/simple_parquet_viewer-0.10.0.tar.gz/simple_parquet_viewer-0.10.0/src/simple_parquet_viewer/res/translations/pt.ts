cadf491943e86dcf95656a3a24c790055458755e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:05 +0000

    Add package simple-parquet-viewer/0.10.0/simple_parquet_viewer-0.10.0.tar.gz

---
+++ packages/simple-parquet-viewer/simple_parquet_viewer-0.10.0.tar.gz/simple_parquet_viewer-0.10.0/src/simple_parquet_viewer/res/translations/pt.ts
@@ -0,0 +1,188 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>AsyncWorker</name>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="69"/>
+        <source>,</source>
+        <comment>CSV Delimiter</comment>
+        <translation>;</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="73"/>
+        <source>Sheet 1</source>
+        <translation>Planilha 1</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="88"/>
+        <source>Simple Parquet Viewer</source>
+        <translation>Simples Visualizador de Parquet</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="334"/>
+        <location filename="../../widgets/mainwindow.py" line="106"/>
+        <source>Open Parquet file</source>
+        <translation>Abrir arquivo Parquet</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="111"/>
+        <source>Save current view into a Parquet file</source>
+        <translation>Salvar visualização atual como arquivo Parquet</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="117"/>
+        <source>Save current view into a CSV file</source>
+        <translation>Salvar visualização atual como arquivo CSV</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="123"/>
+        <source>Save current view into a XLSX file</source>
+        <translation>Salvar visualização atual como arquivo XLSX</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="129"/>
+        <source>Save current view into a JSON file</source>
+        <translation>Salvar visualização atual como arquivo JSON</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="135"/>
+        <source>View information about this application</source>
+        <translation>Ver informações a respeito desta aplicação</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="139"/>
+        <source>About Simple Parquet Viewer</source>
+        <translatorcomment>Sobre o Simples Visualizador de Parquet</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="140"/>
+        <source>&lt;b&gt;About Simple Parquet Viewer (SPV)&lt;/b&gt;&lt;br /&gt;v{}.{}.{}&lt;br /&gt;&lt;br /&gt;Simple Parquet Viewer is a simple GUI program to visualize and interact with data stored in Parquet files.&lt;br /&gt;&lt;br /&gt;SPV is distributed under the terms of GPLv3. You should have received a copy of the GNU General Public License along with this program. If not, you can find it at &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;&lt;span style=&quot;color: white; font-weight: bold;&quot;&gt;GNU&apos;s website&lt;/span&gt;&lt;/a&gt;.&lt;br /&gt;&lt;br /&gt;</source>
+        <translation>&lt;b&gt;Sobre o Simples Visualizador de Parquet (SVP)&lt;/b&gt;&lt;br /&gt;v{}.{}.{}&lt;br /&gt;&lt;br /&gt;O Simples Visualizador de Parquet é um simples programa com interface gráfica de usuário para visualizar a interagir com dados armazenados em arquivos Parquet.&lt;br /&gt;&lt;br /&gt;O SVP é um &lt;i&gt;software&lt;/i&gt; de código aberto distribuído sob os termos da GPLv3. Você deve ter recebido uma cópia da GNU &lt;i&gt;General Public License&lt;/i&gt; junto a este programa. Se este não for o caso, você poderá encontrá-la no &lt;a href=&quot;https://www.gnu.org/licenses/gpl-3.0.html&quot;&gt;&lt;span style=&quot;color: white; font-weight: bold;&quot;&gt;site GNU&lt;/span&gt;&lt;/a&gt;.&lt;br /&gt;&lt;br /&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="158"/>
+        <source>View information about Qt</source>
+        <translation>Ver informações a respeito do Qt</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="325"/>
+        <location filename="../../widgets/mainwindow.py" line="173"/>
+        <source>Ready!</source>
+        <translation>Pronto!</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="199"/>
+        <source>Please, select a valid Parquet file in order to view its content here :)</source>
+        <translation>Por favor, selecione um arquivo Parquet válido para visualizar o conteúdo dele aqui :)</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="208"/>
+        <source>Loading data...</source>
+        <translation>Carregando dados...</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="229"/>
+        <source>Filter</source>
+        <translation>Filtro</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="233"/>
+        <source>Custom filter compatible with Pandas &quot;query&quot;</source>
+        <translation>Filtro personalizado compatível com a &quot;query&quot; da biblioteca Pandas</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="238"/>
+        <source>Apply filter</source>
+        <translation>Aplicar filtro</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="243"/>
+        <source>Clear current filter</source>
+        <translation>Limpar filtro atual</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="268"/>
+        <source>Filter must not contain &quot;@&quot; (no support to query variables).</source>
+        <translation>O filtro não pode conter &quot;@&quot; (sem suporte a variáveis de &quot;query&quot;).</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="271"/>
+        <source>Filter error</source>
+        <translation>Erro de filtro</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="271"/>
+        <source>Error when applying the requested filter. Details:
+{}</source>
+        <translation>Erro ao aplicar o filtro solicitado. Detalhes:
+{}</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="291"/>
+        <source>Export CSV</source>
+        <translation>Exportar CSV</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="292"/>
+        <source>CSV files (*.csv);;All files (*.*)</source>
+        <translation>Arquivos CSV (*.csv);;Todos os arquivos (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="295"/>
+        <source>Export JSON</source>
+        <translation>Exportar JSON</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="296"/>
+        <source>JSON files (*.json);;All files (*.*)</source>
+        <translation>Arquivos JSON (*.json);;Todos os arquivos (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="299"/>
+        <source>Export XLSX</source>
+        <translation>Exportar XLSX</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="300"/>
+        <source>Excel sheet files (*.xlsx);;All files (*.*)</source>
+        <translation>Arquivos de planilhas do Excel (*.xlsx);;Todos os arquivos (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="303"/>
+        <source>Export Parquet</source>
+        <translation>Exportar Parquet</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="336"/>
+        <location filename="../../widgets/mainwindow.py" line="304"/>
+        <source>Parquet files (*.parquet);;All files (*.*)</source>
+        <translation>Arquivos Parquet (*.parquet);;Todos os arquivos (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="309"/>
+        <source>Exporting &quot;{}&quot;...</source>
+        <translatorcomment>Exportanto &quot;{}&quot;...</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="324"/>
+        <source>The file has been exported successfully!</source>
+        <translation>O arquivo foi exportado com sucesso!</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="328"/>
+        <source>Reading error</source>
+        <translation>Erro de leitura</translation>
+    </message>
+    <message>
+        <location filename="../../widgets/mainwindow.py" line="328"/>
+        <source>It was not possible to read the given file.</source>
+        <translation>Não foi possível ler o arquivo solicitado.</translation>
+    </message>
+</context>
+</TS>
