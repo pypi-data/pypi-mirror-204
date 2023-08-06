c88c1e868edfe55391d99dfa2892102e19a6f12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:02 +0000

    Add package SupOpNumTools/0.1.1/SupOpNumTools-0.1.1-py3-none-any.whl

---
+++ packages/SupOpNumTools/SupOpNumTools-0.1.1-py3-none-any.whl/SupOpNumTools/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+SupOpToolbox
+
+Author : Julien VILLEMEJANE
+Laboratoire d Enseignement Experimental - Institut d Optique Graduate School
+Version : 0.1.0 - 2023-04-24
+"""
+
+from . import elec_modeling
+from . import math_phys_methods
+from . import signal_processing
+
+def test():
+    print('ok')
+
+if __name__ == '__main__':
+    print("SupOpToolbox")
