9d9857051535d62df3e132e62b7220c8d53ea313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:57 +0000

    Add package AegosIN/0.1/AegosIN-0.1.tar.gz

---
+++ packages/AegosIN/AegosIN-0.1.tar.gz/AegosIN-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'AegosIN',
+    version='0.1',
+    author= 'AegosIN',
+    description='Checking emails for any domain , for example : gmail,yahoo,aol,hotmail,outlook,mail_ru',
+    packages= ['AegosIN'],
+    author_email='syakkml@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
