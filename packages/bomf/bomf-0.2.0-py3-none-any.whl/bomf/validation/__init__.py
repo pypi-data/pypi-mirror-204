0ca7645204866693694735414fcc58857acac036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:05 +0000

    Add package bomf/0.2.0/bomf-0.2.0-py3-none-any.whl

---
+++ packages/bomf/bomf-0.2.0-py3-none-any.whl/bomf/validation/__init__.py
@@ -0,0 +1,6 @@
+"""
+bomf performs validation on the intermediate bo4e data set layer. The main class is `ValidatorSet`.
+"""
+
+from bomf.validation.core import ValidatorSet
+from bomf.validation.utils import optional_field, required_field
