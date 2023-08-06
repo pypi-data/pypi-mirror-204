cc25a2ba4b8d47dc50a13d0afa1bd2ca38abcb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package gulistandb/0.0.1/gulistandb-0.0.1.tar.gz

---
+++ packages/gulistandb/gulistandb-0.0.1.tar.gz/gulistandb-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='gulistandb',
+    version='0.0.1',
+    install_requires=[
+        'pandas',
+        # add more dependencies as needed
+    ],
+    package_dir={'': 'src'},
+
+    # metadata
+    author='Azaz Ahmed',
+    author_email='lipuahmedazaz79@gmail.com',
+    description='Short project description',
+    license='MIT'
+)
