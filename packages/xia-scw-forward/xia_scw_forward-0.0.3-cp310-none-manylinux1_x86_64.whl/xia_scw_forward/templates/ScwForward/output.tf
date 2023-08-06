9849c2bc02a531198e37594822f614b293ae7c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:25:00 +0000

    Add package xia-scw-forward/0.0.3/xia_scw_forward-0.0.3-cp310-none-manylinux1_x86_64.whl

---
+++ packages/xia-scw-forward/xia_scw_forward-0.0.3-cp310-none-manylinux1_x86_64.whl/xia_scw_forward/templates/ScwForward/output.tf
@@ -0,0 +1,43 @@
+output "project_id" {
+  value = var.project_id
+}
+
+output "vpc_name" {
+  value = var.vpc_name
+}
+
+output "instance_name" {
+  value = var.instance_name
+}
+
+output "service_name" {
+  value = var.service_name
+}
+
+output "wan_ip" {
+  value = var.wan_ip
+}
+
+output "lan_name" {
+  value = data.scaleway_vpc_private_network.service.name
+}
+
+output "lan_ip" {
+  value = scaleway_vpc_public_gateway_pat_rule.service_pat.private_ip
+}
+
+output "wan_port" {
+  value = scaleway_vpc_public_gateway_pat_rule.service_pat.public_port
+}
+
+output "protocol" {
+  value = scaleway_vpc_public_gateway_pat_rule.service_pat.protocol
+}
+
+output "lan_port" {
+  value = scaleway_vpc_public_gateway_pat_rule.service_pat.private_port
+}
+
+output "allowed_ips" {
+  value = var.allowed_ips
+}
