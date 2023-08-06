e586a04d1a7ecc9785d13c28e4f99cbf924b4649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:18 +0000

    Add package jsl-django-sitemap/1.2.4/jsl_django_sitemap-1.2.4-py3-none-any.whl

---
+++ packages/jsl-django-sitemap/jsl_django_sitemap-1.2.4-py3-none-any.whl/jsl_django_sitemap/default_settings.py
@@ -0,0 +1,36 @@
+import logging
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+FETCH_URL_FROM = "FETCH_URL_FROM"
+DEFAULT_FETCH_URL_FROM = "pattern"
+ENABLE = "ENABLE"
+INCLUDE_APPS = "INCLUDE_APPS"
+IGNORE_URL_PATTERNS = ()
+
+FETCH_URL_FROM_ALLOWED_PARAMS = ["name", "pattern"]
+
+
+def fetch_default_settings():
+	if getattr(settings, 'JSL_DJANGO_SITEMAP_SETTINGS', None) is None:
+		JSL_DJANGO_SITEMAP_SETTINGS = {
+			ENABLE: True,
+			FETCH_URL_FROM: DEFAULT_FETCH_URL_FROM,
+			INCLUDE_APPS: ("ALL",),
+			IGNORE_URL_PATTERNS: IGNORE_URL_PATTERNS
+		}
+		return JSL_DJANGO_SITEMAP_SETTINGS
+	else:
+		default_settings_v = ensure_correct_params(getattr(settings, 'JSL_DJANGO_SITEMAP_SETTINGS', None))
+		if default_settings_v.get('IGNORE_URL_PATTERNS') is None:
+			default_settings_v['IGNORE_URL_PATTERNS'] = IGNORE_URL_PATTERNS
+		return default_settings_v
+
+
+def ensure_correct_params(default_settings_v):
+	if default_settings_v.get(FETCH_URL_FROM) not in FETCH_URL_FROM_ALLOWED_PARAMS:
+		error_message = "FETCH_URL_FROM  should be one of " + str(
+			FETCH_URL_FROM_ALLOWED_PARAMS) + "Now defaulting to 'pattern'"
+		logger.error(error_message)
+		default_settings_v[FETCH_URL_FROM] = DEFAULT_FETCH_URL_FROM
+	return default_settings_v
