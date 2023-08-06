78f0ce8abe5ed6b4b3f4a635b48e80783cad6b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:39 +0000

    Add package atlasapi/2.0.10/atlasapi-2.0.10.tar.gz

---
+++ packages/atlasapi/atlasapi-2.0.10.tar.gz/atlasapi-2.0.10/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from setuptools import find_packages, setup
+
+setup(
+    name='atlasapi',
+    version='2.0.10',
+    python_requires='>=3.7',
+    packages=find_packages(exclude=("tests",)),
+    install_requires=['requests', 'python-dateutil', 'isodate', 'future', 'pytz', 'coolname', 'humanfriendly', 'nose'],
+    setup_requires=['wheel'],
+    # Metadata
+    author="Matthew G. Monteleone",
+    author_email="mgm@mgm.dev",
+    license="Apache License 2.0",
+    description="Expose MongoDB Atlas Cloud provider APIs",
+    long_description=open('README.rst').read(),
+    url="https://github.com/mgmonteleone/python-atlasapi",
+    keywords=["atlas", "mongo", "mongodb", "cloud", "api"],
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache Software License',
+
+        'Operating System :: OS Independent',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    entry_points={
+        'console_scripts': [
+            'atlascli=atlascli.cli:main',
+        ]
+    },
+    extras_require={}
+
+)
