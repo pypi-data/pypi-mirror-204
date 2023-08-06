e11d05d6450d03643056d3ad7a810a9550cdd326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:51 +0000

    Add package github-issue-extractor/0.1.3/github_issue_extractor-0.1.3.tar.gz

---
+++ packages/github-issue-extractor/github_issue_extractor-0.1.3.tar.gz/github_issue_extractor-0.1.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="github_issue_extractor",
+    version="0.1.3",
+    author="Sudarshan Sinha",
+    author_email="s.sinha2103@outlook.com",
+    description="GitHubIssueExtractor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ssinha2103/github-issues_to_excel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "setuptools",
+        "requests",
+        "openpyxl"
+    ]
+)
