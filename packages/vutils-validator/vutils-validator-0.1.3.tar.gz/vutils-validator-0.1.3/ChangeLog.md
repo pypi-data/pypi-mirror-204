80674edbf26dce38f2eec4dcc31315adc7b41a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package vutils-validator/0.1.3/vutils-validator-0.1.3.tar.gz

---
+++ packages/vutils-validator/vutils-validator-0.1.3.tar.gz/vutils-validator-0.1.3/ChangeLog.md
@@ -0,0 +1,17 @@
+# Change Log
+
+## 0.1.3
+
+* Fix doc strings
+
+## 0.1.2
+
+* Code cleanup
+
+## 0.1.1
+
+* Fix `safety` issues
+
+## 0.1.0
+
+* Initial development version
---
+++ packages/vutils-validator/vutils-validator-0.1.3.tar.gz/vutils-validator-0.1.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+# File:    ./setup.py
+# Author:  Jiří Kučera <sanczes AT gmail.com>
+# Date:    2022-05-03 21:26:33 +0200
+# Project: vutils-validator: Data validation utilities
+#
+# SPDX-License-Identifier: MIT
+#
+"""Setup for vutils-validator."""
+
+from setuptools import setup
+
+setup()
