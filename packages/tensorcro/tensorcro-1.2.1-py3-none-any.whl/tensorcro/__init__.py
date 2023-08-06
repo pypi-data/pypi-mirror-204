1cfe17675ca128fd6325ce00c98799d8278b1c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package tensorcro/1.2.1/tensorcro-1.2.1-py3-none-any.whl

---
+++ packages/tensorcro/tensorcro-1.2.1-py3-none-any.whl/tensorcro/__init__.py
@@ -0,0 +1,21 @@
+# - x - x - x - x - x - x - x - x - x - x - x - x - x - x - #
+#                                                           #
+#   This file was created by: Alberto Palomo Alonso         #
+# Universidad de Alcalá - Escuela Politécnica Superior      #
+#                                                           #
+# - x - x - x - x - x - x - x - x - x - x - x - x - x - x - #
+from .slcro import TensorCro
+from .__special__ import __version__, __replay_path__
+from .replay import watch_replay
+from .substrates import UniformCrossover, GaussianCrossover, MaskedCrossover, MultipointCrossover, BLXAlphaCrossover, \
+    ComposedSubstrate, Mutation
+from .substrates import HarmonySearch, RandomSearch, DifferentialSearch
+# Format file:
+import os
+if not os.path.exists(__replay_path__):
+    os.mkdir(__replay_path__)
+if not os.path.exists(__replay_path__ + '/replay'):
+    os.mkdir(__replay_path__ + '/replay')
+# - x - x - x - x - x - x - x - x - x - x - x - x - x - x - #
+#                        END OF FILE                        #
+# - x - x - x - x - x - x - x - x - x - x - x - x - x - x - #
