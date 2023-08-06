4ef6e51adacd3e01321d335ded86d0839501c509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package ombot-utils/0.0.2/ombot_utils-0.0.2.tar.gz

---
+++ packages/ombot-utils/ombot_utils-0.0.2.tar.gz/ombot_utils-0.0.2/src/ombot_utils/data_handler/base_sql_data_handler.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class BaseSQLDataHandler:
+    def __init__(self):
+        """
+        从config文件，初始化数据库
+        """
+
+
+    def get_data(self):
+        pass
+
+    def set_data(self):
+        pass
+
+    def add_data(self):
+        pass
+
+    def delete_data(self):
+        pass
+
+
+
