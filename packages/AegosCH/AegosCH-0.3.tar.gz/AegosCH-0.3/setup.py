fd2b6e9298b0c37e88dd542c7aea9baa5b10db34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:25 +0000

    Add package AegosCH/0.3/AegosCH-0.3.tar.gz

---
+++ packages/AegosCH/AegosCH-0.3.tar.gz/AegosCH-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'AegosCH',
+    version='0.3',
+    author= 'AegosCH',
+    description='Checking emails for tiktok or instagram .',
+    packages= ['AegosCH'],
+    author_email='syakkml@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
