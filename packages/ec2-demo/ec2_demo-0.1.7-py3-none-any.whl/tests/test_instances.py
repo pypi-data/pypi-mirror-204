73bd457031ba5cc825b6c591cc514f7a4d8a8e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package ec2-demo/0.1.7/ec2_demo-0.1.7-py3-none-any.whl

---
+++ packages/ec2-demo/ec2_demo-0.1.7-py3-none-any.whl/tests/test_instances.py
@@ -0,0 +1,27 @@
+from unittest import TestCase, mock
+
+from ec2_demo.instances import Instances
+
+
+class TestInstances(TestCase):
+
+    @mock.patch("ec2_demo.instances.Instances.create")
+    def test_create(self, mocked):
+        expected = ["i-created"]
+        mocked.return_value = expected
+        instances = Instances("dev")
+        returned = instances.create("instances/test.yaml")
+        self.assertEqual(expected, returned)
+
+
+    @mock.patch("ec2_demo.instances.Instances.delete")
+    def test_delete(self, mocked):
+        expected = ["i-deleted"]
+        mocked.return_value = expected
+        instances = Instances("dev")
+        returned = instances.delete("nobody-reads-this")
+        self.assertEqual(expected, returned)
+
+
+if __name__ == "__main__":
+    unittest.main()
