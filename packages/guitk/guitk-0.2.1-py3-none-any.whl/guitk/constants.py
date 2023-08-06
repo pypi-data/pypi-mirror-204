0a8c3c865cd60d95202264086cec02a656039347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:52 +0000

    Add package guitk/0.2.1/guitk-0.2.1-py3-none-any.whl

---
+++ packages/guitk/guitk-0.2.1-py3-none-any.whl/guitk/constants.py
@@ -0,0 +1,12 @@
+"""Constants for guitk """
+
+
+from enum import Enum, auto
+
+class GUITK(Enum):
+    """Constants used internally by guitk"""
+
+    ELEMENT_FRAME = ("Frame",)
+    ELEMENT_LABEL_FRAME = "LabelFrame"
+
+
