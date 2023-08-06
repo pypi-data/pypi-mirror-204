c6b66744d1cf3707e5d480fa8077c1227444dafe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:07:57 +0000

    Add package conductor-py/0.0.2/conductor_py-0.0.2.tar.gz

---
+++ packages/conductor-py/conductor_py-0.0.2.tar.gz/conductor_py-0.0.2/conductor_py/netsuite/invoice.py
@@ -0,0 +1,14 @@
+from conductor_py import client
+from conductor_py.graphql_client.input_types import SendIntegrationRequestInput
+from conductor_py.graphql_client.send_integration_request import SendIntegrationRequest
+
+
+def find_many(integration_connection_id: str) -> SendIntegrationRequest:
+    return client.send_integration_request(
+        input=SendIntegrationRequestInput(
+            integrationConnectionId=integration_connection_id,
+            requestObject={
+                "invoice": {},
+            },
+        )
+    )
---
+++ packages/conductor-py/conductor_py-0.0.2.tar.gz/conductor_py-0.0.2/tests/test_package.py
@@ -0,0 +1,11 @@
+import conductor_py as conductor
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+conductor.set_api_key(os.environ["CONDUCTOR__TEST_API_KEY"])
+integration_connection_id = os.environ["CONDUCTOR__TEST_CONNECTION_ID"]
+
+invoices = conductor.netsuite.invoice.find_many(integration_connection_id)
+print(invoices)
