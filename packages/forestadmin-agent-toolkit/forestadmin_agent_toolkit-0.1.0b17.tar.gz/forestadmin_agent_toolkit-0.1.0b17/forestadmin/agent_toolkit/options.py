f26d6f3427206effb21f89669f52127c00d8ef4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package forestadmin-agent-toolkit/0.1.0b17/forestadmin_agent_toolkit-0.1.0b17.tar.gz

---
+++ packages/forestadmin-agent-toolkit/forestadmin_agent_toolkit-0.1.0b17.tar.gz/forestadmin_agent_toolkit-0.1.0b17/forestadmin/agent_toolkit/options.py
@@ -0,0 +1,44 @@
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal, TypedDict
+else:
+    from typing_extensions import Literal, TypedDict
+
+
+class AgentStackMeta(TypedDict, total=False):
+    engine: Literal["python"]
+    engine_version: str
+    database_type: str
+    orm_version: str
+
+
+class AgentMeta(TypedDict):
+    liana: str
+    liana_version: str
+    stack: AgentStackMeta
+
+
+class Options(TypedDict):
+    application_url: str
+    agent_url: str
+    auth_secret: str
+    prefix: str
+    env_secret: str
+    forest_server_url: str
+    is_production: bool
+    # logger
+    # loggerLevel
+    # schemaPath
+    # typingsPath
+    # typingsMaxDepth
+    # permissionsCacheDurationInSeconds
+    # skipSchemaUpdate
+    # forestAdminClient
+
+
+DEFAULT_OPTIONS: Options = {
+    "is_production": True,
+    "prefix": "forest",
+    "forest_server_url": "https://api.forestadmin.com",
+}
