99bc01600af15a89b656fadd7cdedbcf6ba8579b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:05 +0000

    Add package chessmaker/0.4.2/chessmaker-0.4.2-py3-none-any.whl

---
+++ packages/chessmaker/chessmaker-0.4.2-py3-none-any.whl/chessmaker/chess/results/standard_result.py
@@ -0,0 +1,21 @@
+from chessmaker.chess.base.board import Board
+from chessmaker.chess.results.checkmate import checkmate
+from chessmaker.chess.results.no_captures_or_pawn_moves import NoCapturesOrPawnMoves
+from chessmaker.chess.results.repetition import Repetition
+from chessmaker.chess.results.stalemate import stalemate
+
+
+class StandardResult:
+    def __init__(self):
+        self.results = [
+            checkmate,
+            stalemate,
+            Repetition(),
+            NoCapturesOrPawnMoves(),
+        ]
+
+    def __call__(self, board: Board):
+        for result in self.results:
+            result = result(board)
+            if result:
+                return result
