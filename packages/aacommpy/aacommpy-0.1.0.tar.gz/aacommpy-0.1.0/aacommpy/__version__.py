42efc791eaa298f9abc4d930d155e81cf0f0661d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package aacommpy/0.1.0/aacommpy-0.1.0.tar.gz

---
+++ packages/aacommpy/aacommpy-0.1.0.tar.gz/aacommpy-0.1.0/aacommpy/__version__.py
@@ -0,0 +1,15 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'aacommpy'
+__description__ = 'aacommpy - AACOMM nuget package wrapper'
+__version__ = '0.1.0'
+__author__ = 'HIEU'
+__author_email__ = 'trunghieupcs@gmail.com'
+__license__ = 'MIT'
+# fake url
+__url__ = 'https://github.com/AlexIoannides/py-package-template'
