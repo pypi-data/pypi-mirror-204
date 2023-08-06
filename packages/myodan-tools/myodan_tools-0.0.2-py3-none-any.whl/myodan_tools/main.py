4eed69facda6b29bbdac24c2569ff3cfdb637a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:34 +0000

    Add package myodan-tools/0.0.2/myodan_tools-0.0.2-py3-none-any.whl

---
+++ packages/myodan-tools/myodan_tools-0.0.2-py3-none-any.whl/myodan_tools/main.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlparse
+import typer
+
+from extractor import mrcong
+from downloader import mediafire
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback(url_str: str):
+    url = urlparse(url_str)
+
+    download_links = []
+
+    if "mrcong.com" in url.netloc:
+        if "/tag" in url.path:
+            post_links = mrcong.extract_post_links(url_str)
+
+            for post_link in post_links:
+                download_links += mrcong.extract_download_links(
+                    post_link["href"])
+
+        else:
+            download_links += mrcong.extract_download_links(url_str)
+
+        for download_link in download_links:
+            mediafire.download(download_link["href"], None, quiet=False)
+
+
+if __name__ == "__main__":
+    app()
