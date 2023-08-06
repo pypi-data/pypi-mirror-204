587bc72aae08cd2d9b23d03cd593564e08ba2436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:07 +0000

    Add package rewards-api/0.0.5/rewards_api-0.0.5.tar.gz

---
+++ packages/rewards-api/rewards_api-0.0.5.tar.gz/rewards_api-0.0.5/rewards_api/config.py
@@ -0,0 +1,33 @@
+import os 
+from dataclasses import dataclass 
+from typing import Optional
+from .cli_args import args 
+
+
+
+env_file = open(os.path.join(os.getcwd(), ".env"), "r").read()
+aws_access, aws_secret_access = [env_file.split("\n")[i].split("=")[1] for i in range(2)]
+
+@dataclass(kw_only=True)
+class GlobalConfig:
+    DEVICE: str = "cpu"
+    IN_FEATURES: int = 5
+    TEST_TRIALS: int = 5
+    MODEL_NAME: str = "model.pth"
+    REWARDS_PARENT_CONFIG_DIR: str = ".rewards_ai"
+    REWARDS_CONFIG_MODEL_FOLDER_NAME: str = "session_saved_models"
+    REWARDS_CONFIG_METRIC_FOLDER_NAME: str = "session_metrics"
+    
+    TRAINING_METRICS_JSON_NAME : str = "training_metrics.json"
+    EVALUATION_METRICS_JSON_NAME : str = "evaluation_metrics.json"
+    MODEL_HISTORY_JSON_NAME : str = "model_history.json"
+
+    EVALUATION_TOTAL_TIME_FOR_TRAINING_PATH : int = 30
+    EVALUATION_TOTAL_TIME_FOR_EVALUATION_PATH : int = 60
+    
+    AWS_SECRET_KEY : str = aws_access
+    AWS_SECRET_ACCESS_KEY : str = aws_secret_access
+    
+    STREAMING_TEMP_JSON_PATH: str = args.metrics #"/home/anindya/workspace/RewardsHQ/RewardsSuit/training-platform/src/assets/temp.json"
+
+CONFIG = GlobalConfig().__dict__.copy() 
