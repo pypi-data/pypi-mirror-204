2aa5e8908bf42c409749d2449f02893aa55dc346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:39 +0000

    Add package complex-curve-fit-gui/1.0.9/complex-curve-fit-gui-1.0.9.tar.gz

---
+++ packages/complex-curve-fit-gui/complex-curve-fit-gui-1.0.9.tar.gz/complex-curve-fit-gui-1.0.9/curve.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+from curvefitgui import curve_fit_gui
+
+
+class Main:
+    def __init__(self):
+        """
+        This function are only here to create test data
+        """
+
+        def exp_decay(x, a, b, c):
+            return a * np.exp(-x / b) + c
+
+        self.xdata = np.linspace(0, 4, 50)
+
+        # y data is only set to None because dummy data generated in backend
+        # make to return fit result
+        # have option to get rid of guess line
+
+        printout = curve_fit_gui(
+            None,
+            self.xdata,
+            None,
+            title="Sin Graph",
+            xlabel="y axis",
+            ylabel="Imaginary",
+            fitline_color="red",
+            color="black",
+        )
+        # add=[(np.array([1, 2, 3]), np.array([4, 5, 6]), "data2")]
+
+
+w = Main()
+
+# y = exp_decay(self.xdata, 2.5, 1.3, 0.5)
+# rng = np.random.default_rng()
+# yerr = 0.2 * np.ones_like(self.xdata)
+# y_noise = yerr * rng.normal(size=self.xdata.size)
+# self.ydata = y + y_noise
