8f06b965248927b708aeb1424ba005d6fff6dcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:38 +0000

    Add package hyfi-template/0.1.2/hyfi_template-0.1.2-py3-none-any.whl

---
+++ packages/hyfi-template/hyfi_template-0.1.2-py3-none-any.whl/hyfi_template/__init__.py
@@ -0,0 +1,16 @@
+from hyfi import about, global_config
+
+from ._version import __version__
+
+about.name = "ekorpkit"
+about.author = "Young Joon Lee"
+about.description = "ekorpkit provides a flexible interface for NLP and ML research pipelines \
+such as extraction, transformation, tokenization, training, and visualization."
+about.homepage = "https://ekorpkit.entelecheia.ai"
+about.version = __version__
+global_config.hyfi_package_config_path = "pkg://ekorpkit.conf"
+
+
+def get_version() -> str:
+    """This is the cli function of the package"""
+    return __version__
