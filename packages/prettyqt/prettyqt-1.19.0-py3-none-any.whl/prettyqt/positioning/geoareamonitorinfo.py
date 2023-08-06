58498f533092468680c086a1f78697f0b9f2060d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:01 +0000

    Add package prettyqt/1.19.0/prettyqt-1.19.0-py3-none-any.whl

---
+++ packages/prettyqt/prettyqt-1.19.0-py3-none-any.whl/prettyqt/positioning/geoareamonitorinfo.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from prettyqt import core, positioning
+from prettyqt.qt import QtPositioning
+
+
+class GeoAreaMonitorInfo(QtPositioning.QGeoAreaMonitorInfo):
+    def __str__(self):
+        return self.name()
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self.name()!r})"
+
+    def get_area(self) -> positioning.GeoShape:
+        area = self.area()
+        match area:
+            case QtPositioning.QGeoCircle():
+                return positioning.GeoCircle(area)
+            case QtPositioning.QGeoRectangle():
+                return positioning.GeoRectangle(area)
+            case QtPositioning.QGeoPath():
+                return positioning.GeoPath(area)
+            case QtPositioning.QGeoPolygon():
+                return positioning.GeoPolygon(area)
+            case QtPositioning.QGeoShape():
+                return positioning.GeoShape(area)
+            case _:
+                raise RuntimeError()
+
+    def get_expiration(self) -> core.DateTime:
+        return core.DateTime(self.expiration())
+
+
+if __name__ == "__main__":
+    info = GeoAreaMonitorInfo("test")
+    print(info)
+    print(repr(info))
