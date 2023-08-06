e73e5065b90a7d6451459f0322d66af592fcea8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:37 +0000

    Add package linkml-transformer/0.1.3/linkml_transformer-0.1.3-py3-none-any.whl

---
+++ packages/linkml-transformer/linkml_transformer-0.1.3-py3-none-any.whl/linkml_transformer/datamodel/specification_view.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from linkml_transformer.datamodel.transformer_model import (
+    ElementDerivation, TransformationSpecification)
+
+
+@dataclass
+class TransformerSpecificationView:
+    specification: TransformationSpecification = None
+
+    def derivations(self, derivation: ElementDerivation) -> List[ElementDerivation]:
+        """Return all derivations of a given derivation"""
+        closure = [derivation]
+        return closure
