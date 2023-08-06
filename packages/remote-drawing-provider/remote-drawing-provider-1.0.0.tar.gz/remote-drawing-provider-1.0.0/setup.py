766d47e46d867ed527b6cf2bab28e7be752d40c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:16:09 +0000

    Add package remote-drawing-provider/1.0.0/remote-drawing-provider-1.0.0.tar.gz

---
+++ packages/remote-drawing-provider/remote-drawing-provider-1.0.0.tar.gz/remote-drawing-provider-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='remote_drawing_provider',
+    version='1.0.0',
+    description='创建首个可用版本',
+    author='XiaoyanQian',
+    author_email='thinkershare@163.com',
+    url='https://github.com/XiaoyanQian/remote-drawing',
+    platforms = 'any',
+    packages = find_packages()
+)
