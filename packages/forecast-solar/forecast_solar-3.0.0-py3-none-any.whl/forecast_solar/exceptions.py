0ffebf0faf2439742fa46a0ae6e7e941e39c78a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:20 +0000

    Add package forecast-solar/3.0.0/forecast_solar-3.0.0-py3-none-any.whl

---
+++ packages/forecast-solar/forecast_solar-3.0.0-py3-none-any.whl/forecast_solar/exceptions.py
@@ -0,0 +1,44 @@
+"""Exceptions for Forecast.Solar."""
+from datetime import datetime
+
+
+class ForecastSolarError(Exception):
+    """Generic Forecast.Solar exception."""
+
+
+class ForecastSolarConnectionError(ForecastSolarError):
+    """Forecast.Solar API connection exception."""
+
+
+class ForecastSolarAuthenticationError(ForecastSolarError):
+    """Forecast.Solar API authentication exception."""
+
+    def __init__(self, data: dict) -> None:
+        """Init a solar auth error.
+
+        https://doc.forecast.solar/doku.php?id=api#invalid_request
+        """
+        super().__init__(f'{data["text"]} (error {data["code"]})')
+        self.code = data["code"]
+
+
+class ForecastSolarRequestError(ForecastSolarError):
+    """Forecast.Solar wrong request input variables."""
+
+    def __init__(self, data: dict) -> None:
+        """Init a solar request error.
+
+        https://doc.forecast.solar/doku.php?id=api#invalid_request
+        """
+        super().__init__(f'{data["text"]} (error {data["code"]})')
+        self.code = data["code"]
+
+
+class ForecastSolarRatelimit(ForecastSolarRequestError):
+    """Forecast.Solar maximum number of requests reached exception."""
+
+    def __init__(self, data: dict) -> None:
+        """Init a rate limit error."""
+        super().__init__(data)
+
+        self.reset_at = datetime.fromisoformat(data["ratelimit"]["retry-at"])
