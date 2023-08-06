886987da160e422dd0026f4606dcb573227bc746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:44 +0000

    Add package OMGITSLACKO/0.1.5/OMGITSLACKO-0.1.5-py3-none-any.whl

---
+++ packages/OMGITSLACKO/OMGITSLACKO-0.1.5-py3-none-any.whl/OMGITSLACKO/__init__.py
@@ -0,0 +1,7 @@
+from .functions import correlation
+from .functions import find_best_model_classification
+from .functions import find_best_model_regression
+from .functions import find_best_clustering_algorithm
+from .functions import find_best_model_regression_2
+from .functions import tune_MLPc_parameters
+from .functions import tune_MLPr_parameters
