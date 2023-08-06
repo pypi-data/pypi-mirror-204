beec6f914f2d5bd8b3ab49a2dbf86d7e4cba949c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:43 +0000

    Add package worksheet-grading/1.4.3/worksheet_grading-1.4.3-py3-none-any.whl

---
+++ packages/worksheet-grading/worksheet_grading-1.4.3-py3-none-any.whl/worksheet_grading/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Klaus Kraßnitzer"
+__copyright__ = "Copyright (C) 2022 " + __author__
+__version__ = "1.4.3"
+__license__ = "Distributed under the MIT License: <https://mit-license.org/>"
+__program__ = "Worksheet Grading Script"
+__package__ = "worksheet-grading"
+__repository__ = "https://gitlab.com/kdvkrs/worksheet_grading"
