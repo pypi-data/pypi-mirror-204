33cdefc82bf8d7fb7df37fab8f1ceb72b2c2c333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:38 +0000

    Add package snowflake-cli-labs/0.2.8/snowflake_cli_labs-0.2.8-py3-none-any.whl

---
+++ packages/snowflake-cli-labs/snowflake_cli_labs-0.2.8-py3-none-any.whl/snowcli/sql/create_streamlit.sql
@@ -0,0 +1,14 @@
+use role {{ role }};
+use database {{ database }};
+use schema {{ schema }};
+use warehouse {{ warehouse }};
+
+create or replace stage {{ name }}_stage;
+
+create streamlit {{ name }}
+  ROOT_LOCATION = @{{ database }}.{{ schema }}.{{ name }}_stage
+  MAIN_FILE = '/{{ file_name }}'
+  QUERY_WAREHOUSE = {{ warehouse }};
+
+show streamlits;
+describe streamlit {{ name }};
