4b3bbadf5ff91676f3714ecf1b4a33ad9ca0021e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:39 +0000

    Add package py-seasnake/0.2.1/py_seasnake-0.2.1.tar.gz

---
+++ packages/py-seasnake/py_seasnake-0.2.1.tar.gz/py_seasnake-0.2.1/seasnake/summaries/fish_belt.py
@@ -0,0 +1,81 @@
+from ..base import DataFrame, MermaidBase, requires_token
+
+
+class FishBeltTransect(MermaidBase):
+    @requires_token
+    def observations(self, project_id: str) -> DataFrame:
+        """
+        Retrieves a project's Fish Belt Transect observations.
+
+        Args:
+            project_id (str): The ID of the project for which to fetch
+            Fish Belt Transect observations.
+
+        Returns:
+            DataFrame
+        
+        Examples:
+        ```
+        from seasnake import MermaidAuth, FishBeltTransect    
+        
+        auth = MermaidAuth()
+        fish_belt = FishBeltTransect(token=auth.get_token())
+        project_id = "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"
+        print(fish_belt.observations(project_id))
+        ```
+        """
+
+        url = f"/projects/{project_id}/beltfishes/obstransectbeltfishes/"
+        return self.data_frame_from_url(url)
+
+    @requires_token
+    def sample_units(self, project_id: str) -> DataFrame:
+        """
+        Retrieves a project's Fish Belt Transect observations aggregated by sample units.
+
+        Args:
+            project_id (str): The ID of the project for which to fetch
+            Fish Belt Transect sample units.
+
+        Returns:
+            DataFrame
+        
+        Examples:
+        ```
+        from seasnake import MermaidAuth, FishBeltTransect    
+        
+        auth = MermaidAuth()
+        fish_belt = FishBeltTransect(token=auth.get_token())
+        project_id = "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"
+        print(fish_belt.sample_units(project_id))
+        ```
+        """
+
+        url = f"/projects/{project_id}/beltfishes/sampleunits/"
+        return self.data_frame_from_url(url)
+
+    @requires_token
+    def sample_events(self, project_id: str) -> DataFrame:
+        """
+        Retrieves a project's Fish Belt Transect observations aggregated by sample events.
+
+        Args:
+            project_id (str): The ID of the project for which to fetch
+            Fish Belt Transect sample events.
+
+        Returns:
+            DataFrame
+
+        Examples:
+        ```
+        from seasnake import MermaidAuth, FishBeltTransect    
+        
+        auth = MermaidAuth()
+        fish_belt = FishBeltTransect(token=auth.get_token())
+        project_id = "AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE"
+        print(fish_belt.sample_events(project_id))
+        ```
+        """
+
+        url = f"/projects/{project_id}/beltfishes/sampleevents/"
+        return self.data_frame_from_url(url)
