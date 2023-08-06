4a39e83ede9c7be41d0d1bff27650ec8deef56cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:33 +0000

    Add package hstestaq/0.2.13/hstestaq-0.2.13.tar.gz

---
+++ packages/hstestaq/hstestaq-0.2.13.tar.gz/hstestaq-0.2.13/setup.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+install_requires = open("requirements.txt").read().strip().split("\n")
+
+readme_path = Path(os.environ["PWD"], "../../README.md")
+long_description = open(readme_path).read()
+
+setup(
+    name="hstestaq",
+    version="0.2.13",
+    install_requires=install_requires,
+    scripts=["bin/aqueduct"],
+    packages=find_packages(),
+    description="Prediction Infrastructure for Data Scientists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.aqueducthq.com/",
+    license="Apache License 2.0",
+    author="Aqueduct, Inc.",
+    author_email="hello@aqueducthq.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.7",
+)
