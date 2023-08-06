62046594be62ecdb4d79d5ce31f5103c0bdcc91b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:24 +0000

    Add package auto-find-date-pdf/0.1.2/auto_find_date_pdf-0.1.2-py3-none-any.whl

---
+++ packages/auto-find-date-pdf/auto_find_date_pdf-0.1.2-py3-none-any.whl/main.py
@@ -0,0 +1,44 @@
+import datetime
+from email.mime.text import MIMEText
+from typing import Dict
+from typing import Optional
+import smtplib
+from fastapi import HTTPException
+from fastapi import Request
+from fastapi import status
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security import OAuth2
+from fastapi.security.utils import get_authorization_scheme_param
+import logging as log
+
+
+
+
+def db_model_to_dict(model, firstcol=None):
+    """
+    Convert SQLAlchemy model to a dictionary with its column names as keys
+    """
+    data = {}
+    if firstcol:
+        data[firstcol] = ''
+        data['name'] = model.name
+    for column in model.__table__.columns:
+        value = getattr(model, column.name)
+        if isinstance(value, (datetime.date, datetime.datetime)):
+            value = value.isoformat()
+        data[column.name] = value
+    return data
+
+
+def image_rotate(imagename: str, rotateDegree: int):
+    from PIL import Image
+    # Giving The Original image Directory
+    Original_Image = Image.open(imagename)
+    rotated_image1 = Original_Image.transpose(Image.ROTATE_90)
+    rotated_image1.save(imagename)
+
+
+def safe_naming(name):
+    keepcharacters = ('.', '_')
+    return "".join(c for c in name if c.isalnum() or c in keepcharacters).rstrip()
+
