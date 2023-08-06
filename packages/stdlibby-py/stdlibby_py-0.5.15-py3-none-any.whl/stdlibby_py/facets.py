211346d69ccab6fe8071011bec50d339a3db2211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:19 +0000

    Add package stdlibby-py/0.5.15/stdlibby_py-0.5.15-py3-none-any.whl

---
+++ packages/stdlibby-py/stdlibby_py-0.5.15-py3-none-any.whl/stdlibby_py/facets.py
@@ -0,0 +1,108 @@
+import sys
+
+facets = [
+    {
+        "name": "health",
+        "color": "blue",
+        "hex_color": "#3c1cdd",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": [
+            {
+                "name": "physical-health",
+                "hex_color": "#040fb4",
+                "?mantra|theme?": ""
+            },
+            {
+                "name": "mental-health",
+                "hex_color": "#00ffef",
+                "?mantra|theme?": ""
+            }
+        ]
+    },
+    {
+        "name": "hobbies",
+        "color": "yellow",
+        "hex_color": "#ffeb47",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    },
+    {
+        "name": "social",
+        "color": "pink",
+        "hex_color": "#fb4699",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    },
+    {
+        "name": "money",
+        "color": "red",
+        "hex_color": "#d72d2d",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    },
+    {
+        "name": "self-improvements",
+        "color": "green",
+        "hex_color": "#3eb206",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": [
+            {
+                "name": "learning",
+                "hex_color": "#00ff00",
+                "?mantra|theme?": ""
+            },
+            {
+                "name": "skills",
+                "hex_color": "#046307",
+                "?mantra|theme?": ""
+            }
+        ]
+    },
+    {
+        "name": "relaxation",
+        "color": "white",
+        "hex_color": "#ffffef",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    },
+    {
+        "name": "errands",
+        "color": "orange",
+        "hex_color": "#e66c00",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    },
+    {
+        "name": "null",
+        "color": "brown",
+        "hex_color": "#4f3a14",
+        "?mantra|theme?": "",
+        "targets": [],
+        "projects": [],
+        "sub-facets": []
+    }
+]
+
+def grade_day(periods: list):
+    pass
+
+
+def cli(args: list):
+    pass
+
+if __name__ == "__main__":
+    cli(sys.argv)
