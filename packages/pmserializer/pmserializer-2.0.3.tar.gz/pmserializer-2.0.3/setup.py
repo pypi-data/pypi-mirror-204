92d8a1ef547ec5e09bb95c93d0b8686ae43cf3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:01 +0000

    Add package pmserializer/2.0.3/pmserializer-2.0.3.tar.gz

---
+++ packages/pmserializer/pmserializer-2.0.3.tar.gz/pmserializer-2.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pmserializer',
+    version="2.0.3",
+    description="serializer of few formats",
+    author="Carcajo",
+    author_email='t375445391507@gmail.com',
+    install_requires=["pytomlpp", 'pyyaml'],
+    packages=['lab3'],
+    test_suite='tests/',
+)
