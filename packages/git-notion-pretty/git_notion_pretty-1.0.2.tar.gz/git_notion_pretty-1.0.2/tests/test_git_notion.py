00b75c3b1a037ca6fc99bc378b0f60758395cbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:04 +0000

    Add package git-notion-pretty/1.0.2/git_notion_pretty-1.0.2.tar.gz

---
+++ packages/git-notion-pretty/git_notion_pretty-1.0.2.tar.gz/git_notion_pretty-1.0.2/tests/test_git_notion.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""Tests for `git_notion` package."""
+
+import pytest
+
+from git_notion import git_notion
+from git_notion import cli
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+def test_command_line_interface():
+    """Test the CLI"""
+    pass # TODO
+
