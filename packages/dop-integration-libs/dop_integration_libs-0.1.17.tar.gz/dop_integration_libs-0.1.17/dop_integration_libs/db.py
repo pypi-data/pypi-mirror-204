099eee370cc4bea5172c56ab4144b51c7fd9a2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:27 +0000

    Add package dop-integration-libs/0.1.17/dop_integration_libs-0.1.17.tar.gz

---
+++ packages/dop-integration-libs/dop_integration_libs-0.1.17.tar.gz/dop_integration_libs-0.1.17/dop_integration_libs/db.py
@@ -0,0 +1,46 @@
+import requests
+from .environment import Environment
+from typing import List
+
+
+class Db(object):
+    def __init__(self, db_name: str, env: Environment):
+        self.db = db_name
+        self.env = env
+
+    def insert(self, data: dict) -> bool | None:
+        url = f"{self.env.LOG_API_BASE_URL}/remote-cache/db"
+        print(url)
+        headers = {
+            "Authorization": f"Bearer {self.env.LOG_API_TOKEN}"
+        }
+        print(headers)
+        body = {
+            "meta":
+            {
+                "id_field": "id",
+                "db_name": self.db
+            },
+            "data": data
+        }
+        response = requests.post(url, headers=headers, data=body)
+        print(f"response: {response.status_code}")
+        if response.status_code == 200:
+            return True
+        else:
+            return None
+
+    def find(self, filters: dict) -> List[dict] | None:
+        url = f"{self.env.LOG_API_BASE_URL}/remote-cache/db"
+        headers = {
+            "Authorization": f"Bearer {self.env.LOG_API_TOKEN}"
+        }
+        body = {
+            "db_name": self.db,
+            "filters": filters
+        }
+        response = requests.put(url, headers=headers, data=body)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
