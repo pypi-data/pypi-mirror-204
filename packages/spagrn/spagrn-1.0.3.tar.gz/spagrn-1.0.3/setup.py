b8c121bbdb47401e2117d512277c5b80230a44b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:29 +0000

    Add package spagrn/1.0.3/spagrn-1.0.3.tar.gz

---
+++ packages/spagrn/spagrn-1.0.3.tar.gz/spagrn-1.0.3/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import setuptools
+import glob
+import os
+from pathlib import Path
+
+setuptools.setup(
+    name="spagrn",
+    version="1.0.3",
+    author="Yao LI, Lidong GUO, Mengyang XU",
+    author_email="liyao1@genomics.cn, guolidong@genomics.cn, xumengyang@genomics.cn",
+    url="https://github.com/BGI-Qingdao/SpaGRN",
+    #long_description=Path('README.md').read_text('utf-8'),
+    python_requires=">=3.7,<3.11",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "anndata==0.8.0",
+        "pandas>=1.3.5",
+        "scanpy",
+        "seaborn",
+        "matplotlib",
+        "pyscenic==0.12.1",
+        "hotspotsc==1.1.1",
+        "scipy",
+        "numpy",
+        "dask",
+        "arboreto",
+        "ctxcore>=0.2.0",
+        "sklearn"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    license="GPL-3.0+",
+    description="A comprehensive tool to infer TF-centered, spatial gene regulatory networks for the spatially resolved transcriptomic data.",
+    platforms='any',
+)
+
