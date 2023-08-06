b8b75179350813dfe91b98f413a2c71bf7a73212
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:26 +0000

    Add package pandasflow/0.3.0/pandasflow-0.3.0.tar.gz

---
+++ packages/pandasflow/pandasflow-0.3.0.tar.gz/pandasflow-0.3.0/pandasflow/__init__.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+__version__ = '0.3.0'
+
+requirements = ['pandas', 'scikit-learn']
+
+
+
+
+from pandasflow.get_import import get_import
+
+from pandasflow.reset_mi import reset_mi
+from pandasflow.mean_error import mean_error
+
+
+
+# packages
+from pandasflow import split
+
+
+
+
+
+
+
---
+++ packages/pandasflow/pandasflow-0.3.0.tar.gz/pandasflow-0.3.0/pandasflow/get_import.py
@@ -0,0 +1,23 @@
+
+
+
+def get_import():
+	print('''
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.style.use("dark_background")
+'''[1:])
+
+#Удалённое
+'''
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+
+'''
+
+
+if __name__ == "__main__":
+	get_import()
