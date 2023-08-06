9a6160f01ac6b4a97a5a3d7790bf013b7a7c9236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:46 +0000

    Add package fd-component/0.0.1/fd_component-0.0.1.tar.gz

---
+++ packages/fd-component/fd_component-0.0.1.tar.gz/fd_component-0.0.1/fd_component/frontend/build/precache-manifest.9d79e382e67b0ea743cf5e41e98e4fed.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa9a0e1865f21521a97b260abe3e118",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef477196553fa581e532",
+    "url": "./static/css/2.2bdbc9a0.chunk.css"
+  },
+  {
+    "revision": "ef477196553fa581e532",
+    "url": "./static/js/2.becb3afd.chunk.js"
+  },
+  {
+    "revision": "02b98e86acdf7c08ab784cad4f4d6697",
+    "url": "./static/js/2.becb3afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7a983c8e85bc4393e2",
+    "url": "./static/js/main.ca9b5d10.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "5772fe446c95173d5541060503710d3e",
+    "url": "./static/media/alfa-slab-one-all-400-normal.5772fe44.woff"
+  },
+  {
+    "revision": "8c51df773fcb996de5b83b44683523fc",
+    "url": "./static/media/alfa-slab-one-latin-400-normal.8c51df77.woff2"
+  },
+  {
+    "revision": "f99f40799046835f848033cd3eade8fc",
+    "url": "./static/media/alfa-slab-one-latin-ext-400-normal.f99f4079.woff2"
+  },
+  {
+    "revision": "f559bdf3888369250730bc14726d2ff9",
+    "url": "./static/media/alfa-slab-one-vietnamese-400-normal.f559bdf3.woff2"
+  },
+  {
+    "revision": "da6590e4a837b289a0140f5f9525d89a",
+    "url": "./static/media/background.da6590e4.png"
+  },
+  {
+    "revision": "090d39dc24b1a44f21ab2c674de3032b",
+    "url": "./static/media/fd_symbol.090d39dc.png"
+  }
+]);
