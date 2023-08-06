1c3915a79897c1f286508ea4c757d16ca2a72b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:47 +0000

    Add package tinysearch/0.1.0/tinysearch-0.1.0-py3-none-any.whl

---
+++ packages/tinysearch/tinysearch-0.1.0-py3-none-any.whl/tinysearch/index.py
@@ -0,0 +1,31 @@
+"""Index represents a reverted index.
+
+Reverted index is a structure enabling search. It contains documents,
+and each document has been parsed / analyzed to generate a list of
+tokens (terms).
+
+Then the search operates on the indexed documents to find the best results
+for a given query.
+
+Example usage:
+
+    docs = [...]  # List of strings
+    i = Index(docs)
+    print(i.docs)
+"""
+from typing import List
+
+from tinysearch.document import Document
+
+
+class Index:
+    def __init__(self, docs: List[str]) -> None:
+        self.docs = self.process_docs(docs)
+
+    def process_docs(self, docs: List[str]) -> List[Document]:
+        processed = []
+        for doc in docs:
+            d = Document(doc)
+            d.analyze()
+            processed.append(d)
+        return processed
