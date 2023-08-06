0a79e22e15f84a6afc5984e96e8b990102267632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:46 +0000

    Add package sense-energy/0.11.2/sense_energy-0.11.2.tar.gz

---
+++ packages/sense-energy/sense_energy-0.11.2.tar.gz/sense_energy-0.11.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = '0.11.2'
+setup(
+    name = 'sense_energy',
+    packages = ['sense_energy'], 
+    install_requires=[
+        'async_timeout>=3.0.1',
+        'orjson',
+        'requests',
+        'websocket-client',
+        'websockets;python_version>="3.5"',
+        'aiohttp;python_version>="3.5"',
+    ], 
+    version = version,
+    description = 'API for the Sense Energy Monitor',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'scottbonline',
+    author_email = 'scottbonline@gmail.com',
+    url = 'https://github.com/scottbonline/sense',
+    download_url = 'https://github.com/scottbonline/sense/archive/'+version+'.tar.gz',
+    keywords = ['sense', 'energy', 'api'], 
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
