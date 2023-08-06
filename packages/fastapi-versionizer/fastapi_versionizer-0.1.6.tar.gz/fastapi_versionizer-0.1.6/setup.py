e7b28775d9e384873f733960cedcea0d7c2c5664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:25 +0000

    Add package fastapi-versionizer/0.1.6/fastapi_versionizer-0.1.6.tar.gz

---
+++ packages/fastapi-versionizer/fastapi_versionizer-0.1.6.tar.gz/fastapi_versionizer-0.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='fastapi_versionizer',
+    version='0.1.6',
+    author='Alex Schimpf',
+    author_email='aschimpf1@gmail.com',
+    description='API versionizer for FastAPI web applications',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/alexschimpf/fastapi-versionizer',
+    package_data={'fastapi_versionizer': ['py.typed']},
+    packages=['fastapi_versionizer'],
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'fastapi>=0.56.0',
+        'starlette'
+    ],
+    python_requires='>=3.6'
+)
