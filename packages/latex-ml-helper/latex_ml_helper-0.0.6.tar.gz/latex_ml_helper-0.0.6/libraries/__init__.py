399b86c763214af28a99069fc64cb147eb4e554c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:33 +0000

    Add package latex-ml-helper/0.0.6/latex_ml_helper-0.0.6.tar.gz

---
+++ packages/latex-ml-helper/latex_ml_helper-0.0.6.tar.gz/latex_ml_helper-0.0.6/libraries/__init__.py
@@ -0,0 +1,4 @@
+from libraries.definitions import PerceptronDef, SVMDef, LogisticRegressionDef, LinearRegressionDef, PolinomialRegressionDef, SoftmaxDef, HelpDef, GradientPseudo, PerceptronPseudo
+from libraries.snippets import Imports, Grad, Loss, Perc, SVM_Line, SVM_Margin, Contour, Scaler, SoftMax, Pip, HelpSnippets
+from libraries.otup import Imports, graphic_solution, graphic_solution2, C_m, jordan_form_solution, tf, tf2, help_otup
+# from libraries.copy import copy_text
