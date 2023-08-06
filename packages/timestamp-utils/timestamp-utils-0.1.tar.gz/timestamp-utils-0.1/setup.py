61dbfc291dd9f23ecd71783f1cc4fa1edfc42359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:38 +0000

    Add package timestamp-utils/0.1/timestamp-utils-0.1.tar.gz

---
+++ packages/timestamp-utils/timestamp-utils-0.1.tar.gz/timestamp-utils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='timestamp-utils',
+    version='0.1',
+    description='Package for generating Unix timestamps and related timestamp formats',
+    author='Your Name',
+    author_email='your-email@example.com',
+    packages=find_packages(),
+    install_requires=[],
+)
