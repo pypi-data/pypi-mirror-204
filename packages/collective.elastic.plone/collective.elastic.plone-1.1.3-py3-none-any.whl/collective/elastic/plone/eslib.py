1ada63af633a338730bf4720af61ffdc3bffb055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:18 +0000

    Add package collective.elastic.plone/1.1.3/collective.elastic.plone-1.1.3-py3-none-any.whl

---
+++ packages/collective.elastic.plone/collective.elastic.plone-1.1.3-py3-none-any.whl/collective/elastic/plone/eslib.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from collective.elastic.ingest import ELASTICSEARCH_7
+from elasticsearch import Elasticsearch
+
+import logging
+import os
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_query_client(elasticsearch_server_baseurl=None):
+    """return elasticsearch client for ingest"""
+    raw_addr = elasticsearch_server_baseurl or os.environ.get(
+        "ELASTICSEARCH_QUERY_SERVER", "http://localhost:9200"
+    )
+    use_ssl = os.environ.get("ELASTICSEARCH_QUERY_USE_SSL", "0")
+    use_ssl = bool(int(use_ssl))
+    addresses = [x for x in raw_addr.split(",") if x.strip()]
+    if not addresses:
+        addresses.append("127.0.0.1:9200")
+    if ELASTICSEARCH_7:
+        return Elasticsearch(
+            addresses,
+            use_ssl=use_ssl,
+            # here some more params need to be configured.
+        )
+    return Elasticsearch(addresses)
+
+
+def index_name():
+    return os.environ.get("ELASTICSEARCH_INDEX", "plone")
