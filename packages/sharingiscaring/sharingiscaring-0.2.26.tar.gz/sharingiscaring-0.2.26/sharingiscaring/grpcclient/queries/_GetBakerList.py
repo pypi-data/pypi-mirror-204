0394487f8a56d91e7d705515158ef78aa8675b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:08 +0000

    Add package sharingiscaring/0.2.26/sharingiscaring-0.2.26.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.2.26.tar.gz/sharingiscaring-0.2.26/sharingiscaring/grpcclient/queries/_GetBakerList.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from sharingiscaring.GRPCClient.types_pb2 import *
+from sharingiscaring.GRPCClient.service_pb2_grpc import QueriesStub
+from sharingiscaring.GRPCClient.queries._SharedConverters import (
+    Mixin as _SharedConverters,
+)
+from typing import Iterator
+from sharingiscaring.GRPCClient.CCD_Types import *
+from typing import TYPE_CHECKING
+import sys
+
+if TYPE_CHECKING:
+    from sharingiscaring.GRPCClient import GRPCClient
+
+
+class Mixin(_SharedConverters):
+    def get_baker_list(self: GRPCClient, block_hash: str) -> list[CCD_BakerId]:
+        result = []
+        blockHashInput = self.generate_block_hash_input_from(block_hash)
+
+        self.check_connection(sys._getframe().f_code.co_name)
+        grpc_return_value: Iterator[BakerId] = self.stub.GetBakerList(
+            request=blockHashInput
+        )
+
+        for baker in list(grpc_return_value):
+
+            result.append(self.convertType(baker))
+
+        return result
