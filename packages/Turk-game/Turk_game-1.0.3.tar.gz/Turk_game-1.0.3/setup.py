d225b2ca416a3fe321e9ec0aa127ea4182140b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:14 +0000

    Add package Turk-game/1.0.3/Turk_game-1.0.3.tar.gz

---
+++ packages/Turk-game/Turk_game-1.0.3.tar.gz/Turk_game-1.0.3/setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'Turk_game',
+    packages = ['.'],
+    version = '1.0.3',
+    description = 'Türk profesyoneller ve acemiler için basitleştirilmiş ve kısaltılmış pygame',
+    author = 'Eymen',
+    author_email = 'taneymen2009@gmail.com',
+    url = '',
+    requires=["pygame",
+              "threading",
+              "keyboard",
+              
+              ],
+    keywords = [],
+    classifiers = [],
+)
