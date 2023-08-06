5b5e82ec60182d6e054a7d37cd19f992a589f4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:38 +0000

    Add package newtgw/0.0.6/newtgw-0.0.6.tar.gz

---
+++ packages/newtgw/newtgw-0.0.6.tar.gz/newtgw-0.0.6/src/newtgw/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_lambda_python_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "newtgw", "0.0.6", __name__[0:-6], "newtgw@0.0.6.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
