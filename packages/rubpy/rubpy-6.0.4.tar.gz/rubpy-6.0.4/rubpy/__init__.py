68a346bb4583bab7d937330ba0e6a05673915e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:20 +0000

    Add package rubpy/6.0.4/rubpy-6.0.4.tar.gz

---
+++ packages/rubpy/rubpy-6.0.4.tar.gz/rubpy-6.0.4/rubpy/__init__.py
@@ -0,0 +1,7 @@
+from .client import Client
+from .network import Proxies
+from .structs import handlers, models
+from .gadgets import exceptions, methods
+
+__version__ = '6.0.4'
+__author__ = 'Shayan Heidari'
