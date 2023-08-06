889e18caa8e2e06737460b90acc370ceff25ffd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:18 +0000

    Add package pyhtml-desktop/1.1.3/pyhtml_desktop-1.1.3.tar.gz

---
+++ packages/pyhtml-desktop/pyhtml_desktop-1.1.3.tar.gz/pyhtml_desktop-1.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.3' 
+DESCRIPTION = 'build desktop apps with Python and HTML'
+LONG_DESCRIPTION = 'build desktop apps with Python and HTML'
+setup(
+        name="pyhtml_desktop", 
+        version=VERSION,
+        author="Kellan Butler",
+        author_email="kellanbutler52@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['beautifulsoup4'], 
+        
+        keywords=['python', 'html'],
+        classifiers= [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
+            "Operating System :: POSIX :: Linux",
+        ],
+        project_urls={
+            "Github":'https://github.com/kellantech/pyhtmldesktop/'
+        }
+)
