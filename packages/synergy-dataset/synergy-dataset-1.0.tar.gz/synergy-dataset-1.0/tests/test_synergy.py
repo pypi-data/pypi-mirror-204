d6f96463f47ba62d7c0501098066262ee9e1d0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:30 +0000

    Add package synergy-dataset/1.0/synergy-dataset-1.0.tar.gz

---
+++ packages/synergy-dataset/synergy-dataset-1.0.tar.gz/synergy-dataset-1.0/tests/test_synergy.py
@@ -0,0 +1,12 @@
+from synergy_dataset import Dataset
+from synergy_dataset import iter_datasets
+
+
+def test_iter():
+    assert len(list(iter_datasets())) > 1
+
+
+def test_build_dataset():
+    d = Dataset("van_de_Schoot_2017")
+
+    d.to_dict()
