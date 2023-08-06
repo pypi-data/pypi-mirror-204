b191f4b1b9eadc3fe86369b7a1e298e769d55ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:20 +0000

    Add package niubi/1.0/niubi-1.0.tar.gz

---
+++ packages/niubi/niubi-1.0.tar.gz/niubi-1.0/pack/__init__.py
@@ -0,0 +1,4 @@
+# _*_ coding : utf-8 _*_
+# @Time : 2023/4/21 11:38
+# @File : __init__.py
+# @Project : web1
---
+++ packages/niubi/niubi-1.0.tar.gz/niubi-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='niubi',
+      version='1.0',
+      description='测试⾃定义模块的发布与安装',
+      author='drimwai__hlw',  # 作者
+      author_email='drimwai@163.com',
+      py_modules=['pack.myMath'])
