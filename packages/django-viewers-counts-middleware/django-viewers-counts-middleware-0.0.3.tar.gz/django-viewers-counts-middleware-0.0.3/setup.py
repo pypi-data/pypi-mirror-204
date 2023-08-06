dcfe7f815b87eb8356ea29b4147d01a8625778fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:13 +0000

    Add package django-viewers-counts-middleware/0.0.3/django-viewers-counts-middleware-0.0.3.tar.gz

---
+++ packages/django-viewers-counts-middleware/django-viewers-counts-middleware-0.0.3.tar.gz/django-viewers-counts-middleware-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Django Viewers Count Middleware get from user request'
+
+# Setting up
+setup(
+    name="django-viewers-counts-middleware",
+    version=VERSION,
+    author="Hariharan",
+    author_email="hari@ittamil.in",
+    description=DESCRIPTION,
+    long_description = "file: README.md",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/ittamil/django-viewers-counts-middleware",
+    packages=find_packages(),
+    keywords=['python', 'django', 'viewers count middleware', 'viewers count', 'middleware', 'django plugin'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
