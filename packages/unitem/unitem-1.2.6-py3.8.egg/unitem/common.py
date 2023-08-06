3d27bfe315970b6a2fc66bf5352754385d2f51cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:29 +0000

    Add package unitem/1.2.6/unitem-1.2.6-py3.8.egg

---
+++ packages/unitem/unitem-1.2.6-py3.8.egg/unitem/common.py
@@ -0,0 +1,175 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+import os
+import sys
+import ast
+import subprocess
+import logging
+from collections import defaultdict
+
+import unitem.seq_io as seq_io
+
+from unitem.defaults import *
+
+
+def run_cmd(cmd, program=None, exit_on_fail=True):
+    """Run external command."""
+
+    logger = logging.getLogger('timestamp')
+    logger.info(f"Executing: {cmd}")
+
+    try:
+        proc = subprocess.Popen(cmd.split(),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                encoding='utf-8')
+
+        progress_strs = ['Finished processing',
+                         'Finished parsing']
+        while True:
+            out = proc.stdout.readline()
+            if not out and proc.poll() is not None:
+                break
+
+            if out.rstrip():
+                # HACK: if output is logging information for a dependency, remove
+                # the logging information so it isn't shown twice
+                if ' INFO: ' in out:
+                    out = out.split(' INFO: ')[1]
+
+                if any([p in out for p in progress_strs]):
+                    # HACK: CheckM progress bars so write to stdout without newline
+                    print(f'{out.rstrip()}\r', end='')
+                elif program:
+                    logger.info(f'[{program}] {out.rstrip()}')
+                else:
+                    logger.info(out.rstrip())
+
+        if proc.returncode != 0:
+            if exit_on_fail:
+                logger.error(f'Return code: {proc.returncode}')
+                sys.exit(1)
+            else:
+                logger.warning(f'Return code: {proc.returncode}')
+                return False
+    except Exception as e:
+        print(e)
+        logger.error('Failed to execute command.')
+        sys.exit(1)
+
+    return True
+
+
+def calculateN50L50M50(seqs):
+    """Calculate N50 and M50 statistics."""
+
+    seq_lens = [len(seq) for seq in seqs]
+
+    thresholdN50 = sum(seq_lens) / 2
+
+    seq_lens.sort(reverse=True)
+
+    test_sum = 0
+    L50 = 0
+    N50 = 0
+    for seq_len in seq_lens:
+        L50 += 1
+        test_sum += seq_len
+        if test_sum >= thresholdN50:
+            N50 = seq_len
+            break
+
+    M50 = len(seqs) - L50
+    if test_sum != thresholdN50:
+        M50 += 1
+
+    return N50, L50, M50
+
+
+def read_bins(bin_dirs):
+    """Read sequences in bins."""
+
+    bins = defaultdict(lambda: defaultdict(set))
+    contigs = {}
+    contigs_in_bins = defaultdict(lambda: {})
+    for method_id, (bin_dir, bin_ext) in bin_dirs.items():
+        for bf in os.listdir(bin_dir):
+            if not bf.endswith(bin_ext):
+                continue
+
+            bin_id = bf[0:bf.rfind(bin_ext)]
+            if bin_id[-1] == '.':
+                bin_id = bin_id[0:-1]
+            bf_path = os.path.join(bin_dir, bf)
+
+            for seq_id, seq in seq_io.read_seq(bf_path):
+                bins[method_id][bin_id].add(seq_id)
+                contigs[seq_id] = seq
+                contigs_in_bins[seq_id][method_id] = bin_id
+
+            if len(bins[method_id][bin_id]) == 0:
+                logger = logging.getLogger('timestamp')
+                logger.warning('Bin {bf} from {method_id} is empty.')
+
+    return bins, contigs, contigs_in_bins
+
+
+def count_nt(seq):
+    """Count occurrences of each nucleotide in a sequence.
+
+    Only the bases A, C, G, and T(U) are counted. Ambiguous
+    bases are ignored.
+
+    Parameters
+    ----------
+    seq : str
+        Nucleotide sequence.
+
+    Returns
+    -------
+    list
+        Number of A, C, G, and T(U) in sequence.
+    """
+
+    s = seq.upper()
+    a = s.count('A')
+    c = s.count('C')
+    g = s.count('G')
+    t = s.count('T') + s.count('U')
+
+    return a, c, g, t
+
+
+def bin_gc(bin):
+    """Calculate GC-content of bin."""
+
+    a_count = 0
+    c_count = 0
+    g_count = 0
+    t_count = 0
+    for seq in bin.values():
+        a, c, g, t = count_nt(seq)
+
+        a_count += a
+        c_count += c
+        g_count += g
+        t_count += t
+
+    total = (a_count + c_count + g_count + t_count)
+
+    return (g_count + c_count) / total
---
+++ packages/unitem/unitem-1.2.6-py3.8.egg/unitem/main.py
@@ -0,0 +1,262 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+import os
+import sys
+import logging
+from collections import Counter
+
+from unitem.utils import (make_sure_path_exists,
+                          check_dir_exists,
+                          check_file_exists)
+from unitem.bin_tools import BinTools
+from unitem.bin import Bin
+from unitem.profile import Profile
+from unitem.ensemble import Ensemble
+
+
+class OptionsParser():
+    def __init__(self):
+        """Initialization"""
+
+        self.logger = logging.getLogger('timestamp')
+
+    def _bin_extension(self, bin_dir):
+        """Determine extension of bins."""
+
+        exts = []
+        for f in os.listdir(bin_dir):
+            f_split = f.split('.')
+
+            if len(f_split) > 1:
+                ext = f_split[-1]
+                if ext in ['fa', 'fna', 'fasta']:
+                    exts.append(ext)
+
+        if len(exts) == 0:
+            return None, None
+
+        ext, count = Counter(exts).most_common(1)[0]
+        return ext, count
+
+    def _bin_dirs(self, options):
+        """Get directories with bins from different binners."""
+
+        bin_dirs = {}
+        if hasattr(options, 'bin_dirs') and options.bin_dirs:
+            for d in options.bin_dirs:
+                check_dir_exists(d)
+                method_id = os.path.basename(os.path.normpath(d))
+                bin_ext, count = self._bin_extension(d)
+                if not bin_ext:
+                    self.logger.warning(
+                        f'No bins identified for {method_id} in {d}.')
+                else:
+                    bin_dirs[method_id] = (d, bin_ext)
+                    self.logger.info(
+                        f"Processing {count} genomes from {method_id} with extension '{bin_ext}'.")
+
+        if hasattr(options, 'bin_file') and options.bin_file:
+            check_file_exists(options.bin_file)
+            for line in open(options.bin_file):
+                if line.strip():
+                    tokens = [token.strip() for token in line.split('\t')]
+                    if len(tokens) != 2:
+                        self.logger.warning(
+                            f"Skipping invalid line: {line.strip()}")
+                        continue
+
+                    method_id = tokens[0]
+                    d = tokens[1]
+                    check_dir_exists(d)
+                    bin_ext, count = self._bin_extension(d)
+                    if not bin_ext:
+                        self.logger.warning(
+                            f'No bins identified for {method_id} in {d}.')
+                    else:
+                        bin_dirs[method_id] = (d, bin_ext)
+                        self.logger.info(
+                            f"Processing {count} genomes from {method_id} with extension '{bin_ext}'.")
+
+        return bin_dirs
+
+    def bin(self, options):
+        """Bin command"""
+
+        check_file_exists(options.assembly_file)
+        make_sure_path_exists(options.output_dir)
+
+        bin = Bin(options.assembly_file,
+                  options.output_dir,
+                  options.min_contig_len,
+                  options.seed,
+                  options.cpus)
+        bin.check_on_path(options)
+        bin.coverage(options.bam_files, options.cov_file)
+        bin.run(options)
+
+        self.logger.info(
+            f"UniteM 'bin' results written to: {options.output_dir}")
+
+    def profile(self, options):
+        """Profile command"""
+
+        make_sure_path_exists(options.output_dir)
+
+        bin_dirs = self._bin_dirs(options)
+
+        profile = Profile(options.cpus)
+        profile.run(bin_dirs,
+                    options.marker_dir,
+                    options.keep_intermediate,
+                    options.output_dir)
+
+        self.logger.info(
+            f"UniteM 'profile' results written to: {options.output_dir}")
+
+    def consensus(self, options):
+        """Consensus command"""
+
+        check_dir_exists(options.profile_dir)
+        make_sure_path_exists(options.output_dir)
+
+        bin_dirs = self._bin_dirs(options)
+
+        e = Ensemble(options.bin_prefix)
+        e.run(options.profile_dir,
+              bin_dirs,
+              options.marker_dir,
+              options.weight,
+              options.sel_min_quality,
+              options.sel_min_comp,
+              options.sel_max_cont,
+              options.remove_perc,
+              options.add_perc,
+              options.add_matches,
+              False,  # perform consensus bin selection
+              False,  # perform unanimous bin selection
+              options.report_min_quality,
+              options.simple_headers,
+              options.output_dir)
+
+        self.logger.info(
+            f"UniteM 'consensus' results written to: {options.output_dir}")
+
+    def greedy(self, options):
+        """Greedy command"""
+
+        check_dir_exists(options.profile_dir)
+        make_sure_path_exists(options.output_dir)
+
+        bin_dirs = self._bin_dirs(options)
+
+        e = Ensemble(options.bin_prefix)
+        e.run(options.profile_dir,
+              bin_dirs,
+              options.marker_dir,
+              options.weight,
+              options.sel_min_quality,
+              options.sel_min_comp,
+              options.sel_max_cont,
+              None,
+              None,
+              None,
+              True,   # perform greedy bin selection
+              False,  # perform unanimous bin selection
+              options.report_min_quality,
+              options.simple_headers,
+              options.output_dir)
+
+        self.logger.info(
+            f"UniteM 'greedy' results written to: {options.output_dir}")
+
+    def unanimous(self, options):
+        """Unanimous command"""
+
+        check_dir_exists(options.profile_dir)
+        make_sure_path_exists(options.output_dir)
+
+        bin_dirs = self._bin_dirs(options)
+
+        e = Ensemble(options.bin_prefix)
+        e.run(options.profile_dir,
+              bin_dirs,
+              options.marker_dir,
+              options.weight,
+              options.sel_min_quality,
+              options.sel_min_comp,
+              options.sel_max_cont,
+              None,
+              None,
+              None,
+              False,  # perform greedy bin selection
+              True,   # perform unanimous bin selection
+              options.report_min_quality,
+              options.simple_headers,
+              options.output_dir)
+
+        self.logger.info(
+            f"UniteM 'unanimous' results written to: {options.output_dir}")
+
+    def compare(self, options):
+        """Compare command"""
+
+        check_dir_exists(options.bin_dir1)
+        check_dir_exists(options.bin_dir2)
+
+        bt = BinTools()
+        bin_files1 = bt.bin_files(options.bin_dir1, options.extension1)
+        bin_files2 = bt.bin_files(options.bin_dir2, options.extension2)
+
+        bt.compare(bin_files1, bin_files2,
+                   options.assembly_file, options.output_file)
+
+        self.logger.info("UniteM 'compare' results written to: %s" %
+                         options.output_file)
+
+    def unique(self, options):
+        """Unique command"""
+
+        check_dir_exists(options.bin_dir)
+
+        bt = BinTools()
+        bin_files = bt.bin_files(options.bin_dir, options.extension)
+        bt.unique(bin_files)
+
+    def parse_options(self, options):
+        """Parse user options and call the correct pipeline(s)"""
+
+        if(options.subparser_name == 'bin'):
+            self.bin(options)
+        elif(options.subparser_name == 'profile'):
+            self.profile(options)
+        elif(options.subparser_name == 'consensus'):
+            self.consensus(options)
+        elif(options.subparser_name == 'greedy'):
+            self.greedy(options)
+        elif(options.subparser_name == 'unanimous'):
+            self.unanimous(options)
+        elif(options.subparser_name == 'compare'):
+            self.compare(options)
+        elif(options.subparser_name == 'unique'):
+            self.unique(options)
+        else:
+            self.logger.error(
+                f'Unknown UniteM command: {options.subparser_name}\n')
+            sys.exit(1)
+
+        return 0
