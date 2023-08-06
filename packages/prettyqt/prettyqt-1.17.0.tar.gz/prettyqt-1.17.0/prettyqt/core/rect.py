ea9b5292db6f4a5d8aeb9edf062f5b1031609b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:52 +0000

    Add package prettyqt/1.17.0/prettyqt-1.17.0.tar.gz

---
+++ packages/prettyqt/prettyqt-1.17.0.tar.gz/prettyqt-1.17.0/prettyqt/core/rect.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from prettyqt.qt import QtCore
+from prettyqt.utils import datatypes
+
+
+class Rect(QtCore.QRect):
+    def __repr__(self):
+        return (
+            f"{type(self).__name__}({self.x()}, {self.y()}, "
+            f"{self.width()}, {self.height()})"
+        )
+
+    def __reduce__(self):
+        return type(self), (self.x(), self.y(), self.width(), self.height())
+
+    def margins_added(self, margins: datatypes.MarginsType) -> Rect:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMargins(*margins)
+        return Rect(self.marginsAdded(margins))
+
+    def margins_removed(self, margins: datatypes.MarginsType) -> Rect:
+        if isinstance(margins, tuple):
+            margins = QtCore.QMargins(*margins)
+        return Rect(self.marginsRemoved(margins))
+
+
+if __name__ == "__main__":
+    rect = Rect(0, 2, 5, 5)
+    print(repr(rect))
