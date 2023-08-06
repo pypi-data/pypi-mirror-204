1399f32b29adf590de4ed3d1aaa864c78c047fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:05 +0000

    Add package yashpy/0.3/yashpy-0.3.tar.gz

---
+++ packages/yashpy/yashpy-0.3.tar.gz/yashpy-0.3/yashpy/__init__.py
@@ -0,0 +1,13 @@
+def add(num1,num2):
+    a=num1+num2
+    print("addition of this two numbers is:")
+    return a
+def sub(num1,num2):
+    print("subtraction of two numbers is:")
+    return num1-num2
+def mult(num1,num2):
+    print("multilplication of the two numbers is:")
+    return num1*num2
+def div(num1,num2):
+    print("divisio of the two numbers is:")
+    return num1 % num2
