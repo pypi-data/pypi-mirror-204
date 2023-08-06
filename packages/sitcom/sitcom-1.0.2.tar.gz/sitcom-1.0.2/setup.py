eddec0e2007791b7cfea23d0253836509bd42684
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:22 +0000

    Add package sitcom/1.0.2/sitcom-1.0.2.tar.gz

---
+++ packages/sitcom/sitcom-1.0.2.tar.gz/sitcom-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='sitcom',
+    version='1.0.2',
+    packages=['sitcom'],
+    url='https://github.com/pu3/SITCoM',
+    license='LICENSE',
+    author='Purvi Udhwani',
+    author_email='purviaries@aries.res.in',
+    description='SITCoM: SiRGraF Integrated Tool for Coronal dynaMics',
+    install_requires=[
+        'PyQt5','opencv-python','numpy','matplotlib','scikit-image','sunpy','astropy'
+    ],
+    package_data={'sitcom': ['data/*','icon/*','load/*','font/*']},
+)
