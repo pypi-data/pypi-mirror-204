65b2a5fe887409906f78a8b8a4988c07159d2035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:56 +0000

    Add package lovehim/0.3/lovehim-0.3.tar.gz

---
+++ packages/lovehim/lovehim-0.3.tar.gz/lovehim-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lovehim',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'lovehim = lovehim.get_verse:main',
+        ],
+    },
+)
