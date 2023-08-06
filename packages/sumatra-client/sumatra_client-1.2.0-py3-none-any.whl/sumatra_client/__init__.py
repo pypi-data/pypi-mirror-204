f294bf9e8184b63db9777bc11f34ad30c43df05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:12 +0000

    Add package sumatra-client/1.2.0/sumatra_client-1.2.0-py3-none-any.whl

---
+++ packages/sumatra-client/sumatra_client-1.2.0-py3-none-any.whl/sumatra_client/__init__.py
@@ -0,0 +1,8 @@
+from sumatra_client.client import Client, TableVersion, ModelVersion, Materialization
+
+__all__ = [
+    "Client",
+    "TableVersion",
+    "ModelVersion",
+    "Materialization",
+]
