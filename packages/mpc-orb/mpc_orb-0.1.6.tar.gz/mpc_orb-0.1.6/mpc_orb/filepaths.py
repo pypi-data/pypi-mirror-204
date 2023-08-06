507081a78b7572e29f4f3c2243ae065a554de07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:54 +0000

    Add package mpc-orb/0.1.6/mpc_orb-0.1.6.tar.gz

---
+++ packages/mpc-orb/mpc_orb-0.1.6.tar.gz/mpc_orb-0.1.6/mpc_orb/filepaths.py
@@ -0,0 +1,14 @@
+"""
+Defining filepaths used by mpc_orb
+"""
+
+# Import standard packages
+# -----------------------
+import glob
+
+# Directories / Filepaths / ...
+# -----------------------
+# Relative filepath to json schema against which everything is validated
+# - Gets the highest-numbered version in the directory
+schema_relative_filepath = "schema_json/mpcorb_schema_latest.json"
+
