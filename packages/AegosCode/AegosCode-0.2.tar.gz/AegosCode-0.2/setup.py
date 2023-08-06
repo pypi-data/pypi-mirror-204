21410ceeccb29e4953ac297a57db3d0259794660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:38 +0000

    Add package AegosCode/0.2/AegosCode-0.2.tar.gz

---
+++ packages/AegosCode/AegosCode-0.2.tar.gz/AegosCode-0.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name= 'AegosCode',
+    version='0.2',
+    author= 'Aegos',
+    description='Checking emails for any domain , for example : gmail,yahoo,aol,hotmail,outlook,mail_ru',
+    packages= setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
