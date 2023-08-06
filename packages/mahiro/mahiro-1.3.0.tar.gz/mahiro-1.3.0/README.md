4eab524b2438c86493dd0d8347f071e214817097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:04 +0000

    Add package mahiro/1.3.0/mahiro-1.3.0.tar.gz

---
+++ packages/mahiro/mahiro-1.3.0.tar.gz/mahiro-1.3.0/README.md
@@ -0,0 +1,5 @@
+# mahiro
+
+Python bridge for Mahiro.
+
+See more information at [mahiro](https://github.com/opq-osc/mahiro)
---
+++ packages/mahiro/mahiro-1.3.0.tar.gz/mahiro-1.3.0/setup.py
@@ -0,0 +1,25 @@
+import io
+from setuptools import find_packages, setup
+
+def read_files(files):
+    data = []
+    for file in files:
+        with io.open(file, encoding="utf-8") as f:
+            data.append(f.read())
+    return "\n".join(data)
+
+setup(
+    name="mahiro",
+    description="Python bridge for Mahiro",
+    long_description=read_files(["README.md"]),
+    long_description_content_type="text/markdown",
+    version="1.3.0",
+    author="fz6m",
+    author_email="i@fz6m.com",
+    url="https://github.com/opq-osc/mahiro",
+    license="MIT",
+    keywords=["OPQ", "OPQBot", "mahiro"],
+    packages=find_packages(),
+    install_requires=read_files(["requirements.txt"]),
+    python_requires=">=3.7",
+)
