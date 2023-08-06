e92463dbf399cb2fec38598da143ea5b79f30cd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:09 +0000

    Add package aimemory/0.0.1/aimemory-0.0.1.tar.gz

---
+++ packages/aimemory/aimemory-0.0.1.tar.gz/aimemory-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='aimemory',
+    version='0.0.1',
+    description='Manage and search embeddings vectors',
+    long_description='',
+    url='https://www.example.com',
+    author='me',
+    author_email='me@example.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
