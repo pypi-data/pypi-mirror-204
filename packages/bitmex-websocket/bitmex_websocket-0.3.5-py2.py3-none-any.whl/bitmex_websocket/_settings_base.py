15aaf744120b47c5d55f3ee16ff7f148e2292bbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:00 +0000

    Add package bitmex-websocket/0.3.5/bitmex_websocket-0.3.5-py2.py3-none-any.whl

---
+++ packages/bitmex-websocket/bitmex_websocket-0.3.5-py2.py3-none-any.whl/bitmex_websocket/_settings_base.py
@@ -0,0 +1,22 @@
+import logging
+import os
+import alog
+
+BASE_URL = os.environ.get('BITMEX_BASE_URL', "https://www.bitmex.com/api/v1/")
+
+# The BitMEX API requires permanent API keys. Go to
+# https://bitmex.com/api/apiKeys to fill these out.
+
+BITMEX_API_KEY = os.environ.get('BITMEX_API_KEY')
+BITMEX_API_SECRET = os.environ.get('BITMEX_API_SECRET')
+
+LOG_LEVEL = os.environ.get('LOG_LEVEL')
+
+if LOG_LEVEL is None:
+    LOG_LEVEL = logging.INFO
+
+LOG_LEVEL = logging.getLevelName(LOG_LEVEL)
+
+alog.debug(LOG_LEVEL)
+
+alog.set_level(LOG_LEVEL)
