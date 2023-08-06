2bef32510341a7bbd39238e6f265ab764ae57571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:28 +0000

    Add package itkwasm/1.0b97/itkwasm-1.0b97-py3-none-any.whl

---
+++ packages/itkwasm/itkwasm-1.0b97-py3-none-any.whl/itkwasm/__init__.py
@@ -0,0 +1,44 @@
+"""itkwasm: Python interface to itk-wasm WebAssembly modules."""
+
+__version__ = "1.0b97"
+
+from .interface_types import InterfaceTypes
+from .image import Image, ImageType
+from .pointset import PointSet, PointSetType
+from .mesh import Mesh, MeshType
+from .polydata import PolyData, PolyDataType
+from .binary_file import BinaryFile
+from .binary_stream import BinaryStream
+from .text_file import TextFile
+from .text_stream import TextStream
+from .pipeline import Pipeline
+from .pipeline_input import PipelineInput
+from .pipeline_output import PipelineOutput
+from .float_types import FloatTypes
+from .int_types import IntTypes
+from .pixel_types import PixelTypes
+from .environment_dispatch import environment_dispatch, function_factory
+
+__all__ = [
+  "InterfaceTypes",
+  "Pipeline",
+  "PipelineInput",
+  "PipelineOutput",
+  "Image",
+  "ImageType",
+  "PointSet",
+  "PointSetType",
+  "Mesh",
+  "MeshType",
+  "PolyData",
+  "PolyDataType",
+  "BinaryFile",
+  "BinaryStream",
+  "TextFile",
+  "TextStream",
+  "FloatTypes",
+  "IntTypes",
+  "PixelTypes",
+  "environment_dispatch",
+  "function_factory",
+  ]
