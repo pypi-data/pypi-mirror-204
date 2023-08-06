a6620c7beb488eb8aad2a484da226da9d8f0c9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:38 +0000

    Add package img-host/0.1.0/img-host-0.1.0.tar.gz

---
+++ packages/img-host/img-host-0.1.0.tar.gz/img-host-0.1.0/README.md
@@ -0,0 +1,3 @@
+Helps you to download, crop and reupload new photo to [vgy.me](https://vgy.me)
+
+Requires `VGY_KEY` environment variable.
---
+++ packages/img-host/img-host-0.1.0.tar.gz/img-host-0.1.0/tests/__main__.py
@@ -0,0 +1,13 @@
+from asyncio import run
+
+import img_host
+
+URL = "https://telegra.ph/file/334f0f0dc80e933f61a8c.jpg"
+
+
+async def main():
+    r = await img_host.reupload(URL, 2 / 4)
+    print(r)
+
+
+run(main())
