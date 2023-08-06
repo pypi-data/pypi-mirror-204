4425ce0849aa95907cdc9f9f9c8bf4b9d1625a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:54 +0000

    Add package tabularcompare/0.0.3/tabularcompare-0.0.3.tar.gz

---
+++ packages/tabularcompare/tabularcompare-0.0.3.tar.gz/tabularcompare-0.0.3/README.md
@@ -0,0 +1,191 @@
+# TabularCompare
+
+[![Python package](https://github.com/erich-hs/tabularcompare/actions/workflows/python-package.yml/badge.svg)](https://github.com/erich-hs/tabularcompare/actions/workflows/python-package.yml) [![PyPI license](https://img.shields.io/pypi/l/tabularcompare)](https://pypi.python.org/pypi/tabularcompare/) [![PyPI pyversions](https://img.shields.io/pypi/pyversions/tabularcompare.svg)](https://img.shields.io/pypi/pyversions/tabularcompare) ![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)
+
+Tabular data comparison wrapper for [DataComPy](https://capitalone.github.io/datacompy/).
+
+## Quick Install
+```bash
+pip install tabularcompare
+```
+
+## Basic Usage
+
+### Comparison object
+```python
+import pandas as pd
+from tabularcompare import Comparison
+
+df1 = pd.DataFrame(
+    {
+        "idx1": ["A", "B", "B", "C"],
+        "idx2": ["01", "01", "02", "03"],
+        "colA": ["AA", "BA", "BB", "CA"],
+        "colB": [100, 200, 200, 300]
+     }
+)
+df2 = pd.DataFrame(
+    {
+        "idx1": ["A", "B", "C"],
+        "idx2": ["01", "01", "03"],
+        "colA": ["AA", "XA", "CA"],
+        "colB": [101, 200, 300],
+        "colC": ["foo", "bar", "baz"]
+     }
+)
+
+comparison = Comparison(
+    df1, df2, join_columns=["idx1", "idx2"]
+)
+```
+### Added Functionalities
+- #### Diverging Subset
+This method introduces an enhanced look at the changes identified at the intersection of compared DataFrames, following the notation ```{df1} --> {df2}```.
+```python
+comparison.diverging_subset()
+```
+|   | idx1 | idx2 |          colA |            colB |
+|:-:|-----:|-----:|--------------:|----------------:|
+| 0 | A    | 01   | NaN           | {100} --> {101} |
+| 1 | B    | 01   | {BA} --> {XA} | NaN             |
+
+Rows that are unique to either DataFrame can be called via ```.df1_unq_rows()``` and ```.df2_unq_rows()``` methods.
+
+```python
+comparison.df1_unq_rows()
+```
+|   | idx1 | idx2 |	colA | colB |
+|:-:|-----:|-----:|-----:|-----:|
+| 2 |	B  |  02  |  BB  |	200 |
+
+Columns that are unique to either DataFrame can be called via ```.df1_unq_columns()``` and ```.df2_unq_columns()``` methods.
+
+```python
+comparison.df2_unq_columns()
+```
+|   | idx1 | idx2 |	 colC |
+|:-:|-----:|-----:|------:|
+| 0 |	A  |  01  |  foo  |
+| 0 |	B  |  01  |  bar  |
+| 0 |	C  |  03  |  baz  |
+
+- #### Enhanced Reporting
+The report functionality is now callable from the comparison object. It includes a .txt, .html, and .xlsx version.
+```python
+comparison.report_to_txt("./results/Report.txt")
+comparison.report_to_html("./results/Report.html")
+comparison.report_to_xlsx("./results/Report.xlsx", write_originals=True)
+```
+The Excel report will output complete comparison results, with tabs dedicated to:
+* Original dataframes (when ```write_originals=True```).
+* Columns present only on df1 and/or df2.
+* Rows present only on df1 and/or df2.
+* Diverging subset showing all the changes identified from df1 to df2.
+
+The HTML report will also output a rendered table of the diverging subset on top of the file, alongside the native DataComPy summary report.
+
+- #### Fully-fledged [Command Line Interface](#CLI)
+---
+### DataComPy Methods
+Most methods native to ```datacompy.Compare``` functionality are still present, including;
+* ```.report()```
+* ```.df1_unq_rows()``` / ```.df2_unq_rows()```
+* ```.df1_unq_columns()``` / ```.df2_unq_columns()```
+* ```.intersect_columns()```
+* ```.intersect_rows()```
+
+The native ```datacompy.Compare``` method is also callable from the ```tabularcompare``` core module:
+```python
+from tabularcompare import Compare
+
+# datacompy.Compare method
+comparison = Compare(
+    df1, df2, join_columns=["idx1", "idx2"]
+)
+print(comparison.report())
+```
+For a complete documentation on DataComPy you can head to [DataComPy](https://capitalone.github.io/datacompy/).
+
+-----
+## CLI
+Command Line Interface to output an Excel .xlsx report and, optionally, html and txt summaries.
+```bash
+tabularcompare --help
+```
+
+```
+Usage: tabularcompare [OPTIONS] DF1 DF2
+
+Options:
+  -c, --columns, --on TEXT    Comma-separated list of key column(s) to compare
+                              df1 against df2. When not provided, df1 and df2
+                              will be matched on index.
+  -ic, --ignore_columns TEXT  Comma-separated list of column(s) to ignore from
+                              df1 and df2.
+  -n1, --df1_name TEXT        Alias for Data frame 1. Default = df1
+  -n2, --df2_name TEXT        Alias for Data frame 2. Default = df2
+  -is, --ignore_spaces        Flag to strip and ignore whitespaces from string
+                              columns.
+  -ci, --case_insensitive     Flag to compare string columns on a case-
+                              insensitive manner.
+  -cl, --cast_lowercase       Flag to cast column names to lower case before
+                              comparison.
+  -at, --abs_tol FLOAT        Absolute tolerance between two numeric values.
+  -rt, --rel_tol FLOAT        Relative tolerance between two numeric values.
+  -txt, --txt                 Flag to output a .txt report with a comparison
+                              summary.
+  -html, --html               Flag to output an HTML report with a comparison
+                              summary.
+  -od, --only_deltas          Flag to suppress original dataframes from the
+                              output .xlsx report.
+  -o, --output, --out PATH    Output location for report files. Defaults to
+                              current location.
+  -e, --encoding TEXT         Character encoding to read df1 and df2.
+  -v, --verbose               Verbosity.
+  --help                      Show this message and exit.
+```
+
+### Sample usage
+The application reads from two file paths input for csv, json, or excel files.
+```bash
+cd ./data/
+tabularcompare ./df1.csv ./df2.csv -c 'idx1,idx2' -n1 myTable1 -n2 myTable2 -o ../results/
+```
+
+-----
+## Caveat
+> The comparison results will take into account data types across columns. I.E. If we update our sample dataframe df2 to include a missing value on colB, it will now be of dtype ```object```, as oposed to ```Int64``` in df1. This might lead to miss-leading interpretations of the results to users without information on data types.
+
+```python
+import pandas as pd
+from tabularcompare import Comparison
+
+df1 = pd.DataFrame(
+    {
+        "idx1": ["A", "B", "B", "C"],
+        "idx2": ["01", "01", "02", "03"],
+        "colA": ["AA", "BA", "BB", "CA"],
+        "colB": [100, 200, 200, 300]
+     }
+)
+df2 = pd.DataFrame(
+    {
+        "idx1": ["A", "B", "C"],
+        "idx2": ["01", "01", "03"],
+        "colA": ["AA", "XA", "CA"],
+        "colB": [pd.NA, 200, 300],
+        "colC": ["foo", "bar", "baz"]
+     }
+)
+
+comparison = Comparison(
+    df1, df2, join_columns=["idx1", "idx2"]
+)
+comparison.diverging_subset()
+```
+Which will return:
+|   | idx1 | idx2 |          colA |            colB |
+|:-:|-----:|-----:|--------------:|----------------:|
+| 0 | A    | 01   | NaN           | {100} --> {}    |
+| 1 | B    | 01   | {BA} --> {XA} | {200} --> {200} |
+| 3 | C    | 03   | NaN           | {300} --> {300} |
---
+++ packages/tabularcompare/tabularcompare-0.0.3.tar.gz/tabularcompare-0.0.3/tests/test_compare.py
@@ -0,0 +1,172 @@
+import pytest
+import numpy as np
+import pandas as pd
+from tabularcompare import Comparison
+
+# Test parameters
+@pytest.fixture
+def create_test_data():
+    df1 = pd.DataFrame(
+        {"idx1": [1, 1, 2, 3, 3, 4],
+        "idx2": ["A", "B", "A", "A", "B", "A"],
+        "fname": ["Nikola", "Nikola", "Albert", "Stephen", "Stephen", "Steve"],
+        "lname": ["Tesla", "Tesla", "Einstein", "Hawking", "Hawking", "Jobs"],
+        "emptycol": [pd.NA for i in range(6)],
+        "email": ["em@tesla.com", "em@spacex.com", "ae@gmail.com", "sh@cam.uk", pd.NA, "sj@apple.com"],
+        "dob": ["1856-07-10", "1856-07-10", "1879-03-14", "1942-01-08", "1942-01-08", "1955-02-24"],
+        "active": [True, False, True, True, False, True]}
+    )
+    df1["dob"] = pd.to_datetime(df1["dob"])
+    df2 = pd.DataFrame(
+        {"idx1": [1, 1, 2, 3, 3, 2],
+        "idx2": ["A", "B", "A", "A", "B", "A"],
+        "fname": ["Nick", "Nick", "Albert", "Stephen", "Stephen", "Albert"],
+        "lname": ["Tesla", "Tesla", "Einstein", "Hawking", "Hawking", "Einstein"],
+        "emptycol": [pd.NA for i in range(6)],
+        "email": ["em@tesla.com", "em@spacex.com", "ae@gmail.com", "sh@cam.uk", pd.NA, "ae@hotmail.com"],
+        "dob": ["1856-07-10", pd.NA, "1879-03-14", "1942-03-08", "1942-01-08", "1879-03-14"],
+        "active": [True, False, True, True, False, pd.NA]}
+    )
+    df2["dob"] = pd.to_datetime(df2["dob"])
+    join_columns = ["idx1", "idx2"]
+    return df1, df2, join_columns
+
+def test_report(create_test_data: tuple):
+    expected_partial_report = 'DataComPy Comparison\n--------------------\n\nDataFrame Summary\n-----------------\n\n  DataFrame  Columns  Rows\n0       df1        8     6\n1       df2        8     6\n\nColumn Summary\n--------------\n\nNumber of columns in common: 8\nNumber of columns in df1 but not in df2: 0\nNumber of columns in df2 but not in df1: 0\n\nRow Summary\n-----------\n\nMatched on: idx1, idx2\nAny duplicates on match values: Yes\nAbsolute Tolerance: 0\nRelative Tolerance: 0\nNumber of rows in common: 5\nNumber of rows in df1 but not in df2: 1\nNumber of rows in df2 but not in df1: 1\n\nNumber of rows with some compared columns unequal: 3\nNumber of rows with all compared columns equal: 2\n\nColumn Comparison\n-----------------\n\nNumber of columns compared with some values unequal: 2\nNumber of columns compared with all values equal: 6\nTotal number of values which compare unequal: 4\n\nColumns with Unequal Values or Types\n------------------------------------'
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    report = comparison.report()
+    assert expected_partial_report in report
+
+def test_diverging_subset(create_test_data: tuple):
+    expected_diverging_subset = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 3: 3},
+     'idx2': {0: 'A', 1: 'B', 3: 'A'},
+     'fname': {0: '{Nikola} --> {Nick}', 1: '{Nikola} --> {Nick}', 3: np.nan},
+     'dob': {0: np.nan,
+             1: '{1856-07-10} --> {NaT}',
+             3: '{1942-01-08} --> {1942-03-08}'}}
+    )
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    diverging_subset = comparison.diverging_subset()
+    assert diverging_subset.equals(expected_diverging_subset)
+
+def test_df1_unq_columns(create_test_data: tuple):
+    expected_df1_unq_columns = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 2: 2, 3: 3, 4: 3, 5: 4},
+     'idx2': {0: 'A', 1: 'B', 2: 'A', 3: 'A', 4: 'B', 5: 'A'}}
+    )
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    df1_unq_columns = comparison.df1_unq_columns()
+    assert df1_unq_columns.equals(expected_df1_unq_columns)
+
+def test_df2_unq_columns(create_test_data: tuple):
+    expected_df2_unq_columns = pd.DataFrame(
+    {'idx1': {0: 1, 1: 1, 2: 2, 3: 3, 4: 3, 5: 2},
+     'idx2': {0: 'A', 1: 'B', 2: 'A', 3: 'A', 4: 'B', 5: 'A'}}
+    )
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    df2_unq_columns = comparison.df2_unq_columns()
+    assert df2_unq_columns.equals(expected_df2_unq_columns)
+
+def test_df1_unq_rows(create_test_data: tuple):
+    expected_df1_unq_rows = pd.DataFrame(
+    {'idx1': {5: 4},
+     'idx2': {5: 'A'},
+     'fname': {5: 'Steve'},
+     'lname': {5: 'Jobs'},
+     'emptycol': {5: ""},
+     'email': {5: 'sj@apple.com'},
+     'dob': {5: pd.Timestamp('1955-02-24 00:00:00')},
+     'active': {5: True}}
+    )
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    df1_unq_rows = comparison.df1_unq_rows().fillna("")
+    assert df1_unq_rows.equals(expected_df1_unq_rows)
+
+def test_df2_unq_rows(create_test_data: tuple):
+    expected_df2_unq_rows = pd.DataFrame(
+    {'idx1': {6: 2},
+     'idx2': {6: 'A'},
+     'fname': {6: 'Albert'},
+     'lname': {6: 'Einstein'},
+     'emptycol': {6: ""},
+     'email': {6: 'ae@hotmail.com'},
+     'dob': {6: pd.Timestamp('1879-03-14 00:00:00')},
+     'active': {6: ""}}
+    )
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    df2_unq_rows = comparison.df2_unq_rows().fillna("")
+    assert df2_unq_rows.equals(expected_df2_unq_rows)
+
+def test_intersect_columns(create_test_data: tuple):
+    expected_intersect_columns = ['idx1', 'idx2', 'fname', 'lname', 'emptycol', 'email', 'dob', 'active']
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    intersect_columns = comparison.intersect_columns()
+    assert intersect_columns == expected_intersect_columns
+
+def test_intersect_rows(create_test_data: tuple):
+    expected_intersect_rows = pd.DataFrame({
+        'idx1': {0: 1, 1: 1, 2: 2, 3: 3, 4: 3},
+        'idx2': {0: 'A', 1: 'B', 2: 'A', 3: 'A', 4: 'B'},
+        'fname_df1': {0: 'Nikola',
+        1: 'Nikola',
+        2: 'Albert',
+        3: 'Stephen',
+        4: 'Stephen'},
+        'lname_df1': {0: 'Tesla',
+        1: 'Tesla',
+        2: 'Einstein',
+        3: 'Hawking',
+        4: 'Hawking'},
+        'emptycol_df1': {0: '', 1: '', 2: '', 3: '', 4: ''},
+        'email_df1': {0: 'em@tesla.com',
+        1: 'em@spacex.com',
+        2: 'ae@gmail.com',
+        3: 'sh@cam.uk',
+        4: ''},
+        'dob_df1': {0: pd.Timestamp('1856-07-10 00:00:00'),
+        1: pd.Timestamp('1856-07-10 00:00:00'),
+        2: pd.Timestamp('1879-03-14 00:00:00'),
+        3: pd.Timestamp('1942-01-08 00:00:00'),
+        4: pd.Timestamp('1942-01-08 00:00:00')},
+        'active_df1': {0: True, 1: False, 2: True, 3: True, 4: False},
+        'fname_df2': {0: 'Nick', 1: 'Nick', 2: 'Albert', 3: 'Stephen', 4: 'Stephen'},
+        'lname_df2': {0: 'Tesla',
+        1: 'Tesla',
+        2: 'Einstein',
+        3: 'Hawking',
+        4: 'Hawking'},
+        'emptycol_df2': {0: '', 1: '', 2: '', 3: '', 4: ''},
+        'email_df2': {0: 'em@tesla.com',
+        1: 'em@spacex.com',
+        2: 'ae@gmail.com',
+        3: 'sh@cam.uk',
+        4: ''},
+        'dob_df2': {0: pd.Timestamp('1856-07-10 00:00:00'),
+        1: pd.Timestamp('2000-01-01 00:00:00'),
+        2: pd.Timestamp('1879-03-14 00:00:00'),
+        3: pd.Timestamp('1942-03-08 00:00:00'),
+        4: pd.Timestamp('1942-01-08 00:00:00')},
+        'active_df2': {0: True, 1: False, 2: True, 3: True, 4: False},
+        '_merge': {0: 'both', 1: 'both', 2: 'both', 3: 'both', 4: 'both'},
+        'fname_match': {0: False, 1: False, 2: True, 3: True, 4: True},
+        'lname_match': {0: True, 1: True, 2: True, 3: True, 4: True},
+        'emptycol_match': {0: True, 1: True, 2: True, 3: True, 4: True},
+        'email_match': {0: True, 1: True, 2: True, 3: True, 4: True},
+        'dob_match': {0: True, 1: False, 2: True, 3: False, 4: True},
+        'active_match': {0: True, 1: True, 2: True, 3: True, 4: True}
+    })
+    df1, df2, join_columns = create_test_data
+    comparison = Comparison(df1, df2, join_columns)
+    intersect_rows = comparison.intersect_rows().fillna("")
+    dtime_cols = intersect_rows.select_dtypes(include=['datetime'])
+    intersect_rows[dtime_cols.columns] = dtime_cols.fillna(pd.to_datetime('2000-01-01'))
+    intersect_rows["_merge"] = intersect_rows["_merge"].astype("object")
+    assert intersect_rows.equals(expected_intersect_rows)
