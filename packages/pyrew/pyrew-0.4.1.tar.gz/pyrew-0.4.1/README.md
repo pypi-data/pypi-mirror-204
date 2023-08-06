90bacdc724b7306c0a88ca72b51aa036685d001e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:38 +0000

    Add package pyrew/0.4.1/pyrew-0.4.1.tar.gz

---
+++ packages/pyrew/pyrew-0.4.1.tar.gz/pyrew-0.4.1/README.md
@@ -0,0 +1,19 @@
+# Pyrew
+
+Welcome to Pyrew (PYthon REWrite), a Python program that provides the ability to write shorter and more efficient Python code by simply downloading, installing and importing a single file.
+
+## Installation
+
+1. Run the following command:
+
+```
+pip install git+https://github.com/AquaQuokka/pyrew.git
+```
+
+2. At the top of your file, add the following code:
+
+```py
+import pyrew
+
+pyrew = pyrew.Pyrew()
+```
---
+++ packages/pyrew/pyrew-0.4.1.tar.gz/pyrew-0.4.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyrew',
+    version='0.4.1',
+    description='A Python library for writing shorter and more efficient Python code.',
+    long_description="Welcome to Pyrew, a simple Python library for writing shorter and more efficient Python code. Simply install and import the module.",
+    url="https://github.com/AquaQuokka/pyrew",
+    author="AquaQuokka",
+    license='BSD-3-Clause',
+    py_modules=['pyrew'],
+    scripts=['pyrew.py'],
+    install_requires=[]
+)
