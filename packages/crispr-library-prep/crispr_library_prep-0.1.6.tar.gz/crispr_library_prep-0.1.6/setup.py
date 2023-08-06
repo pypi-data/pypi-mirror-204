522de8c49e04fd4c0ed23be90b9b9093db4874c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:13 +0000

    Add package crispr-library-prep/0.1.6/crispr_library_prep-0.1.6.tar.gz

---
+++ packages/crispr-library-prep/crispr_library_prep-0.1.6.tar.gz/crispr_library_prep-0.1.6/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crispr_library_prep']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['biopython>=1.81,<2.0',
+ 'matplotlib>=3.7.1,<4.0.0',
+ 'numpy>=1.23,<2.0',
+ 'pandas>=1.5.3,<2.0.0',
+ 'scipy>=1.10.1,<2.0.0',
+ 'typeguard>=3.0.2,<4.0.0']
+
+setup_kwargs = {
+    'name': 'crispr-library-prep',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': '',
+    'author': 'Basheer Becerra',
+    'author_email': 'bbecerr@outlook.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.12',
+}
+
+
+setup(**setup_kwargs)
