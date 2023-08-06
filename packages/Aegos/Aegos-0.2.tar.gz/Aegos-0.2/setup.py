6e32f9c08d968f73c90e3b98a986eac77eda9d66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:24 +0000

    Add package Aegos/0.2/Aegos-0.2.tar.gz

---
+++ packages/Aegos/Aegos-0.2.tar.gz/Aegos-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+    name= 'Aegos',
+    version='0.2',
+    author= 'Aegos',
+    description='Checking emails for any domain , for example : gmail,yahoo,aol,hotmail,outlook,mail_ru',
+    packages= ['Aegos'],
+    author_email='syakkml@gmail.com',
+    zip_safe=False,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
