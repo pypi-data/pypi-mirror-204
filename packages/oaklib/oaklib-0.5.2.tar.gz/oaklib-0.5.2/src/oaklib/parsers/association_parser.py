e4b154cfbaad2c8e7e2895ef49914af4088f8896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:59 +0000

    Add package oaklib/0.5.2/oaklib-0.5.2.tar.gz

---
+++ packages/oaklib/oaklib-0.5.2.tar.gz/oaklib-0.5.2/src/oaklib/parsers/association_parser.py
@@ -0,0 +1,38 @@
+"""Parser for GAF/HPOA and related association formats"""
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Iterable, Iterator, Optional, TextIO, Union
+
+from oaklib.datamodels.association import (
+    Association,
+    NegatedAssociation,
+    ParserConfiguration,
+)
+from oaklib.parsers.parser_base import Parser
+
+
+@dataclass
+class AssociationParser(Parser, ABC):
+    """Base class for all association parsers."""
+
+    @abstractmethod
+    def parse(
+        self, file: TextIO, configuration: Optional[ParserConfiguration] = None, **kwargs
+    ) -> Iterator[Union[NegatedAssociation, Association]]:
+        raise NotImplementedError
+
+    def add_metadata(
+        self,
+        associations: Iterable[Union[NegatedAssociation, Association]],
+        primary_knowledge_source: Optional[str] = None,
+    ):
+        """
+        Add metadata to associations.
+
+        :param associations:
+        :param primary_knowledge_source:
+        :return:
+        """
+        if primary_knowledge_source:
+            for association in associations:
+                association.primary_knowledge_source = primary_knowledge_source
