a6cee78be34c566a8e81cabfd1e28b22a628b96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:14 +0000

    Add package plone.app.mosaic/3.0.2/plone.app.mosaic-3.0.2.tar.gz

---
+++ packages/plone.app.mosaic/plone.app.mosaic-3.0.2.tar.gz/plone.app.mosaic-3.0.2/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    # metadata in setup.cfg
+    name="plone.app.mosaic",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "Products.CMFPlone",
+        "plone.app.blocks",
+        "plone.app.tiles",
+        "plone.app.drafts",
+        "plone.app.standardtiles",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.contenttypes",
+            "plone.app.robotframework",
+            "robotframework-selenium2library",
+        ],
+        "docs": [
+            "Sphinx",
+            "sphinx-rtd-theme",
+            "sphinxcontrib-robotframework",
+        ],
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
