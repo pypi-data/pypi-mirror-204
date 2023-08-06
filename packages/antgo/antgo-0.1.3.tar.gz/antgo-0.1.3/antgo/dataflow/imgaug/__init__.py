1a32c43ce9498d7db5cb629c4f0cfd4ebac4b1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:37 +0000

    Add package antgo/0.1.3/antgo-0.1.3.tar.gz

---
+++ packages/antgo/antgo-0.1.3.tar.gz/antgo-0.1.3/antgo/dataflow/imgaug/__init__.py
@@ -0,0 +1,54 @@
+# encoding=utf-8
+# @Time    : 17-6-22
+# @File    : __init__.py
+# @Author  : jian<jian@mltalker.com>
+from .operators import (
+  DecodeImage, 
+  ResizeS, 
+  RandomFlipImage, 
+  RandomDistort, 
+  Rotation,
+  KeepRatio, 
+  ColorDistort,
+  RandomErasingImage,
+  CropImage,
+  MixupImage,
+  CutmixImage,
+  Meta,
+  RandomScaledCrop,
+  ResizeByLong,
+  ResizeRangeScaling,
+  ResizeStepScaling,
+  AutoAugmentImage,
+  Permute,
+  UnSqueeze,
+  ConvertRandomObjJointsAndOffset,
+  ResizeByShort,
+  RGB2BGR,
+  FixedCrop)
+
+__all__ = [
+  'DecodeImage', 
+  'ResizeS',
+  'RandomFlipImage', 
+  'RandomDistort', 
+  'Rotation', 
+  'KeepRatio', 
+  'ColorDistort',
+  'RandomErasingImage',
+  'CropImage',
+  'Permute',
+  'MixupImage',
+  'CutmixImage',
+  'RandomScaledCrop',
+  'ResizeByLong',
+  'ResizeRangeScaling',
+  'ResizeStepScaling',
+  'AutoAugmentImage',
+  'UnSqueeze',
+  'ConvertRandomObjJointsAndOffset',
+  'ResizeByShort',
+  'FixedCrop',
+  'RGB2BGR',
+  'Meta'
+]
