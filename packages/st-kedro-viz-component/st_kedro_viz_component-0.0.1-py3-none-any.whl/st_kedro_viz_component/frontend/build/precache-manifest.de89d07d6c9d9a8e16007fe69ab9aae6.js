1e10cdc83562a5551ecce614bc236bd468e0901d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:40 +0000

    Add package st-kedro-viz-component/0.0.1/st_kedro_viz_component-0.0.1-py3-none-any.whl

---
+++ packages/st-kedro-viz-component/st_kedro_viz_component-0.0.1-py3-none-any.whl/st_kedro_viz_component/frontend/build/precache-manifest.de89d07d6c9d9a8e16007fe69ab9aae6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ca297c4f8f667fac08ecb1f02f41f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa46f1e5deb6f0c51180",
+    "url": "./static/css/2.1096dae2.chunk.css"
+  },
+  {
+    "revision": "fa46f1e5deb6f0c51180",
+    "url": "./static/js/2.91a3a690.chunk.js"
+  },
+  {
+    "revision": "5c5fb831dec0f0e363dd967aa192f9f9",
+    "url": "./static/js/2.91a3a690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c669ecac6556539e80f",
+    "url": "./static/js/main.af195e39.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
