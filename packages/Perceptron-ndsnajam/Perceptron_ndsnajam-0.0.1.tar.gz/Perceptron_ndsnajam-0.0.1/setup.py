5e73e0ea4aae5f8a32e452e76dcb109ef8b5d356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:40 +0000

    Add package Perceptron-ndsnajam/0.0.1/Perceptron_ndsnajam-0.0.1.tar.gz

---
+++ packages/Perceptron-ndsnajam/Perceptron_ndsnajam-0.0.1.tar.gz/Perceptron_ndsnajam-0.0.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+PROJECT_NAME = 'Perceptron_ndsnajam'
+USER_NAME = 'nds-najam'
+
+setuptools.setup(
+    name = "Perceptron_ndsnajam",
+    version = "0.0.1",
+    author = USER_NAME,
+    author_email="najam.iitm@gmail.com",
+    description="Perceptron or/and/xor gates package",
+    long_description=long_description,
+    long_description_content_type = "text/markdown",
+    url = f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_urls = {
+    "Bug Tracker": f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues"
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6",
+    install_requires = [
+        "numpy==1.21.6",
+        "pandas==1.3.5",
+        "joblib==1.2.0",
+        "matplotlib==3.5.3"
+    ]
+)
