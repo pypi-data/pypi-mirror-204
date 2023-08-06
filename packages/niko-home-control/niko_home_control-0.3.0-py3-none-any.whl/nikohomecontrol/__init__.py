4c8108e8dec73f7a872a604ef30542782ec542c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:22 +0000

    Add package niko-home-control/0.3.0/niko_home_control-0.3.0-py3-none-any.whl

---
+++ packages/niko-home-control/niko_home_control-0.3.0-py3-none-any.whl/nikohomecontrol/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# new since 0.6.5
+from .nhcdiscover import (
+    NikoHomeControlDiscover
+)
+from .nhcmonitor import (
+    NikoHomeControlMonitor,
+    listen
+)
+from . nhcconnection import (
+    NikoHomeControlConnection
+)
+from .nikohomecontrol import (
+    NikoHomeControl
+)
+package_version = '0.3.0'
