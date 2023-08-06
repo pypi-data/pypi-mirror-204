cb41cbee58f178889adac5691076f1c31fea436d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:03 +0000

    Add package varya/0.0.26a1/varya-0.0.26a1-py3-none-any.whl

---
+++ packages/varya/varya-0.0.26a1-py3-none-any.whl/varya/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.0.26a1'
+__author__ = 'Kritik Seth'
+__maintainer__ = __author__
+__license__ = 'BSD 3-Clause License'
+__url__ = 'https://github.com/varya-ml/varya'
+__connect__ = 'https://www.kritikseth.com/'
---
+++ packages/varya/varya-0.0.26a1-py3-none-any.whl/varya/compose/__init__.py
@@ -0,0 +1,3 @@
+from .core import Datum
+
+__all__ = ['Datum']
