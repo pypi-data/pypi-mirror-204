c5602db23f32a1428485a2331e6daaad7494a126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:01 +0000

    Add package AMAS-sb/0.0.4/AMAS_sb-0.0.4-py3-none-any.whl

---
+++ packages/AMAS-sb/AMAS_sb-0.0.4-py3-none-any.whl/AMAS/recommend_species.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# recommend_species.py
+"""
+Predicts annotations of species using a local XML file
+and the species ID. 
+Usage: python recommend_species.py files/BIOMD0000000190.xml --min_score 0.6 --outfile res.csv
+"""
+
+import argparse
+import os
+from os.path import dirname, abspath
+import sys
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+
+from AMAS import constants as cn
+from AMAS import recommender
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Recommend species annotations of an SBML model and save results') 
+  parser.add_argument('model', type=str, help='SBML model file (.xml)')
+  # One or more species IDs can be given
+  parser.add_argument('--species', type=str, help='ID(s) of species to be recommended. If not provided, all species will be used', nargs='*')
+  parser.add_argument('--min_score', type=float, help='minimum match score threshold', nargs='?', default=0.0)
+  parser.add_argument('--method', type=str,
+                                  help='Choose either "top" or "above". "top" recommends ' +\
+                                       'the best candidates that are above the min_score, ' +\
+                                       'and "above" recommends all candidates that are above ' +\
+                                       'the min_score. Default is "top"',
+                                  nargs='?',
+                                  default='top')
+  parser.add_argument('--outfile', type=str, help='file path to save recommendation', nargs='?',
+                      default=os.path.join(os.getcwd(), 'species_rec.csv'))
+  args = parser.parse_args()
+  recom = recommender.Recommender(libsbml_fpath=args.model)
+  one_fpath = args.model
+  specs = args.species
+  min_score = args.min_score
+  method = args.method
+  outfile = args.outfile
+
+  try:
+    recom = recommender.Recommender(libsbml_fpath=one_fpath)
+    recom.current_type = 'species'
+    # if nothing is given, predict all IDs
+    if specs is None:
+      specs = recom.getSpeciesIDs()
+    print("...\nAnalyzing %d species...\n" % len(specs))
+    res = recom.getSpeciesListRecommendation(pred_ids=specs, get_df=True)
+    for idx, one_df in enumerate(res):
+      filt_df = recom.autoSelectAnnotation(df=one_df,
+                                           min_score=min_score,
+                                           method=method)
+      recom.updateSelection(specs[idx], filt_df)
+    # save file to csv
+    recom.saveToCSV(outfile)
+    print("Recommendations saved as:\n%s\n" % os.path.abspath(outfile))
+  except:
+  	raise ValueError("Please check arguments.")
+
+
+if __name__ == '__main__':
+  main()
