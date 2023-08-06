33224e3cdffb5cf2d7227f37eb07625a062f10b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:26 +0000

    Add package bs-orm/0.8/bs_orm-0.8.tar.gz

---
+++ packages/bs-orm/bs_orm-0.8.tar.gz/bs_orm-0.8/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bs_orm',
+      version='0.8',
+      description='better simple orm (everyone can take part in the development)',
+      packages=['bs_orm'],
+      author_email='mr.z.75@mail.ru',
+      zip_safe=False)
