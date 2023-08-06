caaef33484cfe0dc21e16807ba40bb534226eb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:30 +0000

    Add package ml-starter/0.0.22/ml-starter-0.0.22.tar.gz

---
+++ packages/ml-starter/ml-starter-0.0.22.tar.gz/ml-starter-0.0.22/ml/optimizers/adam.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+from torch import nn
+from torch.optim.adam import Adam
+
+from ml.core.config import conf_field
+from ml.core.registry import register_optimizer
+from ml.optimizers.base import BaseOptimizer, BaseOptimizerConfig
+
+
+@dataclass
+class AdamOptimizerConfig(BaseOptimizerConfig):
+    lr: float = conf_field(1e-3, help="Learning rate")
+    betas: tuple[float, float] = conf_field((0.9, 0.999), help="Beta coefficients")
+    eps: float = conf_field(1e-4, help="Epsilon term to add to the denominator for stability")
+    weight_decay: float = conf_field(0.0, help="Weight decay regularization to use")
+    amsgrad: bool = conf_field(False, help="Whether to use the AMSGrad variant of the algorithm")
+
+
+@register_optimizer("adam", AdamOptimizerConfig)
+class AdamOptimizer(BaseOptimizer[AdamOptimizerConfig]):
+    def get(self, model: nn.Module) -> Adam:
+        return Adam(
+            model.parameters(),
+            lr=self.config.lr,
+            betas=self.config.betas,
+            eps=self.config.eps,
+            weight_decay=self.config.weight_decay,
+            amsgrad=self.config.amsgrad,
+            **self.common_kwargs,
+        )
