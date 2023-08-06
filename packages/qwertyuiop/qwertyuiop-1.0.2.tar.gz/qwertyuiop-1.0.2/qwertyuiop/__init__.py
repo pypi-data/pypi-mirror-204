6568443edf2ef78aa678614dc358bf89986fa6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package qwertyuiop/1.0.2/qwertyuiop-1.0.2.tar.gz

---
+++ packages/qwertyuiop/qwertyuiop-1.0.2.tar.gz/qwertyuiop-1.0.2/qwertyuiop/__init__.py
@@ -0,0 +1,5 @@
+from qwertyuiop.classifiers import *
+from qwertyuiop.regressors import *
+from qwertyuiop.linear_algebra import *
+from qwertyuiop.metrics import *
+from qwertyuiop.data_processing import *
