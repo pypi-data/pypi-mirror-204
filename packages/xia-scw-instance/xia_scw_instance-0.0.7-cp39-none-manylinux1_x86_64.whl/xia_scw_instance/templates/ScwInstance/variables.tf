5a4abe9134478fe16058e9160c33dd508af7efbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:31 +0000

    Add package xia-scw-instance/0.0.7/xia_scw_instance-0.0.7-cp39-none-manylinux1_x86_64.whl

---
+++ packages/xia-scw-instance/xia_scw_instance-0.0.7-cp39-none-manylinux1_x86_64.whl/xia_scw_instance/templates/ScwInstance/variables.tf
@@ -0,0 +1,40 @@
+variable "state" {
+  type = string
+  default = null  #xia#
+  #xia# default = {% if state is defined and state is not none %}"{{ state }}"{% else %}null{% endif %}
+
+}
+
+variable "project_id" {
+  type = string
+  default = null  #xia#
+  #xia# default = {% if project_id is defined and project_id is not none %}"{{ project_id }}"{% else %}null{% endif %}
+
+}
+
+variable "name" {
+  type = string
+  default = null  #xia#
+  #xia# default = {% if name is defined and name is not none %}"{{ name }}"{% else %}null{% endif %}
+
+}
+
+variable "type" {
+  type = string
+  default = null  #xia#
+  #xia# default = {% if type is defined and type is not none %}"{{ type }}"{% else %}null{% endif %}
+
+}
+
+variable "image" {
+  type = string
+  default = null  #xia#
+  #xia# default = {% if image is defined and image is not none %}"{{ image }}"{% else %}null{% endif %}
+
+}
+
+variable "tags" {
+    type = list(string)
+    default = null  #xia#
+    #xia# default = [{% for v in tags %}{% if loop.index > 1 %}, {% endif %}{% if v is not none %}"{{ v }}"{% else %}null{% endif %}{% endfor %}]
+}
