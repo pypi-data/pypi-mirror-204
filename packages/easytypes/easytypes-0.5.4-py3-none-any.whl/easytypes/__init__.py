d64aafefa2fb556ddba79b016f95558e9e67e79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:02 +0000

    Add package easytypes/0.5.4/easytypes-0.5.4-py3-none-any.whl

---
+++ packages/easytypes/easytypes-0.5.4-py3-none-any.whl/easytypes/__init__.py
@@ -0,0 +1,5 @@
+from easytypes.easytypes_impl import easy_type_decorator, safe_type, fast_type, unlimited_type
+from easytypes.easytypes_impl import EasyTypesException
+from easytypes.easytypes_impl import AttributeRequired, AttributeNotAllowed, AttributeTypeError
+from easytypes.easytypes_impl import Required
+from easytypes.easytypes_impl import disable_all_checks, enable_all_checks
