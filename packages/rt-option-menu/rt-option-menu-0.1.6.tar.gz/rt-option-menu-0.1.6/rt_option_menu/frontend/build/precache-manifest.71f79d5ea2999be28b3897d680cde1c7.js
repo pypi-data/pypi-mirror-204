b8d06bbac9bb018fbbd37506dabe550eb5b533d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:00 +0000

    Add package rt-option-menu/0.1.6/rt-option-menu-0.1.6.tar.gz

---
+++ packages/rt-option-menu/rt-option-menu-0.1.6.tar.gz/rt-option-menu-0.1.6/rt_option_menu/frontend/build/precache-manifest.71f79d5ea2999be28b3897d680cde1c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1effaa80610e223e2241129858d2be09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "730ae406e0f1224e9f4e",
+    "url": "./static/js/2.6ff9f471.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.6ff9f471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b631fd210f738913ec",
+    "url": "./static/js/main.ec56508e.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
