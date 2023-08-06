3f8f385da43347296d69b86aa9caf0474803a9dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:12 +0000

    Add package PyQt6-Fluent-Widgets/0.7.2/PyQt6_Fluent_Widgets-0.7.2-py3-none-any.whl

---
+++ packages/PyQt6-Fluent-Widgets/PyQt6_Fluent_Widgets-0.7.2-py3-none-any.whl/qfluentwidgets/components/widgets/icon_widget.py
@@ -0,0 +1,25 @@
+# coding:utf-8
+from typing import Union
+
+from PyQt6.QtGui import QIcon, QPainter
+from PyQt6.QtWidgets import QWidget
+
+from ...common.icon import FluentIconBase, drawIcon
+
+
+class IconWidget(QWidget):
+    """ Icon widget """
+
+    def __init__(self, icon: Union[str, QIcon, FluentIconBase], parent=None):
+        super().__init__(parent=parent)
+        self.icon = icon
+
+    def setIcon(self, icon: Union[str, QIcon, FluentIconBase]):
+        self.icon = icon
+        self.update()
+
+    def paintEvent(self, e):
+        painter = QPainter(self)
+        painter.setRenderHints(QPainter.RenderHint.Antialiasing |
+                               QPainter.RenderHint.SmoothPixmapTransform)
+        drawIcon(self.icon, painter, self.rect())
