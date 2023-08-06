7a307e9b6fa5dd2ef03069cd694ff2fd6c69390b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:52 +0000

    Add package varsdump/1.0.1/varsdump-1.0.1.tar.gz

---
+++ packages/varsdump/varsdump-1.0.1.tar.gz/varsdump-1.0.1/README.md
@@ -0,0 +1,40 @@
+# vardump
+Colored Python vardump for CLI
+
+a small wrapper function to output the colored variable prints to the terminal. The peculiarity of the library is that, like in PHP, the var_dump function is in the global scope. You just need to write import vardump in one startup file and then the var_dump function will be available in all modules that you connect to this script.
+
+## Installation
+```python
+pip install vardump
+```
+
+##Enable
+```python
+# app.py
+import vardump
+import some_controller
+
+...
+var_dump(...)
+...
+```
+
+##Usage
+```python
+# some_controller.py
+
+some_obj = {
+  'int': 123,
+  'str': 'abc',
+  'dict': {
+    'list': [1,2,3]
+  }
+}
+
+var_dump(some_obj)
+```
+
+## For update use:
+```
+pip install vardump --upgrade
+```
---
+++ packages/varsdump/varsdump-1.0.1.tar.gz/varsdump-1.0.1/setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+import re
+import sys
+import tomllib
+import pathlib
+from setuptools import find_packages, setup
+
+WORK_DIR = pathlib.Path(__file__).parent
+
+# Check python version
+MINIMAL_PY_VERSION = (3, 10, 8)
+if sys.version_info < MINIMAL_PY_VERSION:
+    raise RuntimeError('aiogram works only with Python {}+'.format('.'.join(map(str, MINIMAL_PY_VERSION))))
+
+
+def get_description() -> str:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
+def get_meta_info() -> dict:
+    try:
+        with open("pyproject.toml", "rb") as f:
+            return tomllib.load(f)
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+project_meta = get_meta_info()
+project_dir = project_meta["project"]["name"].replace("-", "_")
+
+
+setup(
+    license="MIT",
+    name=project_meta["project"]["name"],
+    version=project_meta["project"]["version"],
+    author=project_meta["project"]["authors"][0]["name"],
+    author_email=project_meta["project"]["authors"][0]["email"],
+    description=project_meta["project"]["description"],
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/frontdevops/vardump',
+    download_url=("https://github.com/frontdevops/vardump/archive/refs/tags/"
+                  f'{project_meta["project"]["version"]}.tar.gz'),
+    project_urls={
+        "Documentation": "https://github.com/frontdevops/vardump/blob/main/README.md",
+        "Source": "https://github.com/frontdevops/vardump",
+        "Bug Tracker": "https://github.com/frontdevops/vardump/issues",
+    },
+    keywords=["pypi", "config", "library", "varsdump", "var", "dump", "vardump", "vardump"],
+    classifiers=[
+        # 'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10.8',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development :: Libraries :: Application Utilities',
+    ],
+    package_dir={"": "varsdump"},
+    packages=find_packages(where="varsdump", exclude=('tests', 'tests.*', 'examples.*', 'docs',)),
+    include_package_data=False,
+    python_requires='>=3.10.8',
+    install_requires=[
+        "python-dotenv>=0.21.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+)
