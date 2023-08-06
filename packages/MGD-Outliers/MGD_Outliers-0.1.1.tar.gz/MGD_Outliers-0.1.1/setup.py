ff9e8d73677df10bc6892bec81e282d2eb4f18ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:54 +0000

    Add package MGD-Outliers/0.1.1/MGD_Outliers-0.1.1.tar.gz

---
+++ packages/MGD-Outliers/MGD_Outliers-0.1.1.tar.gz/MGD_Outliers-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'MGD_Outliers is a Python package for identifying and visualizing outliers in a DataFrame.'
+
+
+# Setting up
+setup(
+    name="MGD_Outliers",
+    version=VERSION,
+    author="Mayur Dushetwar",
+    author_email="<mdushetwar@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    license='Apache License 2.0',
+    install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'jinja2'],
+    keywords=['python', 'dataframe', 'outlier', 'outliers', 'inter quartile range', 'plot outliers', 'count outliers', 'data processing', 'drop outliers'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    python_requires= '>= 3.9.7',
+    py_modules=['MGD_Outliers'],
+    package_dir={'':'utils'}
+
+)
