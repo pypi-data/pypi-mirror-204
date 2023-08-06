f98ad0e37cc8c70e453fa57298a561946a2bb6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:49 +0000

    Add package lsptrain/0.0.3/lsptrain-0.0.3.tar.gz

---
+++ packages/lsptrain/lsptrain-0.0.3.tar.gz/lsptrain-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst', 'r', encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(name='lsptrain',
+      version='0.0.3',
+      description='nlp for anyone',
+      long_description=long_description,
+      author='ykallan',
+      author_email='815583442@qq.com',
+      url='http://www.gitee.com/ykallan',
+      install_requires=['torch>=1.10',
+                        'transformers>=4.20.1',
+                        'scikit-learn>=1.1.0',
+                        'tqdm'],
+      python_requires='>=3.7',
+      license='BSD License',
+      packages=find_packages(),
+      platforms=['all'],
+      include_package_data=True
+      )
