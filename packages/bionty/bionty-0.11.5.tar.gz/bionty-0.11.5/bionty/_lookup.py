e8e4ad708ca797beb120e1747810ca3e3c74ef46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:49 +0000

    Add package bionty/0.11.5/bionty-0.11.5.tar.gz

---
+++ packages/bionty/bionty-0.11.5.tar.gz/bionty-0.11.5/bionty/_lookup.py
@@ -0,0 +1,17 @@
+from collections import namedtuple
+from typing import Iterable
+
+from ._normalize import GENE_COLUMNS, PROTEIN_COLUMNS
+
+
+def _lookup(values: Iterable[str]):
+    """Look up a list of values via tab completion."""
+    nt = namedtuple("feature", values)  # type: ignore
+    return nt(**{i: i for i in values})
+
+
+class lookup:
+    """Look up a list of values via tab completion."""
+
+    gene_id = _lookup(values=set(GENE_COLUMNS.values()))
+    protein_id = _lookup(values=set(PROTEIN_COLUMNS.values()))
