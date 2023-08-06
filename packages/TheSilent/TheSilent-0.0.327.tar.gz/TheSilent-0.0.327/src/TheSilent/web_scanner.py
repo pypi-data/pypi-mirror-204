399bdae9e2de930d705f7e74ad2ffad98287cef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:57 +0000

    Add package TheSilent/0.0.327/TheSilent-0.0.327.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.327.tar.gz/TheSilent-0.0.327/src/TheSilent/web_scanner.py
@@ -0,0 +1,36 @@
+from TheSilent.clear import clear
+from TheSilent.html_lint import html_lint
+from TheSilent.sql_injection_scanner import sql_injection_scanner
+from TheSilent.xss_scanner import xss_scanner
+
+CYAN = "\033[1;36m"
+
+# scans for security flaws and bad practices
+def web_scanner(url, secure=True, tor=False, my_file=" ", crawl="0", parse=" ", delay=1, report=True, problem="all"):
+    clear()
+
+    my_sql_injection_scanner = sql_injection_scanner(url=url, secure=secure, tor=tor, my_file=my_file, crawl=crawl, parse=parse, delay=delay)
+    my_xss_scanner = xss_scanner(url=url, secure=secure, tor=tor, my_file=my_file, crawl=crawl, parse=parse, delay=delay)
+
+    clear()
+
+    print(CYAN + "sql injection:")
+
+    for i in my_sql_injection_scanner:
+        if report:
+            with open(url +  ".txt", "a") as f:
+                f.write(i + "\n")
+
+        print(CYAN + i)
+
+    print("")
+    print(CYAN + "xss:")
+
+    for i in my_xss_scanner:
+        if report:
+            with open(url +  ".txt", "a") as f:
+                f.write(i + "\n")
+                
+        print(CYAN + i)
+
+    html_lint(url, secure=secure, crawl=crawl, delay=delay, tor=tor, parse=parse, problem=problem, report=report)
