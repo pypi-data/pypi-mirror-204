398126bcde83e49e25d407e04163a49bdf7d1c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:18 +0000

    Add package pykrakenfiles/0.0.2/pykrakenfiles-0.0.2.tar.gz

---
+++ packages/pykrakenfiles/pykrakenfiles-0.0.2.tar.gz/pykrakenfiles-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pykrakenfiles",
+    version="0.0.2",
+    author="bontoutou",
+    description="Krakenfiles upload API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['krakenfiles', 'upload'],
+    url="https://github.com/bontoutou00/pykrakenfiles",
+    project_urls={
+        "Bug Tracker": "https://github.com/bontoutou00/pykrakenfiles/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=['requests', 'tqdm', 'requests_toolbelt', 'pyyAML'],
+    entry_points={
+        "console_scripts":
+            ["pykrakenfiles = pykrakenfiles.cli:main",
+             "pykrakenfiles-list = pykrakenfiles.cli:list"]
+    },
+)
