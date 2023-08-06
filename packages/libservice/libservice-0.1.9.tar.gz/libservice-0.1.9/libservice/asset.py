7cea562a37bf2c6ea56f0ed6af266bf01488b2c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:03 +0000

    Add package libservice/0.1.9/libservice-0.1.9.tar.gz

---
+++ packages/libservice/libservice-0.1.9.tar.gz/libservice-0.1.9/libservice/asset.py
@@ -0,0 +1,36 @@
+import base64
+from typing import Optional
+from cryptography.fernet import Fernet
+from .ticonn import ticonn
+
+
+class Asset:
+    __slots__ = ('container_id', 'asset_id', 'check_id', 'config', 'key')
+
+    container_id: int
+    asset_id: int
+    check_id: int
+    config: dict
+    key: Optional[str]
+
+    def __init__(self, container_id: int, asset_id: int, check_id: int,
+                 config: dict):
+        self.container_id = container_id
+        self.asset_id = asset_id
+        self.check_id = check_id
+        self.config = config
+        self.key = None
+
+    def __repr__(self) -> str:
+        return f"asset: {self.asset_id} check: {self.check_id}"
+
+    def get_interval(self) -> int:
+        return self.config['_interval']
+
+    async def decrypt(self, secret: str):
+        if self.key is None:
+            self.key = await ticonn.run(
+                'get_encryption_key',
+                self.container_id)
+
+        return Fernet(self.key).decrypt(base64.b64decode(secret)).decode()
