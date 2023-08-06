e83e70b2afd1d0829d2fe6a0f5e26fbe8b1227a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:20 +0000

    Add package visokio-omniprint/1.0.1/visokio_omniprint-1.0.1.tar.gz

---
+++ packages/visokio-omniprint/visokio_omniprint-1.0.1.tar.gz/visokio_omniprint-1.0.1/visokio_omniprint/Image.py
@@ -0,0 +1,20 @@
+import os
+from .DriverBase import DriverBase
+import tinify
+from fpdf import FPDF
+
+class Image(DriverBase):
+
+    def grab_screenshot_in_path(self, url, timeout, is_docker, path):
+        driver = self.get_driver_for_url(url, timeout, is_docker)
+        driver.save_screenshot(path)
+        # Navigate away to ensure page is unloaded properly:
+        driver.get("about:blank") # FIXME: deploy this change to pypi
+
+
+    def tinify(self, key, path, width):
+        tinify.key = key
+        img = tinify.from_file(path)
+        resized = img.resize(method="scale", width=width)
+        os.remove(path)
+        resized.to_file(path)
