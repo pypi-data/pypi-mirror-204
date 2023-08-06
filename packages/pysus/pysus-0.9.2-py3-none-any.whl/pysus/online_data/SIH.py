b08e3f11ebbb840b94e4e4149f764a91ce0492ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:15 +0000

    Add package pysus/0.9.2/pysus-0.9.2-py3-none-any.whl

---
+++ packages/pysus/pysus-0.9.2-py3-none-any.whl/pysus/online_data/SIH.py
@@ -0,0 +1,32 @@
+"""
+Downloads SIH data from Datasus FTP server
+Created on 21/09/18
+by fccoelho
+license: GPL V3 or Later
+"""
+from typing import Union
+
+from pysus.online_data import CACHEPATH, FTP_Downloader
+
+
+def download(
+    states: Union[str, list],
+    years: Union[str, list, int],
+    months: Union[str, list, int],
+    data_dir: str = CACHEPATH,
+) -> list:
+    """
+    Download SIH records for state year and month and returns dataframe
+    :param months: 1 to 12, can be a list
+    :param states: 2 letter state code, can be alist
+    :param years: 4 digit integer, can be a list
+    :param data_dir: Directory where parquets will be downloaded.
+    :return: a list of parquet paths
+    """
+    return FTP_Downloader('SIH').download(
+        UFs=states,
+        years=years,
+        months=months,
+        SIH_group='RD',
+        local_dir=data_dir,
+    )
