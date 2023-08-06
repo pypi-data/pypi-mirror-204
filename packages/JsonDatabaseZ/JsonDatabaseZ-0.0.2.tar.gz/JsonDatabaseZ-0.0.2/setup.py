29ed9c32afb1f8ee43a2f3e127f105cabb68bc61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:23 +0000

    Add package JsonDatabaseZ/0.0.2/JsonDatabaseZ-0.0.2.tar.gz

---
+++ packages/JsonDatabaseZ/JsonDatabaseZ-0.0.2.tar.gz/JsonDatabaseZ-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import find_packages, setup
+
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.0.2'
+PAKAGE_NAME = 'JsonDatabaseZ'
+AUTHOR = 'Rey Michel Del Toro Martinez'
+AUTHOR_EMAIL = 'martinez200921@gmail.com'
+URL = 'https://github.com/ReyDev2009'
+
+LICENSE = 'MIT'
+LONG_DESCRIPTION = (HERE/"README.md").read_text(encoding='utf-8')
+LONG_DESC_TYPE = "text/markdown"
+
+setup(
+    name=PAKAGE_NAME,
+    version=VERSION,
+    long_description=LONG_DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    include_package_data=True,
+    long_description_content_type=LONG_DESC_TYPE,
+    license=LICENSE,
+    packages=find_packages()
+)
