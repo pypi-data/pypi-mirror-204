50b45b8d9a9c7bcb26908df10a468d0939f9a5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:56 +0000

    Add package fx-py-sdk/0.2.0/fx-py-sdk-0.2.0.tar.gz

---
+++ packages/fx-py-sdk/fx-py-sdk-0.2.0.tar.gz/fx-py-sdk-0.2.0/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+with open("README.md", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fx-py-sdk",
+    version="0.2.0",
+    platforms='any',
+    description="Python library for fxCore",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='zakir',
+    packages=find_packages(),
+    url='https://github.com/functionx/fx-py-sdk',
+    keywords=["fxCore", "blockchain"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    include_package_data=True
+)
