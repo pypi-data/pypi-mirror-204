2e55007dc0d0376dd1ad59c7dc3ec88a8f4d26cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:37 +0000

    Add package python-can-canine/0.2.5/python-can-canine-0.2.5.tar.gz

---
+++ packages/python-can-canine/python-can-canine-0.2.5.tar.gz/python-can-canine-0.2.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+setup.py
+
+python-can-canine
+"""
+
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="python-can-canine",
+    version="0.2.5",
+    author="Yannis Chatzikonstantinou",
+    author_email="info@tinymovr.com",
+    description="Python-can CANine",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yconst/python-can-canine",
+    py_modules = ["canine"],
+    python_requires=">=3.9",
+    install_requires=[
+        "python-can",
+        "pyusb~=1.0"
+    ],
+    entry_points = {
+        'can.interface': [
+            'canine = canine:CANineBus'
+        ]
+    }
+)
