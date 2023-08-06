833abd19760749ceb39ee88d8fb64830caff1675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:08 +0000

    Add package biobear/0.1.5/biobear-0.1.5.tar.gz

---
+++ packages/biobear/biobear-0.1.5.tar.gz/biobear-0.1.5/python/tests/test_gff_reader.py
@@ -0,0 +1,17 @@
+# Test the fasta reader can be converted to a polars dataframe
+
+from pathlib import Path
+
+from biobear import GFFReader
+
+DATA = Path(__file__).parent / "data"
+
+
+def test_gff_reader():
+
+    reader = GFFReader(DATA / "test.gff")
+    df = reader.to_polars()
+
+    print(df)
+
+    assert len(df) == 2
