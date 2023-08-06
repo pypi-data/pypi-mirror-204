a288c7619c39b964a3b8f9c372420575356523e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:51 +0000

    Add package intecomm-edc/0.1.20/intecomm-edc-0.1.20.tar.gz

---
+++ packages/intecomm-edc/intecomm-edc-0.1.20.tar.gz/intecomm-edc-0.1.20/intecomm_dashboard/views/subject/dashboard/dashboard_view.py
@@ -0,0 +1,13 @@
+from edc_subject_dashboard.views import SubjectDashboardView
+
+
+class DashboardView(SubjectDashboardView):
+    consent_model = "intecomm_consent.subjectconsent"
+    navbar_selected_item = "consented_subject"
+    visit_model = "intecomm_subject.subjectvisit"
+    history_button_label = "Audit"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(subject_listboard_url="screen_group_url")
+        return context
