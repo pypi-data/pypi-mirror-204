91e0c7d35a3595c51a4a2207e602b9b70d032a09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:29 +0000

    Add package odoo12-addon-metadata/12.0.0.1.0/odoo12_addon_metadata-12.0.0.1.0-py3-none-any.whl

---
+++ packages/odoo12-addon-metadata/odoo12_addon_metadata-12.0.0.1.0-py3-none-any.whl/odoo/addons/metadata/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "metadata",
+
+    'summary': """
+        Save custom metadata on any model""",
+
+    'author': "Coopdevs Treball SCCL",
+    'website': "",
+
+    'category': 'server',
+    'version': '12.0.0.1.0',
+    "license": "AGPL-3",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [],
+}
