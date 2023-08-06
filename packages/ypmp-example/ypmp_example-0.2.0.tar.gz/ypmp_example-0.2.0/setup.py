61e766dc62234bc6df4047acb2b7be1c3dbacdb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:06 +0000

    Add package ypmp-example/0.2.0/ypmp_example-0.2.0.tar.gz

---
+++ packages/ypmp-example/ypmp_example-0.2.0.tar.gz/ypmp_example-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ypmp_example']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ypmp-example',
+    'version': '0.2.0',
+    'description': 'Example package for YP 24',
+    'long_description': 'None',
+    'author': 'ovsds',
+    'author_email': 'ovsds@yandex-team.ru',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
