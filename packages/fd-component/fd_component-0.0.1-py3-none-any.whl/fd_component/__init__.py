9b916c9a91440c4c84b1085274645834df5b5d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:46 +0000

    Add package fd-component/0.0.1/fd_component-0.0.1-py3-none-any.whl

---
+++ packages/fd-component/fd_component-0.0.1-py3-none-any.whl/fd_component/__init__.py
@@ -0,0 +1,32 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        # We give the component a simple, descriptive name ("my_component"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "fd_component",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3001",
+    )
+else:
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("fd_component", path=build_dir)
+
+def fd_component(question):
+
+    _ = _component_func(question=question)
+
+if not _RELEASE:
+    import streamlit as st
+    
+    fd_component("Question #1")
