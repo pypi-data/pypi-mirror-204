32f5fe4c79f24465232bce4f7dd55f56e84f4aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:53 +0000

    Add package bizlogic/0.0.2/bizlogic-0.0.2.tar.gz

---
+++ packages/bizlogic/bizlogic-0.0.2.tar.gz/bizlogic-0.0.2/bizlogic/loan/reader.py
@@ -0,0 +1,78 @@
+from ipfsclient import Ipfs
+from typing import Self
+from bizlogic.loan import PREFIX
+from bizlogic.loan.status import LoanStatus, LoanStatusType
+from ipfskvs import Index, Store
+
+from protoc.loan_pb2 import Loan
+
+class LoanReader():
+    ipfsclient: Ipfs
+
+    def __init__(self: Self, ipfsclient: Ipfs):
+        self.ipfsclient = ipfsclient
+ 
+    def get_open_loan_offers(self: Self, borrower: str):
+        return self.query_for_status(
+            status=LoanStatus.PENDING_ACCEPTANCE,
+            index=Index(
+                prefix=PREFIX,
+                index={
+                    "borrower": borrower
+                }
+            ),
+        )
+
+    def query_for_status(self: Self, status: LoanStatusType, index: dict = {}):
+        # get all applications from ipfs
+        loans = Store.query(
+            query_index=Index(
+                prefix=PREFIX,
+                index=index
+            ),
+            ipfs=self.ipfsclient,
+            reader=Loan()
+        )
+
+        # filter for unexpired and unaccepted loans
+        return [
+            loan
+            for loan in loans
+            if LoanStatus.loan_status(loan.reader) == status
+        ]
+
+    def query_for_borrower(self: Self, borrower: str):
+        return Store.query(
+            query_index=Index(
+                prefix=PREFIX,
+                index={
+                    "borrower": borrower
+                }
+            ),
+            ipfs=self.ipfsclient,
+            reader=Loan()
+        )
+
+    def query_for_lender(self: Self, lender: str):
+        return Store.query(
+            query_index=Index(
+                prefix=PREFIX,
+                index={
+                    "lender": lender
+                }
+            ),
+            ipfs=self.ipfsclient,
+            reader=Loan()
+        )
+
+    def query_for_loan(self: Self, loan_id: str):
+        return Store.query(
+            query_index=Index(
+                prefix=PREFIX,
+                index={
+                    "loan": loan_id
+                }
+            ),
+            ipfs=self.ipfsclient,
+            reader=Loan()
+        )
