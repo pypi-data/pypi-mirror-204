e5410a33ea22b0ee4d52da81bb9d39c2b148c745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package aiogram-ext/0.1.5/aiogram-ext-0.1.5.tar.gz

---
+++ packages/aiogram-ext/aiogram-ext-0.1.5.tar.gz/aiogram-ext-0.1.5/botty/helpers.py
@@ -0,0 +1,39 @@
+from typing import Awaitable
+
+from aiogram.types import ChatType
+
+from .deps import ReplyKeyboardRemove, Message, ReplyMarkup, Query, Chat, State
+
+
+def reply(
+        msg: Message | Query,
+        text: str,
+        markup: ReplyMarkup | bool = None,
+) -> Awaitable[Message]:
+    if isinstance(msg, Query):
+        msg = msg.message
+    if markup is False:
+        markup = ReplyKeyboardRemove()
+    return msg.answer(text, reply_markup=markup)
+
+
+async def ask(
+        state: State,
+        msg: Message,
+        text: str,
+        markup: ReplyMarkup = None,
+):
+    await state.set()
+    await reply(msg, text, markup)
+
+
+def is_channel(chat: Chat) -> bool:
+    return chat.type in [ChatType.CHANNEL]
+
+
+def is_group(chat: Chat) -> bool:
+    return chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]
+
+
+def is_private(chat: Chat) -> bool:
+    return chat.type in [ChatType.PRIVATE]
