5521491d55d8039953b00056994aed6378c4e623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:17 +0000

    Add package gradio-tools/0.0.5/gradio_tools-0.0.5-py3-none-any.whl

---
+++ packages/gradio-tools/gradio_tools-0.0.5-py3-none-any.whl/gradio_tools/__init__.py
@@ -0,0 +1,18 @@
+from gradio_tools.tools import (ClipInterrogatorTool,
+                                DocQueryDocumentAnsweringTool, GradioTool,
+                                ImageCaptioningTool, ImageToMusicTool,
+                                StableDiffusionPromptGeneratorTool,
+                                StableDiffusionTool, TextToVideoTool,
+                                WhisperAudioTranscriptionTool)
+
+__all__ = [
+    "GradioTool",
+    "StableDiffusionTool",
+    "ClipInterrogatorTool",
+    "ImageCaptioningTool",
+    "ImageToMusicTool",
+    "WhisperAudioTranscriptionTool",
+    "StableDiffusionPromptGeneratorTool",
+    "TextToVideoTool",
+    "DocQueryDocumentAnsweringTool",
+]
