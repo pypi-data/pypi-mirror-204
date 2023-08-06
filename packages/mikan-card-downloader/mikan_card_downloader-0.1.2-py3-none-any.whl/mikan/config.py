4e29f944feb83affa18eb61bd255ddab9a0b54a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:56 +0000

    Add package mikan-card-downloader/0.1.2/mikan_card_downloader-0.1.2-py3-none-any.whl

---
+++ packages/mikan-card-downloader/mikan_card_downloader-0.1.2-py3-none-any.whl/mikan/config.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2022-2023 DemonicSavage
+# This file is part of Mikan.
+
+# Mikan is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+
+# Mikan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+
+import configparser
+import shutil
+from pathlib import Path
+import os
+
+import platformdirs
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+cfg = configparser.ConfigParser()
+
+cfg_dir = Path(platformdirs.user_config_dir("mikan", ensure_exists=True))
+cfg_file = cfg_dir / "config.cfg"
+
+if not cfg_file.exists():
+    shutil.copy(Path(ROOT_DIR) / "default_config.cfg", cfg_file)
+
+cfg.read(cfg_dir / "config.cfg")
+
+CARDS_DIR = Path(cfg["Paths"]["data_dir"])
