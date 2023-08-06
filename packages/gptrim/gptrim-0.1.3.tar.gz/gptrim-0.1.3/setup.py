5887096e7277dd2234f290abdd9c9f2c2c925d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:23 +0000

    Add package gptrim/0.1.3/gptrim-0.1.3.tar.gz

---
+++ packages/gptrim/gptrim-0.1.3.tar.gz/gptrim-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='gptrim',
+    version='0.1.3',
+    description='Reduce the size of GPT inputs by 40-60% without losing most of the information.',
+    long_description=long_description,
+    url='https://github.com/vlad-ds/gptrim',
+    author='Vlad Gheorghe',
+    author_email='vlad.datapro@gmail.com',
+    packages=['gptrim'],
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
