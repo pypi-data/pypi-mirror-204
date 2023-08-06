53ce2d36a258835c3a8e276fef0fd67b725b321d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:21 +0000

    Add package DecisionTreeClassifier/0.0.6/DecisionTreeClassifier-0.0.6.tar.gz

---
+++ packages/DecisionTreeClassifier/DecisionTreeClassifier-0.0.6.tar.gz/DecisionTreeClassifier-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+with open("DecisionTreeClassifier/README.txt", "r") as f:
+    long_description = f.read()
+ 
+setup(
+  name='DecisionTreeClassifier',
+  version='0.0.6',
+  description='A Decision Tree Classifier.',
+  long_description=long_description,
+  package_dir={"": "DecisionTreeClassifier"},
+  packages=find_packages(where="DecisionTreeClassifier"),
+  url='https://github.com/mlouii/Decision-Tree-Practicum',  
+  author='Mark Lou, Jobin Joyson',
+  author_email='mlou@hawk.iit.edu, jjoyson1@hawk.iit.edu',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='decision tree', 
+  install_requires=['numpy', 'pandas'] 
+)
