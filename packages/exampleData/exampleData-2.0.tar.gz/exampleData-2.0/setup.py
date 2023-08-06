a79371492136d229c529c50f4c580c103aad92c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package exampleData/2.0/exampleData-2.0.tar.gz

---
+++ packages/exampleData/exampleData-2.0.tar.gz/exampleData-2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="exampleData",
+    version="2.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "requests"
+    ],
+    author="NCQH",
+    author_email="ncquochuy21@gmail.com",
+    description="Package for example data for numerical study",
+    long_description_content_type="text/markdown",
+    url="https://github.com/NCQH/exampleData",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
