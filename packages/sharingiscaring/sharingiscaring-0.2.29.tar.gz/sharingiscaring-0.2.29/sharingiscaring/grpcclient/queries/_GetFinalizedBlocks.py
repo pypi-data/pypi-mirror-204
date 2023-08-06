4cd58b4a2d3d02798743534ef92259a72caf1271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:49 +0000

    Add package sharingiscaring/0.2.29/sharingiscaring-0.2.29.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.2.29.tar.gz/sharingiscaring-0.2.29/sharingiscaring/grpcclient/queries/_GetFinalizedBlocks.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from sharingiscaring.GRPCClient.types_pb2 import *
+from sharingiscaring.GRPCClient.service_pb2_grpc import QueriesStub
+from sharingiscaring.GRPCClient.queries._SharedConverters import (
+    Mixin as _SharedConverters,
+)
+from typing import Iterator
+from typing import TYPE_CHECKING
+import sys
+
+if TYPE_CHECKING:
+    from sharingiscaring.GRPCClient import GRPCClient
+from sharingiscaring.GRPCClient.CCD_Types import *
+from rich import print
+
+
+class Mixin(_SharedConverters):
+    def convertFinalizedBlock(self, block) -> CCD_FinalizedBlockInfo:
+        result = {}
+
+        for descriptor in block.DESCRIPTOR.fields:
+            key, value = self.get_key_value_from_descriptor(descriptor, block)
+
+            if type(value) in self.simple_types:
+                result[key] = self.convertType(value)
+
+        return CCD_FinalizedBlockInfo(**result)
+
+    def get_finalized_blocks(self: GRPCClient) -> CCD_FinalizedBlockInfo:
+
+        self.check_connection(sys._getframe().f_code.co_name)
+        for block in self.stub.GetFinalizedBlocks(request=Empty()):
+            print(self.convertFinalizedBlock(block))
