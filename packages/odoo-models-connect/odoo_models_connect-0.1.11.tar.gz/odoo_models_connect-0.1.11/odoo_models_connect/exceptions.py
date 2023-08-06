d9be47d5646eda249eaf732077eb3781f0be96ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:31 +0000

    Add package odoo-models-connect/0.1.11/odoo_models_connect-0.1.11.tar.gz

---
+++ packages/odoo-models-connect/odoo_models_connect-0.1.11.tar.gz/odoo_models_connect-0.1.11/odoo_models_connect/exceptions.py
@@ -0,0 +1,16 @@
+class ObjectDoesNotExist(Exception):
+
+    def __init__(self, obj_id):
+        self.id = obj_id
+
+    def __str__(self):
+        return f'Element by id {self.id} does not exist'
+
+class IDRequiredException(ValueError):
+    def __str__(self):
+        return 'id is required'
+
+
+class EnvVariablesException(ValueError):
+    def __str__(self):
+            return 'required environment variables'
