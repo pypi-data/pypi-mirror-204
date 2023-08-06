cb076b1ff78ef6eb5bacefc842eb95d58f7d94d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:11 +0000

    Add package chebyfit/2023.4.22/chebyfit-2023.4.22.tar.gz

---
+++ packages/chebyfit/chebyfit-2023.4.22.tar.gz/chebyfit-2023.4.22/setup.py
@@ -0,0 +1,102 @@
+# chebyfit/setup.py
+
+"""Chebyfit package Setuptools script."""
+
+import sys
+import re
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('chebyfit/chebyfit.py') as fh:
+    code = fh.read()
+
+version = search(r"__version__ = '(.*?)'", code)
+
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
+
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+license = search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+
+license = license.replace('# ', '').replace('#', '')
+
+if 'sdist' in sys.argv:
+    with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+    with open('README.rst', 'w') as fh:
+        fh.write(readme)
+
+
+class build_ext(_build_ext):
+    """Delay import numpy until build."""
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        if isinstance(__builtins__, dict):
+            __builtins__['__NUMPY_SETUP__'] = False
+        else:
+            setattr(__builtins__, '__NUMPY_SETUP__', False)
+        import numpy
+
+        self.include_dirs.append(numpy.get_include())
+
+
+setup(
+    name='chebyfit',
+    version=version,
+    license='BSD',
+    description=description,
+    long_description=readme,
+    author='Christoph Gohlke',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/chebyfit/issues',
+        'Source Code': 'https://github.com/cgohlke/chebyfit',
+        # 'Documentation': 'https://',
+    },
+    python_requires='>=3.9',
+    install_requires=['numpy'],
+    setup_requires=['setuptools', 'numpy'],
+    cmdclass={'build_ext': build_ext},
+    packages=['chebyfit'],
+    ext_modules=[Extension('chebyfit._chebyfit', ['chebyfit/chebyfit.c'])],
+    package_data={'chebyfit': ['examples/*.py']},
+    zip_safe=False,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
