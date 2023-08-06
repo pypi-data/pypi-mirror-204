0c2fcc6e4de852f80afcb8d1ae889832f45618d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:15 +0000

    Add package chatllm/2023.4.25.9.19.45/ChatLLM-2023.4.25.9.19.45.tar.gz

---
+++ packages/chatllm/ChatLLM-2023.4.25.9.19.45.tar.gz/ChatLLM-2023.4.25.9.19.45/chatllm/applications/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2023/4/21 11:50
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from chatllm.applications.chat import Chat
+from chatllm.applications.ann4qa import ANN4QA
