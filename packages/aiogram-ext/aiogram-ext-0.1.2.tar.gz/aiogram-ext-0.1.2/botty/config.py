bb262ad8bc36cebbc1068c123fc32de99b3b0844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package aiogram-ext/0.1.2/aiogram-ext-0.1.2.tar.gz

---
+++ packages/aiogram-ext/aiogram-ext-0.1.2.tar.gz/aiogram-ext-0.1.2/botty/config.py
@@ -0,0 +1,12 @@
+import env
+
+BOT_TOKEN = env.get("BOT_TOKEN")
+APP_PORT = env.get_int("APP_PORT", 80)
+
+
+class MONGO:
+    _ = "MONGO_"
+    DB = env.get(_ + "DB")
+    HOST = env.get(_ + "HOST")
+    USER = env.get(_ + "USER", "root")
+    PASSWORD = env.get(_ + "PASSWORD")
