17783ed114914f7e123cf78762e69d28f467a9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package peak-mas/1.0.5/peak-mas-1.0.5.tar.gz

---
+++ packages/peak-mas/peak-mas-1.0.5.tar.gz/peak-mas-1.0.5/src/peak/__init__.py
@@ -0,0 +1,26 @@
+"""PEAK - Python-based framework for heterogeneous agent communities.
+
+PEAK is a framework for developing communities of heterogeneous agents. 
+This communities are multi-agent systems that can coexist and exchange 
+resources and information with each other easly. 
+
+isort: skip_file
+"""
+
+# Standard library imports
+import logging
+
+# Third party imports
+from spade.message import Message
+from spade.template import Template
+
+# Reader imports
+from peak.core import *
+from peak.agents import *
+from peak.behaviours import *
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Bruno Ribeiro"
+__email__ = "brgri@isep.ipp.pt"
+__version__ = "1.0.5"
