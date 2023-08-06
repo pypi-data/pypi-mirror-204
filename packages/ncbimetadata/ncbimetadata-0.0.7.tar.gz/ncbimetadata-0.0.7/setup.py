c15d475be0099155d4816a80669d05ac288a175e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:07 +0000

    Add package ncbimetadata/0.0.7/ncbimetadata-0.0.7.tar.gz

---
+++ packages/ncbimetadata/ncbimetadata-0.0.7.tar.gz/ncbimetadata-0.0.7/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readfile(filename):
+    with open(filename, 'r+') as f:
+        return f.read()
+
+
+setup(
+    name="ncbimetadata",
+    version="0.0.7",
+    description="ncbimetadata is a package for fast fetching and parsing metadata from ncbi database, and more functionalities are on the way!",
+    long_description=readfile('README.md'),
+    long_description_content_type='text/markdown',
+    author="Ryan Alloriadonis",
+    author_email="yuantai78@gmail.com",
+    url="https://github.com/RyanYuanSun/ncbimetadata",
+    py_modules=['ncbimetadata'],
+    packages=['ncbimetadata'],
+    license=readfile('LICENSE'),
+    scripts=['bin/ncbimetadata'],
+    install_requires=[
+          'requests',
+      ],
+    zip_safe=False
+)
