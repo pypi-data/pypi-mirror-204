891d3f8a095156ba244c18c55a7ae17a6f7bd22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:56 +0000

    Add package alaric/1.3.2/alaric-1.3.2-py3-none-any.whl

---
+++ packages/alaric/alaric-1.3.2-py3-none-any.whl/alaric/__init__.py
@@ -0,0 +1,24 @@
+import logging
+from collections import namedtuple
+
+from .advanced_query import AQ
+from .document import Document
+from .cursor import Cursor
+from .encrypted_document import EncryptedDocument
+
+Ascending = 1
+Descending = -1
+
+__all__ = (
+    "AQ",
+    "Document",
+    "Ascending",
+    "Descending",
+    "Cursor",
+    "EncryptedDocument",
+    "util",
+)
+__version__ = "1.3.2"
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(major=1, minor=3, micro=2, releaselevel="final", serial=0)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
