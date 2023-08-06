c7fd6f402dab8f6f9d61f47b56da17645b9e8224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package chesscomwrapper/0.0.4/chesscomwrapper-0.0.4.tar.gz

---
+++ packages/chesscomwrapper/chesscomwrapper-0.0.4.tar.gz/chesscomwrapper-0.0.4/app/chesscomwrapper/src/player/playerclub.py
@@ -0,0 +1,16 @@
+# from src.chessplayer import ChessPlayer
+# from src.chessclub import Club
+
+
+
+
+class PlayerClub(object):
+    
+    
+    def __init__(self, clubname, joined) -> None:
+        self.name = clubname
+        self.joined = joined
+
+    
+    
+
