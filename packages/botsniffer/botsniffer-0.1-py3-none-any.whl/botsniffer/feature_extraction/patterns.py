1208ca4bead1c3420f0bb5b72e9034e42cfc727a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:35 +0000

    Add package botsniffer/0.1/botsniffer-0.1-py3-none-any.whl

---
+++ packages/botsniffer/botsniffer-0.1-py3-none-any.whl/botsniffer/feature_extraction/patterns.py
@@ -0,0 +1,43 @@
+import ast
+from collections import defaultdict
+
+
+def detect_repetitive_patterns(parsed_code):
+    pattern_counts = defaultdict(int)
+    # Traverse the AST and count the number of occurrences of each pattern
+    for node in ast.walk(parsed_code):
+        if hasattr(node, "lineno") and hasattr(node, "col_offset"):
+            # Ignore nodes without line and column information (e.g., module)
+            node_pattern = get_node_pattern(node)
+            pattern_counts[node_pattern] += 1
+    repetitive_patterns = [
+        pattern for pattern, count in pattern_counts.items() if count > 1
+    ]
+    total_patterns = len(repetitive_patterns)
+    num_nodes = sum(1 for _ in ast.walk(parsed_code))
+    if num_nodes == 0:
+        return 0.0
+    else:
+        return total_patterns / num_nodes
+
+
+def get_node_pattern(node):
+    if isinstance(node, ast.Call):
+        # For function calls, include the function name and argument types
+        arg_types = [get_node_pattern(arg) for arg in node.args]
+        return f"Call:{get_node_pattern(node.func)}.{','.join(arg_types)}"
+    elif isinstance(node, ast.Assign):
+        # For assignments, include the target names and value type
+        target_names = [get_node_pattern(target) for target in node.targets]
+        value_type = get_node_pattern(node.value)
+        return f"Assign:{','.join(target_names)}={value_type}"
+    elif isinstance(node, ast.Name):
+        # For variable names, return the name
+        return node.id
+    elif isinstance(node, ast.Attribute):
+        # For attributes, recursively get the pattern of the value
+        # and concatenate the attribute name
+        return f"{get_node_pattern(node.value)}.{node.attr}"
+    else:
+        # For other node types, return the class name
+        return node.__class__.__name__
---
+++ packages/botsniffer/botsniffer-0.1-py3-none-any.whl/botsniffer/scanner.py
@@ -0,0 +1,110 @@
+import os
+import ast
+import pickle
+import argparse
+
+from botsniffer.feature_extraction.feature_extraction import extract_features
+from botsniffer.ml_model.model import train_model, predict
+
+
+def get_features(file_path, tree):
+    features = extract_features(file_path, tree)
+    feature_values = []
+    for key, value in features.items():
+        if isinstance(value, str):
+            feature_values.append(float(value))
+        elif isinstance(value, list):
+            feature_values.extend(value)
+        else:
+            feature_values.append(float(value))
+    sample = feature_values
+    # For training, is expected that AI generated
+    # files had "ai" in their name/path
+    if "ai" in file_path.lower():
+        label = True
+    else:
+        label = False
+    return features, sample, label
+
+
+def scan_path(path, identify, train):
+    is_ai = False
+    samples = []
+    labels = []
+    pkl_dir = os.path.join(os.path.dirname(__file__), 'data')
+    pkl_path = os.path.join(
+                        pkl_dir,
+                        'botcode.pkl')
+    if not os.path.exists(path):
+        print("Error: '{}' does not exist.".format(path))
+    elif os.path.isdir(path):
+        # Run the feature extraction in a folder
+        for root, dirs, files in os.walk(path):
+            for file_name in files:
+                if file_name.endswith(".py"):
+                    file_path = os.path.join(root, file_name)
+                    with open(file_path) as f:
+                        tree = ast.parse(f.read(), type_comments=True)
+                        features, sample, label = get_features(file_path, tree)
+                        if identify:
+                            is_ai = predict(pkl_path, features)
+                            print('File:', file_path)
+                            print('AI:', is_ai)
+                            print('Features:', features)
+                        elif train:
+                            samples.append(sample)
+                            if label:
+                                labels.append(1)
+                            else:
+                                labels.append(0)
+    else:
+        if path.endswith(".py"):
+            # Run the feature extraction in a single file
+            with open(path) as f:
+                tree = ast.parse(f.read(), type_comments=True)
+                features, sample, label = get_features(path, tree)
+                if identify:
+                    is_ai = predict(pkl_path, features)
+                    print('File:', path)
+                    print('AI:', is_ai)
+                    print('Features:', features)
+                elif train:
+                    samples.append(sample)
+                    if label:
+                        labels.append(1)
+                    else:
+                        labels.append(0)
+        else:
+            print("Error: '{}' is not a Python source code file.".format(path))
+    if train:
+        train_model(pkl_path, labels, samples)
+        print('Trained:')
+        print(' - labels:', len(labels))
+        print(' - samples:', len(samples))
+    elif identify:
+        print('Done!')
+    else:
+        print("Error: you must specify either \
+        --identify or --train.")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Scan Python source code files \
+        and extract information about functions.")
+    parser.add_argument("path", help="The path to scan.")
+    parser.add_argument(
+        "--identify",
+        action="store_true",
+        help="Identify AI-generated code.")
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Train a model to identify AI-generated code.")
+    args = parser.parse_args()
+
+    scan_path(args.path, args.identify, args.train)
+
+
+if __name__ == "__main__":
+    main()
