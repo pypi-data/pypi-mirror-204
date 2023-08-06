5d9c3b6c36e71a153a7693ada9cbee10e0030428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:33 +0000

    Add package image-frequency-analysis/0.2.0/image_frequency_analysis-0.2.0.tar.gz

---
+++ packages/image-frequency-analysis/image_frequency_analysis-0.2.0.tar.gz/image_frequency_analysis-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="image_frequency_analysis",
+    version="0.2.0",
+    author="M Thivagar",
+    packages=find_packages(),
+    install_requires=["opencv-python", "numpy", "pandas", "matplotlib"],
+    python_requires=">=3.6",
+    homepage="https://github.com/thivagar-manickam/image_frequency_analysis",
+    description="A package to perform image frequency analysis using the Fourier Transform method",
+    bugtracker="https://github.com/thivagar-manickam/image_frequency_analysis/issues",
+    readme="README.md",
+)
