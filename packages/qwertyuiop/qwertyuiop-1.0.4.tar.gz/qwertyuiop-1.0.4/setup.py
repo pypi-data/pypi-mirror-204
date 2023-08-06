2546bcf4c6539f19fb1861ee40b363cb5c7cce15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package qwertyuiop/1.0.4/qwertyuiop-1.0.4.tar.gz

---
+++ packages/qwertyuiop/qwertyuiop-1.0.4.tar.gz/qwertyuiop-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from update import update_version
+
+
+current_version = '1.0.4'
+
+setup(
+    name='qwertyuiop',
+    packages=['qwertyuiop'],
+    version=current_version,
+    license='MIT',
+    description='Machine Learning Models',
+    author='Gleb Maksimov',
+    author_email='glebmaksimov092@gmail.com',
+    download_url=f'https://github.com/Glebmaksimov/maxilaern/archive/refs/tags/{current_version}.tar.gz',
+    keywords=['ML', 'FROM SWCRATCH', 'ADVANCED'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'seaborn',
+        'IPython'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+update_version()
