3da5ed510842d25d02ebfbd7f542fd877bcd6f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:43 +0000

    Add package sqlmesh/0.5.1.dev22/sqlmesh-0.5.1.dev22.tar.gz

---
+++ packages/sqlmesh/sqlmesh-0.5.1.dev22.tar.gz/sqlmesh-0.5.1.dev22/sqlmesh/dbt/seed.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from sqlmesh.core.model import Model, SeedKind, create_seed_model
+from sqlmesh.dbt.basemodel import BaseModelConfig
+from sqlmesh.dbt.common import SqlStr
+from sqlmesh.dbt.context import DbtContext
+
+
+class SeedConfig(BaseModelConfig):
+    """
+    seedConfig contains all config parameters available to DBT seeds
+
+    See https://docs.getdbt.com/reference/configs-and-properties for
+    a more detailed description of each config parameter under the
+    General propreties, General configs, and For seeds sections.
+    """
+
+    @property
+    def all_sql(self) -> SqlStr:
+        return SqlStr(";\n".join(self.pre_hook + self.post_hook))
+
+    def to_sqlmesh(self, context: DbtContext) -> Model:
+        """Converts the dbt seed into a SQLMesh model."""
+        model_context = self._context_for_dependencies(context, self._dependencies)
+
+        return create_seed_model(
+            self.model_name,
+            SeedKind(path=self.path.absolute()),
+            **self.sqlmesh_model_kwargs(
+                model_context,
+            ),
+        )
