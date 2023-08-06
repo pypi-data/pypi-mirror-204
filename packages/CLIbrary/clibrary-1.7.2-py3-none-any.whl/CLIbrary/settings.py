f7382da2c09eb8b0a4421c1e427792adb2728143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:25 +0000

    Add package CLIbrary/1.7.2/clibrary-1.7.2-py3-none-any.whl

---
+++ packages/CLIbrary/clibrary-1.7.2-py3-none-any.whl/CLIbrary/settings.py
@@ -0,0 +1,8 @@
+# Global settings for CLIbrary.
+
+class style:
+	setting_darkMode = False # Enables global dark mode.
+	setting_plainMode = False # Disables styling.
+
+class data:
+	setting_fileExtension = ".pickle" # Defines a file extension for aDump and aLoad. Default value based on Python's module pickle.
