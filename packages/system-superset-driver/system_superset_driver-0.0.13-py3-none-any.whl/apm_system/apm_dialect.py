470f9a7dc9a31523ab87e38289c7120841e52d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package system-superset-driver/0.0.13/system_superset_driver-0.0.13-py3-none-any.whl

---
+++ packages/system-superset-driver/system_superset_driver-0.0.13-py3-none-any.whl/apm_system/apm_dialect.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+from shillelagh.backends.apsw.dialects.base import APSWDialect
+from sqlalchemy.engine import Connection
+from sqlalchemy.engine.url import URL
+
+# -----------------------------------------------------------------------------
+
+ADAPTER_NAME = "system"
+
+
+# -----------------------------------------------------------------------------
+
+
+class SystemDialect(APSWDialect):
+    supports_statement_cache = True
+
+    def __init__(
+            self,
+            **kwargs: Any,
+    ):
+        # We tell Shillelagh that this dialect supports just one adapter
+        super().__init__(safe=True, adapters=[ADAPTER_NAME], **kwargs)
+
+    def get_table_names(
+            self, connection, schema=None, sqlite_include_internal=False, **kw
+    ) -> List[str]:
+        return ["data"]
+
+    def create_connect_args(
+            self,
+            url: URL,
+    ) -> Tuple[Tuple[()], Dict[str, Any]]:
+        args, kwargs = super().create_connect_args(url)
+        return args, {**kwargs, "path": ":memory:", "adapter_kwargs": {}}
