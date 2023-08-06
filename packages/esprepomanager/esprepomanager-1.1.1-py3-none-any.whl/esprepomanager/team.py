13e1fd6a5dab117a577cc7d9f7fbfc5ccc667f1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:02 +0000

    Add package esprepomanager/1.1.1/esprepomanager-1.1.1-py3-none-any.whl

---
+++ packages/esprepomanager/esprepomanager-1.1.1-py3-none-any.whl/esprepomanager/team.py
@@ -0,0 +1,50 @@
+from typing import Dict, List, Optional, TYPE_CHECKING
+from .repo import Repository
+from .utils import save_get
+
+if TYPE_CHECKING:
+    from .person import Person
+
+
+class Team:
+    id: int
+    key: str
+    assignment: str
+    persons: 'List[Person]'
+    repository: Optional[Repository]
+
+    def __init__(
+        self, id_: int, assignment: str, persons: 'Optional[List[Person]]' = None,
+        repository: Optional[Repository] = None
+    ) -> None:
+        self.id = id_
+        self.assignment = assignment
+        self.key = f"{assignment}-{id_}"
+        self.persons = persons or []
+        self.repository = repository
+
+    @classmethod
+    def from_csv_line(
+        cls, headers: Dict[str, int], data: List[str], assignments: List[str], person: 'Person'
+    ) -> 'List[Team]':
+        ret: 'List[Team]' = []
+        for ass in assignments:
+            if not save_get(data, headers[f"Team{ass}"]):
+                continue
+            team = Team(
+                int(data[headers[f"Team{ass}"]]),
+                ass,
+                [person]
+            )
+            if f"WebUrlRepo{ass}" in headers and data[headers[f"WebUrlRepo{ass}"]]:
+                server = data[headers[f"WebUrlRepo{ass}"]].split("//", 1)[1].split("/", 1)
+                team.repository = Repository(
+                    server[0],
+                    server[1],
+                    int(data[headers[f"Repo{ass}ID"]])
+                )
+            ret.append(team)
+        return ret
+
+    def __repr__(self) -> str:
+        return f"Team({self.id}, {self.assignment}, {self.persons!r}, {self.repository!r})"
