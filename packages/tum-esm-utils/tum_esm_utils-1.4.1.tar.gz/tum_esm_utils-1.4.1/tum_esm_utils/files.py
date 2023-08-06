bb6f0587d4f5f0c3b110b1589aa371d9a3370d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:04 +0000

    Add package tum-esm-utils/1.4.1/tum_esm_utils-1.4.1.tar.gz

---
+++ packages/tum-esm-utils/tum_esm_utils-1.4.1.tar.gz/tum_esm_utils-1.4.1/tum_esm_utils/files.py
@@ -0,0 +1,35 @@
+import json
+import os
+from typing import Any, Optional
+
+
+def load_file(path: str) -> str:
+    with open(path, "r") as f:
+        return f.read()
+
+
+def dump_file(path: str, content: str) -> None:
+    with open(path, "w") as f:
+        f.write(content)
+
+
+def load_json_file(path: str) -> Any:
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def dump_json_file(path: str, content: Any, indent: Optional[int] = 4) -> None:
+    with open(path, "w") as f:
+        json.dump(content, f, indent=indent)
+
+
+def get_parent_dir_path(script_path: str, current_depth: int = 1) -> str:
+    """Get the absolute path of a parent directory based on the
+    current script path. Simply pass the `__file__` variable of
+    the current script to this function. Depth of 1 will return
+    the direct parent directory of the current script."""
+    assert current_depth > 0, "depth must be greater than 0"
+    output = os.path.dirname(os.path.abspath(script_path))
+    for _ in range(current_depth - 1):
+        output = os.path.dirname(output)
+    return output
