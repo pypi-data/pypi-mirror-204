237b1b067377c23fc7a485e0d8a5f089f030ccba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:19:50 +0000

    Add package lamindb/0.38.2/lamindb-0.38.2.tar.gz

---
+++ packages/lamindb/lamindb-0.38.2.tar.gz/lamindb-0.38.2/sub/lnschema-wetlab/README.md
@@ -0,0 +1,5 @@
+[![pypi](https://img.shields.io/pypi/v/lnschema_wetlab?color=%2334D058&label=pypi%20package)](https://pypi.org/project/lnschema_wetlab)
+
+# lnschema-wetlab: Generic bio wetlab (`vvhc`)
+
+Read the [docs](https://lamin.ai/docs/lnschema-wetlab).
---
+++ packages/lamindb/lamindb-0.38.2.tar.gz/lamindb-0.38.2/sub/lnschema-wetlab/lnschema_wetlab/link.py
@@ -0,0 +1,17 @@
+"""Link tables.
+
+.. autosummary::
+   :toctree: .
+
+   BiosampleTechsample
+   ProjectExperiment
+   FileBiosample
+   FileExperiment
+
+"""
+from ._link import (  # noqa
+    BiosampleTechsample,
+    FileBiosample,
+    FileExperiment,
+    ProjectExperiment,
+)
