d8b2e443bcdaf24e55a9b160e1c097428873e541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:40 +0000

    Add package satcfdi/4.0.16/satcfdi-4.0.16.tar.gz

---
+++ packages/satcfdi/satcfdi-4.0.16.tar.gz/satcfdi-4.0.16/satcfdi/utils.py
@@ -0,0 +1,27 @@
+import enum
+from collections.abc import Sequence, Mapping
+
+from lxml import etree
+
+parser = etree.XMLParser(no_network=True, remove_comments=True, remove_blank_text=True, huge_tree=True, collect_ids=False, remove_pis=True)
+
+
+class ScalarMap(dict):
+    pass
+
+
+def iterate(item):
+    if isinstance(item, str | bytes | ScalarMap):
+        return [item]
+    if isinstance(item, Mapping):
+        return item.values()
+    if isinstance(item, Sequence):
+        return item
+    if item is None:
+        return []
+    return [item]
+
+
+class StrEnum(str, enum.Enum):
+    def __str__(self):
+        return self.value
