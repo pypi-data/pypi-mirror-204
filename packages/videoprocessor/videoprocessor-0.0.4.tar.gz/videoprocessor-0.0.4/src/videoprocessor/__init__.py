5f3bae1df3e8618c6dbc89cf1731f55b367564a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:30 +0000

    Add package videoprocessor/0.0.4/videoprocessor-0.0.4.tar.gz

---
+++ packages/videoprocessor/videoprocessor-0.0.4.tar.gz/videoprocessor-0.0.4/src/videoprocessor/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding：utf-8 -*-
+# author = FogSalary
+# author_email = Kairong_Wu@163.com
+#
+
+from videoprocessor.videoprocessor import VideoProcessor
+from videoprocessor.videoprocessor import videoProc
