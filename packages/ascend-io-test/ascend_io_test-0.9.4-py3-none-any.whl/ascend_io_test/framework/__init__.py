7b9c0b1996211d2692d8044b30b1fcc6ab37f88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:58 +0000

    Add package ascend-io-test/0.9.4/ascend_io_test-0.9.4-py3-none-any.whl

---
+++ packages/ascend-io-test/ascend_io_test-0.9.4-py3-none-any.whl/ascend_io_test/framework/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+logging.getLogger().setLevel(logging.WARN)
+
+from .ascend_pyspark_transform import *
+from .ascend_python_bytestream_read_connector import *
+from .ascend_python_read_connector import *
+
+__all__ = ['AscendPySparkTransform',
+           'AscendPythonBytestreamReadConnector',
+           'AscendPythonReadConnector']
