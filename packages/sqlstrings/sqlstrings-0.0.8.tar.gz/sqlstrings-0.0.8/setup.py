6b8f191a9709101ecda61390642020cd2cbf8c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package sqlstrings/0.0.8/sqlstrings-0.0.8.tar.gz

---
+++ packages/sqlstrings/sqlstrings-0.0.8.tar.gz/sqlstrings-0.0.8/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='sqlstrings',
+    version='0.0.8',    
+    description='Generates SQL query strings in multiple dialects.',
+    keywords='sql strings generate query',
+    url='https://github.com/alikellaway/sqlstrings',
+    author='Ali Kellaway',
+    author_email='ali.kellaway139@gmail.com',
+    license='MIT',
+    # package_dir={'':''},
+    packages=['sqlstrings'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',         
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+    ]
+)
