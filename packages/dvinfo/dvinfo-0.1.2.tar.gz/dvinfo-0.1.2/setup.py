edb316b5a38c9c7b65f513c418bce2810c7da6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:54 +0000

    Add package dvinfo/0.1.2/dvinfo-0.1.2.tar.gz

---
+++ packages/dvinfo/dvinfo-0.1.2.tar.gz/dvinfo-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dvinfo",
+    version="0.1.2",
+    description="A package for getting system information on Windows and Linux",
+    url="https://github.com/0mgRod/deviceinfo",
+    author="OmgRod",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    keywords="system info",
+    packages=find_packages(),
+    install_requires=[
+        "psutil",
+        "netifaces",
+        "wmi",
+    ],
+)
