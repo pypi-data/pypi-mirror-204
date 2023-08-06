b008530516fef710e12f25327843d86fe5f8d6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:07 +0000

    Add package llm4gpt/2023.4.20.17.30.9/llm4gpt-2023.4.20.17.30.9.tar.gz

---
+++ packages/llm4gpt/llm4gpt-2023.4.20.17.30.9.tar.gz/llm4gpt-2023.4.20.17.30.9/llm/chatllm.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : chatllm
+# @Time         : 2023/4/20 15:46
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from langchain.llms.base import LLM
+from langchain.llms.utils import enforce_stop_tokens
+
+# ME
+from meutils.pipe import *
+from llm.utils import cuda_empty_cache
+
+
+class ChatLLM(LLM):
+    """
+    from llm.utils import llm_load
+
+    model, tokenizer = llm_load("/Users/betterme/PycharmProjects/AI/CHAT_MODEL/chatglm")
+    glm = ChatLLM()
+    glm.chat_func = model.chat # partial(self.chat_func, **kwargs)
+    """
+    chat_func: Callable = None
+    history = []
+    max_turns = 3
+
+    @property
+    def _llm_type(self) -> str:
+        return "ChatLLM"
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        cuda_empty_cache()  # todo: 减少调用次数
+
+        response, _ = self.chat_func(query=prompt, history=self.history[-self.max_turns:])
+        if stop:
+            response = enforce_stop_tokens(response, stop)
+        self.history += [(None, response)]
+        return response
+
+    def set_chat_kwargs(self, **kwargs):
+        self.chat_func = partial(self.chat_func, **kwargs)
+
+
+if __name__ == '__main__':
+    from llm.utils import llm_load
+
+    model, tokenizer = llm_load("/Users/betterme/PycharmProjects/AI/CHAT_MODEL/chatglm")
+    glm = ChatLLM()
+    glm.chat_func = model.chat  # partial(self.chat_func, **kwargs)
