6350dadc00bdcd46e9579257ea9335110a365b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:13 +0000

    Add package avplib/1.4.5/avplib-1.4.5.tar.gz

---
+++ packages/avplib/avplib-1.4.5.tar.gz/avplib-1.4.5/setup.py
@@ -0,0 +1,20 @@
+import os
+import setuptools
+
+# * This setup
+setuptools.setup(
+	name="avplib",
+	version="1.4.5",
+	description='AVP - ASCII Video Player. Allows you to play any video as ASCII-art.',
+	keywords=["avplib"],
+	packages=setuptools.find_packages(),
+	author_email='semina054@gmail.com',
+	url="https://github.com/romanin-rf/avplib",
+	long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+	long_description_content_type="text/markdown",
+	include_package_data=True,
+	author='ProgrammerFromParlament',
+	license='MIT',
+	install_requires=["click", "rich", "soundfile", "opencv-python", "pillow", "pygame", "fpstimer", "ffmpeg", "moviepy", "numpy"],
+    setup_requires=["click", "rich", "soundfile", "opencv-python", "pillow", "pygame", "fpstimer", "ffmpeg", "moviepy", "numpy"]
+)
