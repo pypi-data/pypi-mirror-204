b434fcfa41f759e13b2b1a3f7c097af1ec291a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:03 +0000

    Add package tggl-client/1.1.0/tggl_client-1.1.0.tar.gz

---
+++ packages/tggl-client/tggl_client-1.1.0.tar.gz/tggl_client-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tggl_client',
+    version='1.1.0',
+    description='Tggl python client',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['Tggl', 'feature flag'],
+)
