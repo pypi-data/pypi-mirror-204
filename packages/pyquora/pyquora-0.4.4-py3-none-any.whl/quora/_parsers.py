35f5745af4d2599c3da5669556c4c9e98c074061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:45 +0000

    Add package pyquora/0.4.4/pyquora-0.4.4-py3-none-any.whl

---
+++ packages/pyquora/pyquora-0.4.4-py3-none-any.whl/quora/_parsers.py
@@ -0,0 +1,35 @@
+"""Data parsers for library."""
+import json
+import re
+
+from quora.answer import Answer
+from quora.topic import Topic
+from .exceptions import ProfileNotFoundError
+
+
+def parse_page(html_data, user):
+    """Parse HTML and return JSON."""
+    try:
+        data = re.search(
+            r'window\.ansFrontendGlobals\.data\.inlineQueryResults\.results\[".*\]\.push\((?P<extracted_data>\".*\")\);',
+            html_data,
+        )  # noqa: E501
+        data = data.group('extracted_data')
+        data = json.loads(json.loads(data))
+        return data["data"]["user"]
+    except Exception as e:
+        raise ProfileNotFoundError(
+            f"No profile\
+found with the username {user.username}."
+        )
+
+
+def parse_answers(json_data, user, language):
+    """Parse JSON string of answers and return list of `Answer` object."""
+    answers = json_data["recentPublicAndPinnedAnswersConnection"]["edges"]
+    return [Answer(ans["node"], user, language) for ans in answers]
+
+
+def parse_topics(json_data, language):
+    topics = json_data["expertiseTopicsConnection"]["edges"]
+    return [Topic(topic["node"], language) for topic in topics]
