a59873c716bf3ab4b646f998181d3859d94ce74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:53 +0000

    Add package sentiic/0.0.3/sentiic-0.0.3.tar.gz

---
+++ packages/sentiic/sentiic-0.0.3.tar.gz/sentiic-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from distutils.core import setup
+setup(
+    name = "sentiic",
+    version= "0.0.3",
+    description= " Sentimental Analysis From Social Media",
+    author="DUMPA NAVEEN KUMAR REDDY",
+    author_email="naveenkumarreddydumpa@gmail.com",
+    py_modules=["rdvac6"],
+    package_dir={"":"src"},
+    include_package_data=True,
+)
