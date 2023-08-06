1ff42f42eb0954b1d0443cafd8fef957e583692d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:04 +0000

    Add package biobear/0.1.2/biobear-0.1.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/biobear/biobear-0.1.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/biobear/fasta_reader.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from .biobear import _FastaReader
+
+import polars as pl
+
+class FastaReader:
+    def __init__(self, path: Path):
+        self._fasta_reader = _FastaReader(str(path))
+
+    def read(self) -> pl.DataFrame:
+        return self.to_polars()
+
+    def to_polars(self) -> pl.DataFrame:
+        contents = self._fasta_reader.read()
+        return pl.read_ipc(contents)
