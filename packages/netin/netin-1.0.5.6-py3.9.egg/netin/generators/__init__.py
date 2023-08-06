65ada33e30c4345cf539b32d4f4029d473482724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:24 +0000

    Add package netin/1.0.5.6/netin-1.0.5.6-py3.9.egg

---
+++ packages/netin/netin-1.0.5.6-py3.9.egg/netin/generators/__init__.py
@@ -0,0 +1,18 @@
+# from .graph import Graph
+# from .h import Homophily
+# from .tc import TC
+# from .directed import DiGraph
+# from .undirected import UnDiGraph
+
+from .graph import Graph
+from .directed import DiGraph
+from .undirected import UnDiGraph
+from .dh import DH
+from .dpa import DPA
+from .dpah import DPAH
+from .pa import PA
+from .pah import PAH
+from .patc import PATC
+from .patch import PATCH
+from .tc import TriadicClosure
+from .h import Homophily
