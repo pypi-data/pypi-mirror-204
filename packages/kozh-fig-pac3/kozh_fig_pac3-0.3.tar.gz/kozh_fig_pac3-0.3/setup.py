bf52cb078aeb765e601ef347a33fd64be915f00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:22 +0000

    Add package kozh-fig-pac3/0.3/kozh_fig_pac3-0.3.tar.gz

---
+++ packages/kozh-fig-pac3/kozh_fig_pac3-0.3.tar.gz/kozh_fig_pac3-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kozh_fig_pac3',
+      version='0.3',
+      description='circle, triangle, square',
+      packages=['kozh_fig_pac3'],
+      author_email='alexeykozhakin@gmail.com',
+      zip_safe=False)
