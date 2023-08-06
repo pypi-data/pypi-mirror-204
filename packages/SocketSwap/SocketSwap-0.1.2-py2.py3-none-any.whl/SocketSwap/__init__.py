45cc9c44431402e495255c9592832fb139728b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:54:38 +0000

    Add package SocketSwap/0.1.2/SocketSwap-0.1.2-py2.py3-none-any.whl

---
+++ packages/SocketSwap/SocketSwap-0.1.2-py2.py3-none-any.whl/SocketSwap/__init__.py
@@ -0,0 +1,15 @@
+"""
+SocketSwap
+
+SocketSwap is a python package that allows to proxy any third-party libraries traffic through a local TCP Proxy
+"""
+
+__version__ = "0.1.0"
+__author__ = 'fyx99'
+__credits__ = 'No credits'
+
+from SocketSwap.context_manager import SocketSwapContext
+
+__all__ = [
+    SocketSwapContext
+]
