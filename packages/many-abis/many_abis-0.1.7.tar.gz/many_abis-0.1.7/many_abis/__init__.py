250ac65fdf2b7999dbf962bbf58d1f1ead331433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:33 +0000

    Add package many-abis/0.1.7/many_abis-0.1.7.tar.gz

---
+++ packages/many-abis/many_abis-0.1.7.tar.gz/many_abis-0.1.7/many_abis/__init__.py
@@ -0,0 +1,5 @@
+from .utils import *
+from .chains import *
+from .abis import *
+from ._base import *
+from .constants import *
---
+++ packages/many-abis/many_abis-0.1.7.tar.gz/many_abis-0.1.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+# print(find_packages())
+setup(
+    name='many_abis',
+    packages=['many_abis'],
+    package_dir={'many_abis': 'many_abis'},
+    package_data={'': [
+        '**/*.abi', '**/*.json']},
+    version='0.1.7',
+    license='MIT',
+    description='A simple way to get different DEXs abis for block chains.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Yong',
+    author_email='ackness8@gmail.com',
+    url='https://github.com/ackness/many_abis',
+    keywords=['abi', 'dex', 'block chain', 'bsc', 'eth', 'matic', 'heco',
+              'kcc', 'pancakeswap', 'mdex', 'quickswap', 'uniswap', 'koffeeswap'],
+
+    install_requires=[
+        "eth_utils",
+        "addict"
+    ],
+
+)
