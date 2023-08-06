727d64c448a29c1bbace9db45927a48cbacf766d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:26 +0000

    Add package metamorphic-relations/0.1.1/metamorphic_relations-0.1.1-py3-none-any.whl

---
+++ packages/metamorphic-relations/metamorphic_relations-0.1.1-py3-none-any.whl/metamorphic_relations/MRs.py
@@ -0,0 +1,15 @@
+
+
+class MR:
+
+    def adder(self, num1, num2):
+        """
+        Adds two numbers together
+
+        :param self: the object
+        :param num1: the first number
+        :param num2: the second number
+        :return: Their sum
+        """
+
+        return num1 + num2
---
+++ packages/metamorphic-relations/metamorphic_relations-0.1.1-py3-none-any.whl/metamorphic_relations/__init__.py
@@ -0,0 +1,1 @@
+from metamorphic_relations.MR import MR
