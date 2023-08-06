3e0ab8e4c7ce1f4814553446179ed27dacce0e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:33 +0000

    Add package pyclarity/0.3.1/pyclarity-0.3.1-py3-none-any.whl

---
+++ packages/pyclarity/pyclarity-0.3.1-py3-none-any.whl/clarity/utils/signal_processing.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+import numpy as np
+
+
+def compute_rms(signal: np.ndarray) -> float:
+    """Compute RMS of signal
+    Args:
+        signal: Signal to compute RMS of.
+    Returns:
+        float: RMS of the signal.
+    """
+    if len(signal) == 0:
+        return 0
+    return np.sqrt(np.mean(np.square(signal)))
+
+
+def normalize_signal(signal: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    """Standardize the signal to have zero mean and unit variance.
+
+    Args:
+        signal: The signal to be standardized.
+    Returns:
+        The standardized signal and the reference signal.
+    """
+    ref = signal.mean(0)
+    return (signal - ref.mean()) / ref.std(), ref
+
+
+def denormalize_signals(sources: np.ndarray, ref: np.ndarray) -> np.ndarray:
+    """Scale signals back to the original scale.
+
+    Args:
+        sources (np.ndarray): Source to be scaled.
+        ref (np.ndarray): Original sources to be used for reverting scaling.
+
+    Returns:
+        np.ndarray: Signal rescaled back to its original."""
+    return sources * ref.std() + ref.mean()
