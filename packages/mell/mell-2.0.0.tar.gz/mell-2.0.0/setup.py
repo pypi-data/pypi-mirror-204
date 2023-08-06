aece922a74801e5176b26b1f23d27a62e8bd7e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:06 +0000

    Add package mell/2.0.0/mell-2.0.0.tar.gz

---
+++ packages/mell/mell-2.0.0.tar.gz/mell-2.0.0/setup.py
@@ -0,0 +1,43 @@
+
+#import mell.consts as c
+import setuptools
+
+name = "mell"
+version = "2.0.0"
+author = "Diego Souza"
+author_email = "diegofpsouza+mell@gmail.com"
+url = "https://github.com/diegofps/pywup"
+
+description = "A Metaprogramming Logic Layer designed to generate anything from template files"
+long_description = None
+long_description_content_type = None # "text/markdown"
+
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type=long_description_content_type,
+    url=url,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'mell = mell.main:main'
+        ],
+    },
+    install_requires=[
+        'jinja2'
+    ],
+    extras_require={
+        "full": []
+    }
+)
+
