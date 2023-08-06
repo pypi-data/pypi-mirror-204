1d74d7c4a4efe438101919fc125d7c8f3d2b53c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:00 +0000

    Add package AAEEGGOOSS/0.3/AAEEGGOOSS-0.3.tar.gz

---
+++ packages/AAEEGGOOSS/AAEEGGOOSS-0.3.tar.gz/AAEEGGOOSS-0.3/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name= 'AAEEGGOOSS',
+    version='0.3',
+    author= 'AegosCOOOD',
+    description='Gamaa',
+    packages= setuptools.find_packages(),
+    python_requires = '>=3.8',
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
