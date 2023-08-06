c2df5d7bc2d59dc197e0e667adbf712cdd7683c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:02 +0000

    Add package ssenv/1.0.0/ssenv-1.0.0-py3-none-any.whl

---
+++ packages/ssenv/ssenv-1.0.0-py3-none-any.whl/ssenv/main.py
@@ -0,0 +1,22 @@
+import os
+
+class Environment:
+    def __init__(self):
+        self._dotenv_path = os.path.join(os.getcwd(), '.env')
+        self._dotenv_dict = {}
+        self.load_dotenv()
+    
+    def __str__(self) -> dict:
+        return self._dotenv_dict
+
+    def load_dotenv(self):
+        with open(self._dotenv_path) as f:
+            for line in f:
+                line = line.strip()
+                if not line or line.startswith('#'):
+                    continue
+                k, v = line.split('=', 1)
+                self.dotenv_dict[k] = v
+
+    def get(self, key: str):
+        return self._dotenv_dict[key] if key in self._dotenv_dict else None
