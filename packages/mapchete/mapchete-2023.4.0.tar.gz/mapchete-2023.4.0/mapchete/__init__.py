34661c133c34494f52b123694c90d3ae8efac2ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package mapchete/2023.4.0/mapchete-2023.4.0.tar.gz

---
+++ packages/mapchete/mapchete-2023.4.0.tar.gz/mapchete-2023.4.0/mapchete/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+from mapchete._core import open, Mapchete
+from mapchete._executor import Executor, FakeFuture, SkippedFuture
+from mapchete._processing import Job, ProcessInfo
+from mapchete._user_process import MapcheteProcess
+from mapchete.tile import count_tiles
+from mapchete._timer import Timer
+
+
+__all__ = [
+    "open",
+    "count_tiles",
+    "Mapchete",
+    "MapcheteProcess",
+    "ProcessInfo",
+    "Timer",
+    "Executor",
+    "FakeFuture",
+    "SkippedFuture",
+    "Job",
+]
+__version__ = "2023.4.0"
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
