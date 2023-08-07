25a36bc3008e64eaf101cf7ff8aea643812d8b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:44 +0000

    Add package pychoosealicense/2023.4.25/pychoosealicense-2023.4.25-py3-none-any.whl

---
+++ packages/pychoosealicense/pychoosealicense-2023.4.25-py3-none-any.whl/pychoosealicense/_licenses/__init__.py
@@ -0,0 +1,17 @@
+"""
+License metadata used by ``pychoosealicense``.
+"""
+
+CHOOSEALICENSE_COMMIT_INFO = """
+commit 6364c98418b689efccdeeb4c2f82eb84c21b6d3c
+Merge: 0ccd6c5 d68908d
+Author: Mike Linksvayer <mlinksva@github.com>
+Date:   Wed Mar 29 15:21:03 2023 -0700
+
+    Merge pull request #1096 from tomjmech/patch-1
+
+    Fix inconsistent quotes in UPL-1.0
+"""
+
+CHOOSEALICENSE_COMMIT_HASH = "6364c98418b689efccdeeb4c2f82eb84c21b6d3c"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "6364c98"
