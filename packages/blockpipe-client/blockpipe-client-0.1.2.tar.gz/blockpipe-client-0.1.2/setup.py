f31ce27af0941f89a8e34cf5b02e2c89322bca0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:37 +0000

    Add package blockpipe-client/0.1.2/blockpipe-client-0.1.2.tar.gz

---
+++ packages/blockpipe-client/blockpipe-client-0.1.2.tar.gz/blockpipe-client-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="blockpipe-client",
+    version="0.1.2",
+    author="blockpipe.io",
+    author_email="support@blockpipe.io",
+    description="A Python library for interacting with Blockpipe Endpoint API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blockpipe/python",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+    install_requires=["requests"],
+)
