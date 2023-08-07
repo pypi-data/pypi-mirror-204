a5d04010ee7852a57f18b99b29ebe2b6ff7c0105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:43:12 +0000

    Add package eotdl-cli/0.0.7/eotdl_cli-0.0.7-py3-none-any.whl

---
+++ packages/eotdl-cli/eotdl_cli-0.0.7-py3-none-any.whl/eotdl_cli/src/repos/APIRepo.py
@@ -0,0 +1,64 @@
+import requests
+from tqdm import tqdm
+from pathlib import Path
+import os 
+import httpx
+import time
+
+class APIRepo():
+    def __init__(self, url='http://localhost:8000/'):
+    # def __init__(self, url='https://api.eotdl.com/'):
+        self.url = url
+
+    def login(self):
+        return requests.get(self.url + 'auth/login')
+    
+    def token(self, code):
+        return requests.get(self.url + 'auth/token?code=' + code)
+
+    def logout_url(self):
+        response = requests.get(self.url + 'auth/logout')
+        return response.json()['logout_url']
+    
+    def retrieve_datasets(self):
+        return requests.get(self.url + 'datasets').json()
+
+    def retrieve_dataset(self, name):
+        return requests.get(self.url + 'datasets?name=' + name)
+    
+    def download_dataset(self, dataset_id, id_token, path):
+        url = self.url + 'datasets/' + dataset_id + '/download'
+        headers={'Authorization': 'Bearer ' + id_token}
+        if path is None:
+            path = str(Path.home()) + '/.etodl/datasets'
+            os.makedirs(path, exist_ok=True)
+        with requests.get(url, headers=headers, stream=True) as r:
+            r.raise_for_status()
+            total_size = int(r.headers.get('content-length', 0))
+            block_size = 1024  # 1 Kibibyte
+            progress_bar = tqdm(total=total_size, unit='iB', unit_scale=True)
+            filename = r.headers.get('content-disposition').split('filename=')[1][1:-1]
+            path = f'{path}/{filename}'
+            with open(path, 'wb') as f:
+                for chunk in r.iter_content(block_size):
+                    progress_bar.update(len(chunk))
+                    f.write(chunk)
+            progress_bar.close()
+            return path
+        
+    def ingest_dataset(self, name, description, path, id_token):
+        # Not sure this will work with large datasets, need to test
+        url = self.url + 'datasets'
+        # headers={'Authorization': 'Bearer ' + id_token}
+        headers={'Filename': 'bigFile.zip'}
+        files = {'file': open(path, 'rb')}
+        # data = {'name': name, 'description': description}
+        data = {'data': 'Hello World!'}
+        # response = requests.post(url, headers=headers, files=files, data=data)
+        with httpx.Client() as client:
+            start = time.time()
+            r = client.post(url, data=data, files=files, headers=headers)
+            end = time.time()
+            print(f'Time elapsed: {end - start}s')
+            print(r.status_code, r.json(), sep=' ')
+        # return response
