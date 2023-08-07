29d506cfca85d40e43ece646bfea60027524b529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:08 +0000

    Add package Age-detection/0.0.1/Age_detection-0.0.1.tar.gz

---
+++ packages/Age-detection/Age_detection-0.0.1.tar.gz/Age_detection-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='Age_detection',  
+     version='0.0.1',
+     author="Circles",
+     author_email="info@bubbelz.life",
+     description="PyPI Package for age detection",
+     long_description="This is a package for running age detection and getting approximate age",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
