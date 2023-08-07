71895366ddd47bd27486339f86b295747ee01feb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:25 +0000

    Add package scipion-em-xmipptomo/3.23.3.6/scipion-em-xmipptomo-3.23.3.6.tar.gz

---
+++ packages/scipion-em-xmipptomo/scipion-em-xmipptomo-3.23.3.6.tar.gz/scipion-em-xmipptomo-3.23.3.6/xmipptomo/utils.py
@@ -0,0 +1,96 @@
+# **************************************************************************
+# *
+# * Authors:     Federico P. de Isidro Gomez (fp.deisidro@cnb.csic.es) [1]
+# *
+# * [1] Centro Nacional de Biotecnologia, CSIC, Spain
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+This module contains utils functions for xmipp tomo protocols
+"""
+
+import math
+import csv
+
+import emtable
+from tomo.constants import BOTTOM_LEFT_CORNER
+from pwem import emlib
+
+
+def calculateRotationAngleFromTM(ti):
+    """ This method calculates que tilt image rotation angle from its associated transformation matrix."""
+
+    tm = ti.getTransform().getMatrix()
+    cosRotationAngle = tm[0][0]
+    sinRotationAngle = tm[1][0]
+    rotationAngle = math.degrees(math.atan(sinRotationAngle/cosRotationAngle))
+
+    return rotationAngle
+
+
+def readXmdStatisticsFile(fnmd):
+    x_pos = []
+    y_pos = []
+    z_pos = []
+    avg = []
+    std = []
+
+    table = emtable.Table(fileName=fnmd)
+
+    for row in table.iterRows(fileName='noname@'+fnmd):
+        avg.append(row.get('avg'))
+        std.append(row.get('stddev'))
+        x_pos.append(row.get('xcoor'))
+        y_pos.append(row.get('ycoor'))
+        z_pos.append(row.get('zcoor'))
+
+    return x_pos, y_pos, z_pos, avg, std
+
+
+def writeOutputCoordinates3dXmdFile(soc, filePath, tomoId=None):
+    """ Generates a 3D coordinates xmd file from the set of coordinates associated to a given tomogram (identified by
+     its tomo tomoId). If no tomoId is input the the xmd output file will contain all the coordinates belonging to the
+     set. """
+
+    xmdHeader = "# XMIPP_STAR_1 *\n" \
+                "#\n" \
+                "data_noname\n" \
+                "loop_\n" \
+                " _xcoor\n" \
+                " _ycoor\n" \
+                " _zcoor\n"
+
+    coordinatesInfo = []
+    fieldNames = ['x', 'y', 'z']
+
+    for coord in soc.iterCoordinates(tomoId):
+        coordinatesInfo.append([coord.getX(BOTTOM_LEFT_CORNER),
+                                coord.getY(BOTTOM_LEFT_CORNER),
+                                coord.getZ(BOTTOM_LEFT_CORNER)])
+
+    with open(filePath, 'w') as f:
+        f.write(xmdHeader)
+        writer = csv.DictWriter(f, delimiter='\t', fieldnames=fieldNames)
+
+        for ci in coordinatesInfo:
+            writer.writerow({'x': ci[0],
+                             'y': ci[1],
+                             'z': ci[2]})
