a5fc8448337907da6e2149caee62be6b7354e241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:22 +0000

    Add package hkhklab/0.0.2/hkhklab-0.0.2.tar.gz

---
+++ packages/hkhklab/hkhklab-0.0.2.tar.gz/hkhklab-0.0.2/hkhklab/commonFunction.py
@@ -0,0 +1,15 @@
+def calcSumListHk(inputList: list):
+
+    """ define function: calculate sum of List
+        parameter: (str) List 
+        return: (int) sum of List
+    """
+
+    # inputList = ["1","2","3","4","5"]
+    inputListLen = len(inputList)
+
+    for i in range(0, len(inputList)):
+        inputList[i]  = int( inputList[i] )
+
+    outputValue = sum(inputList)
+    return outputValue
