d740d965562be104d644e438fc30c4edf1338281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:40:43 +0000

    Add package payu-websdk/0.1.2/payu-websdk-0.1.2.tar.gz

---
+++ packages/payu-websdk/payu-websdk-0.1.2.tar.gz/payu-websdk-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme:
+    readme_content = readme.read()
+
+setup(
+    name='payu-websdk',
+    version='0.1.2',
+    description='PayU',
+    author='PayU Web SDK for Python',
+    author_email='integration@payu.in',
+    url='https://github.com/payu-india/web-sdk-python',
+    packages=find_packages(),
+    long_description=readme_content,
+    long_description_content_type='text/markdown',
+    license="MIT",
+    install_requires=[
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
