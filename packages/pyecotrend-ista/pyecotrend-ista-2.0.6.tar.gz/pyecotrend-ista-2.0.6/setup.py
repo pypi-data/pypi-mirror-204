524e4d1e3dca3150a054e343eb46e8999285cc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:44 +0000

    Add package pyecotrend-ista/2.0.6/pyecotrend-ista-2.0.6.tar.gz

---
+++ packages/pyecotrend-ista/pyecotrend-ista-2.0.6.tar.gz/pyecotrend-ista-2.0.6/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+import configparser
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("./src/pyecotrend_ista/const.py") as f:
+    config_string = "[dummy_section]\n" + f.read()
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read_string(config_string)
+    version = config["dummy_section"]["VERSION"].strip('"')
+
+setuptools.setup(
+    name="pyecotrend-ista",
+    version=version,
+    author="Ludy87",
+    author_email="android@astra-g.org",
+    description="Python ecotrend-ista Api",
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ludy87/pyecotrend-ista",
+    project_urls={
+        "Bug Tracker": "https://github.com/Ludy87/pyecotrend-ista/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Home Automation",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords="python api ecotrend ista",
+    install_requires=["aiohttp>=3.8.1", "dataclasses-json==0.5.7"],
+    python_requires=">=3.6",
+)
