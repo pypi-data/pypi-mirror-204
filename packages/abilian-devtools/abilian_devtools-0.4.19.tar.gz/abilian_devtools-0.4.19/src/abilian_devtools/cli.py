c6ac1262edcdab1e390cc52e97bfae17bcc400ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:41:53 +0000

    Add package abilian-devtools/0.4.19/abilian_devtools-0.4.19.tar.gz

---
+++ packages/abilian-devtools/abilian_devtools-0.4.19.tar.gz/abilian_devtools-0.4.19/src/abilian_devtools/cli.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2023 Abilian SAS <https://abilian.com/>
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import importlib.metadata
+
+from cleez import CLI
+
+
+def main():
+    cli = get_cli()
+    cli.run()
+
+
+def get_cli():
+    version = importlib.metadata.version("abilian-devtools")
+    cli = CLI(name="adt", version=version)
+    cli.add_option(
+        "-V",
+        "--version",
+        default=False,
+        action="store_true",
+        help="Show version and exit",
+    )
+    cli.add_option(
+        "-d", "--debug", default=False, action="store_true", help="Enable debug mode"
+    )
+    cli.add_option(
+        "-v", "--verbose", default=False, action="store_true", help="Increase verbosity"
+    )
+    cli.scan("abilian_devtools.commands")
+    return cli
