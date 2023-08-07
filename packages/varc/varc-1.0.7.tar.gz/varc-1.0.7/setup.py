91bce32d6ba53668e6f560280dfbc42a680a3579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:55 +0000

    Add package varc/1.0.7/varc-1.0.7.tar.gz

---
+++ packages/varc/varc-1.0.7.tar.gz/varc-1.0.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+VERSION = '1.0.7'
+
+setup(
+    name='varc',
+    version=VERSION,
+    description='varc Volatile Artifact Collector',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Cado Security',
+    author_email='varc@cadosecurity.com',
+    url='https://github.com/cado-security/varc',
+    download_url='https://github.com/cado-security/varc/archive/refs/heads/main.zip',
+    py_modules=['varc'],
+    install_requires=['psutil', 'mss', 'tqdm', 'pymem'],
+    packages=find_packages()
+)
