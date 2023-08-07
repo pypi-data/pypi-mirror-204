9bfe1a47e859395d7f13c796de989d6decbfc7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:42:22 +0000

    Add package nrdataaccess/0.0.1/nrdataaccess-0.0.1.tar.gz

---
+++ packages/nrdataaccess/nrdataaccess-0.0.1.tar.gz/nrdataaccess-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Example Package
+
+This is a shared code package.
---
+++ packages/nrdataaccess/nrdataaccess-0.0.1.tar.gz/nrdataaccess-0.0.1/src/error.py
@@ -0,0 +1,7 @@
+class DatabaseConnectionError(Exception):
+    def __init__(self,msg):
+        super().__init__(msg)
+
+class QueryExecutionError(Exception):
+    def __init__(self,msg):
+       super().__init__(msg)
