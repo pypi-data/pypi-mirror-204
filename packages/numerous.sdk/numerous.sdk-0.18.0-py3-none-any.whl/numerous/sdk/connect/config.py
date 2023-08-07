9e32d92d5d55a2da24cf5d584c140a792fd27ab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:45 +0000

    Add package numerous.sdk/0.18.0/numerous.sdk-0.18.0-py3-none-any.whl

---
+++ packages/numerous.sdk/numerous.sdk-0.18.0-py3-none-any.whl/numerous/sdk/connect/config.py
@@ -0,0 +1,20 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    max_status_message_length: int = 32
+    grpc_max_message_size: int = 2**28
+    terminate_handler_timeout: float | None = 5.0
+
+    @staticmethod
+    def from_environment():
+        config = Config()
+        if "MAX_STATUS_MESSAGE_LENGTH" in os.environ:
+            config.max_status_message_length = int(
+                os.environ["MAX_STATUS_MESSAGE_LENGTH"]
+            )
+        if "GRPC_MAX_MESSAGE_SIZE" in os.environ:
+            config.grpc_max_message_size = int(os.environ["GRPC_MAX_MESSAGE_SIZE"])
+        return config
