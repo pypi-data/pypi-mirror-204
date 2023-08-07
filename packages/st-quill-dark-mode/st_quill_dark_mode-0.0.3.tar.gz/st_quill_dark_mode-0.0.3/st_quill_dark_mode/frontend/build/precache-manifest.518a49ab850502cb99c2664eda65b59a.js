d40fc9de788fafb7bdb9e1e872cdf1f9d804125f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:12 +0000

    Add package st-quill-dark-mode/0.0.3/st_quill_dark_mode-0.0.3.tar.gz

---
+++ packages/st-quill-dark-mode/st_quill_dark_mode-0.0.3.tar.gz/st_quill_dark_mode-0.0.3/st_quill_dark_mode/frontend/build/precache-manifest.518a49ab850502cb99c2664eda65b59a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4227421d559d9e6b5609ef9a0f9ba8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5615eaf3c55f4d156b66",
+    "url": "./static/css/main.ad6d5a17.chunk.css"
+  },
+  {
+    "revision": "1e811ec26bdfd8b303e8",
+    "url": "./static/js/2.ecf90cd3.chunk.js"
+  },
+  {
+    "revision": "84988c0c37bbfd6e320c411b54c8aece",
+    "url": "./static/js/2.ecf90cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5615eaf3c55f4d156b66",
+    "url": "./static/js/main.c9752ecc.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
