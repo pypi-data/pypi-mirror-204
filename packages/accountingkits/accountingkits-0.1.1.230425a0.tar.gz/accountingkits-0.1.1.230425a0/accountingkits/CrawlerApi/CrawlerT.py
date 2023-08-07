f94c1dfd93f91bb02e659482ec52f5d7bc0276ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 00:39:54 +0000

    Add package accountingkits/0.1.1.230425a0/accountingkits-0.1.1.230425a0.tar.gz

---
+++ packages/accountingkits/accountingkits-0.1.1.230425a0.tar.gz/accountingkits-0.1.1.230425a0/accountingkits/CrawlerApi/CrawlerT.py
@@ -0,0 +1,25 @@
+import requests
+import bs4
+
+
+def request_html_url_text(url, **kwargs) -> str:
+    """
+    extract the texture in an url/website, return string. If you need sleep, set it outside.
+    :param url: the url to be request
+    :param kwargs: the kwargs of request.get(), which help you to modify and face some situations.
+    :return: string of url text
+    """
+    response = requests.get(url, **kwargs)
+    if response.status_code == 200:
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
+
+        # Remove script and style elements
+        for script in soup(['script', 'style']):
+            script.extract()
+
+        # Extract text
+        text = soup.get_text()
+
+        return text
+    else:
+        print(f"Error: Unable to fetch the content. Status code: {response.status_code}")
